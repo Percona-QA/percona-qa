4520e0e9c69d605e1978e7bc5a6d86103b757caa
Bernt M. Johnsen <bernt.johnsen@sun.com>
2009-10-19 09:29:37 +0200

    batch file for .t-tests in Windows

---
+++ randgen/t/test.bat
@@ -0,0 +1,11 @@
+perl t\gensql.t
+perl t\gensql.yy
+perl t\gentest.t
+perl t\grammar-doubledefine.t.disabled
+perl t\grammar-doubledefine.yy
+perl t\grammar.t
+perl t\prng.t
+perl t\simplify-empty.t
+perl t\simplify-empty.yy
+perl t\simplify-grammar.t
+perl t\simplify-grammar.yy
