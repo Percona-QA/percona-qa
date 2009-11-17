370e2531dfa9517c2dc871b793fd848643dc5437
John H. Embretsen <john.embretsen@sun.com>
2009-11-17 15:37:49 +0100

    XML: Whitespace changes only: Change tabs-->spaces, using 4-space indentation, for the entire XML package. The old mix of tabs and spaces for indentation was messy.

--- randgen/lib/GenTest/XML/BuildInfo.pm
+++ randgen/lib/GenTest/XML/BuildInfo.pm
@@ -7,107 +7,106 @@ use strict;
 use GenTest;
 use DBI;
 
-use constant BUILDINFO_DSNS	=> 0;
-use constant BUILDINFO_SERVERS	=> 1;
+use constant BUILDINFO_DSNS     => 0;
+use constant BUILDINFO_SERVERS  => 1;
 
-use constant BUILDINFO_SERVER_VERSION	=> 0;
-use constant BUILDINFO_SERVER_PACKAGE	=> 1;
-use constant BUILDINFO_SERVER_BIT	=> 2;
-use constant BUILDINFO_SERVER_PATH	=> 3;
-use constant BUILDINFO_SERVER_VARIABLES	=> 4;
+use constant BUILDINFO_SERVER_VERSION   => 0;
+use constant BUILDINFO_SERVER_PACKAGE   => 1;
+use constant BUILDINFO_SERVER_BIT       => 2;
+use constant BUILDINFO_SERVER_PATH      => 3;
+use constant BUILDINFO_SERVER_VARIABLES => 4;
 
 sub new {
-	my $class = shift;
+    my $class = shift;
 
-	my $buildinfo = $class->SUPER::new({
-		dsns	=> BUILDINFO_DSNS
-	}, @_);
+    my $buildinfo = $class->SUPER::new({
+        dsns    => BUILDINFO_DSNS
+    }, @_);
 
-	$buildinfo->[BUILDINFO_SERVERS] = [];
+    $buildinfo->[BUILDINFO_SERVERS] = [];
 
-	foreach my $id (0..$#{$buildinfo->[BUILDINFO_DSNS]}) {
-		my $dsn = $buildinfo->[BUILDINFO_DSNS]->[$id];
-		next if $dsn eq '';
-		my $dbh = DBI->connect($dsn);
+    foreach my $id (0..$#{$buildinfo->[BUILDINFO_DSNS]}) {
+    my $dsn = $buildinfo->[BUILDINFO_DSNS]->[$id];
+    next if $dsn eq '';
+    my $dbh = DBI->connect($dsn);
 
-		my $server;
+    my $server;
 
-		$server->[BUILDINFO_SERVER_VERSION] = $dbh->selectrow_array('SELECT @@version');
-		$server->[BUILDINFO_SERVER_PACKAGE] = $dbh->selectrow_array('SELECT @@version_comment');
-		$server->[BUILDINFO_SERVER_BIT] = $dbh->selectrow_array('SELECT @@version_compile_machine');
-		$server->[BUILDINFO_SERVER_PATH] = $dbh->selectrow_array('SELECT @@basedir');
-		$server->[BUILDINFO_SERVER_VARIABLES] = [];
+    $server->[BUILDINFO_SERVER_VERSION] = $dbh->selectrow_array('SELECT @@version');
+    $server->[BUILDINFO_SERVER_PACKAGE] = $dbh->selectrow_array('SELECT @@version_comment');
+    $server->[BUILDINFO_SERVER_BIT] = $dbh->selectrow_array('SELECT @@version_compile_machine');
+    $server->[BUILDINFO_SERVER_PATH] = $dbh->selectrow_array('SELECT @@basedir');
+    $server->[BUILDINFO_SERVER_VARIABLES] = [];
 
-		my $sth = $dbh->prepare("SHOW VARIABLES");
-		$sth->execute();
-		while (my ($name, $value) = $sth->fetchrow_array()) {
-			push @{$server->[BUILDINFO_SERVER_VARIABLES]}, [ $name , $value ];
-		}
-		$sth->finish();
+    my $sth = $dbh->prepare("SHOW VARIABLES");
+    $sth->execute();
+    while (my ($name, $value) = $sth->fetchrow_array()) {
+        push @{$server->[BUILDINFO_SERVER_VARIABLES]}, [ $name , $value ];
+    }
+    $sth->finish();
 
-		$dbh->disconnect();
+    $dbh->disconnect();
 
-		$buildinfo->[BUILDINFO_SERVERS]->[$id] = $server;
-	}
+    $buildinfo->[BUILDINFO_SERVERS]->[$id] = $server;
 
-	return $buildinfo;
+    return $buildinfo;
 }
 
 sub xml {
-	require XML::Writer;
+    require XML::Writer;
 
-	my $buildinfo = shift;
-	my $buildinfo_xml;
+    my $buildinfo = shift;
+    my $buildinfo_xml;
 
-	my $writer = XML::Writer->new(
-		OUTPUT		=> \$buildinfo_xml,
-	);
+    my $writer = XML::Writer->new(
+        OUTPUT      => \$buildinfo_xml,
+    );
 
-	$writer->startTag('product');
-	$writer->dataElement('name','mysql');
-	$writer->startTag('builds');
+    $writer->startTag('product');
+    $writer->dataElement('name','mysql');
+    $writer->startTag('builds');
 
-	foreach my $id (0..$#{$buildinfo->[BUILDINFO_DSNS]}) {
-		my $server = $buildinfo->[BUILDINFO_SERVERS]->[$id];
-		next if not defined $server;
+    foreach my $id (0..$#{$buildinfo->[BUILDINFO_DSNS]}) {
+    my $server = $buildinfo->[BUILDINFO_SERVERS]->[$id];
+    next if not defined $server;
 
-		$writer->startTag('build', id => $id);
-		$writer->dataElement('version', $server->[BUILDINFO_SERVER_VERSION]);
-		$writer->dataElement('package', $server->[BUILDINFO_SERVER_PACKAGE]);
-		$writer->dataElement('bit', $server->[BUILDINFO_SERVER_BIT]);
-		$writer->dataElement('path', $server->[BUILDINFO_SERVER_PATH]);
-		# <compile_options> 
-		$writer->endTag('build');
-	}
+    $writer->startTag('build', id => $id);
+    $writer->dataElement('version', $server->[BUILDINFO_SERVER_VERSION]);
+    $writer->dataElement('package', $server->[BUILDINFO_SERVER_PACKAGE]);
+    $writer->dataElement('bit', $server->[BUILDINFO_SERVER_BIT]);
+    $writer->dataElement('path', $server->[BUILDINFO_SERVER_PATH]);
+    # <compile_options>
+    $writer->endTag('build');
 
-	$writer->endTag('builds');
 
-	$writer->startTag('binaries');
+    $writer->endTag('builds');
 
-        foreach my $id (0..$#{$buildinfo->[BUILDINFO_DSNS]}) {
-		my $server = $buildinfo->[BUILDINFO_SERVERS]->[$id];
-		next if not defined $server;
+    $writer->startTag('binaries');
 
-		$writer->startTag('binary');
-		$writer->dataElement('name', 'mysqld');
-		$writer->startTag('commandline_options');
+    foreach my $id (0..$#{$buildinfo->[BUILDINFO_DSNS]}) {
+    my $server = $buildinfo->[BUILDINFO_SERVERS]->[$id];
+    next if not defined $server;
 
-		foreach my $option (@{$server->[BUILDINFO_SERVER_VARIABLES]}) {
-			$writer->startTag('option');
-			$writer->dataElement('name', $option->[0]);
-			$writer->dataElement('value', $option->[1]);
-			$writer->endTag('option');
-		}
+    $writer->startTag('binary');
+    $writer->dataElement('name', 'mysqld');
+    $writer->startTag('commandline_options');
 
-		$writer->endTag('commandline_options');
-		$writer->endTag('binary');
-	}
-	
-	$writer->endTag('binaries');
-	$writer->endTag('product');
-	$writer->end();
+    foreach my $option (@{$server->[BUILDINFO_SERVER_VARIABLES]}) {
+        $writer->startTag('option');
+        $writer->dataElement('name', $option->[0]);
+        $writer->dataElement('value', $option->[1]);
+        $writer->endTag('option');
+    }
 
-	return $buildinfo_xml;	
+    $writer->endTag('commandline_options');
+    $writer->endTag('binary');
+
+
+    $writer->endTag('binaries');
+    $writer->endTag('product');
+    $writer->end();
+
+    return $buildinfo_xml;
 }
 
 1;
--- randgen/lib/GenTest/XML/Environment.pm
+++ randgen/lib/GenTest/XML/Environment.pm
@@ -28,292 +28,292 @@ our $patch;
 our $DEBUG=0;
 
 sub new {
-	my $class = shift;
+    my $class = shift;
 
-	my $environment = $class->SUPER::new({
-	}, @_);
+    my $environment = $class->SUPER::new({
+    }, @_);
 
-	return $environment;
+    return $environment;
 }
 
 sub xml {
-	require XML::Writer;
+    require XML::Writer;
 
-	# Obtain environmental info from host.
-	# In separate function because lots of logic is needed to parse various
-	# information based on the OS.
-	getInfo();
+    # Obtain environmental info from host.
+    # In separate function because lots of logic is needed to parse various
+    # information based on the OS.
+    getInfo();
 
-	my $environment = shift;
-	my $environment_xml;
+    my $environment = shift;
+    my $environment_xml;
 
-	my $writer = XML::Writer->new(
-		OUTPUT		=> \$environment_xml,
-	);
+    my $writer = XML::Writer->new(
+        OUTPUT      => \$environment_xml,
+    );
 
-	$writer->startTag('environments');
-	$writer->startTag('environment', 'id' => 0);
-	$writer->startTag('hosts');
-	$writer->startTag('host');
+    $writer->startTag('environments');
+    $writer->startTag('environment', 'id' => 0);
+    $writer->startTag('hosts');
+    $writer->startTag('host');
 
-	# Some elements may be empty either because
-	#  a) we don't know that piece of information
-	#  b) values are fetched from a database of test hosts
-	$writer->dataElement('name', $hostname);
-	$writer->dataElement('arch', $arch);
-	$writer->dataElement('kernel', $kernel);
-	$writer->dataElement('bit', $bit) if defined $bit;
-	$writer->dataElement('cpu', $cpu);
-	$writer->dataElement('memory', $memory);
-	$writer->dataElement('disk', '');
-	$writer->dataElement('role', 'server');
-	$writer->dataElement('locale', $locale);
-	$writer->dataElement('encoding', $encoding);
-	$writer->dataElement('timezone', $tz);
+    # Some elements may be empty either because
+    #  a) we don't know that piece of information
+    #  b) values are fetched from a database of test hosts
+    $writer->dataElement('name', $hostname);
+    $writer->dataElement('arch', $arch);
+    $writer->dataElement('kernel', $kernel);
+    $writer->dataElement('bit', $bit) if defined $bit;
+    $writer->dataElement('cpu', $cpu);
+    $writer->dataElement('memory', $memory);
+    $writer->dataElement('disk', '');
+    $writer->dataElement('role', 'server');
+    $writer->dataElement('locale', $locale);
+    $writer->dataElement('encoding', $encoding);
+    $writer->dataElement('timezone', $tz);
 
-	# <software> ...
-	$writer->startTag('software');
+    # <software> ...
+    $writer->startTag('software');
 
-	# <os>
-	$writer->startTag('program');
-	$writer->dataElement('name', $osType);
-	$writer->dataElement('version', $osVer);
-	$writer->dataElement('patch', $patch) if defined $patch;
-	$writer->endTag('program');
-	
-	# <perl>
-	$writer->startTag('program');
-	$writer->dataElement('name', 'perl');
-	#$writer->dataElement('version', $^V); # Solaris yields: Code point \u0005 is not a valid character in XML at lib/GenTest/XML/Environment.pm line 45
-	$writer->dataElement('path', $^X);
-	$writer->endTag('program');
+    # <os>
+    $writer->startTag('program');
+    $writer->dataElement('name', $osType);
+    $writer->dataElement('version', $osVer);
+    $writer->dataElement('patch', $patch) if defined $patch;
+    $writer->endTag('program');
 
-	$writer->endTag('software');
+    # <perl>
+    $writer->startTag('program');
+    $writer->dataElement('name', 'perl');
+    #$writer->dataElement('version', $^V); # Solaris yields: Code point \u0005 is not a valid character in XML at lib/GenTest/XML/Environment.pm line 45
+    $writer->dataElement('path', $^X);
+    $writer->endTag('program');
 
-	$writer->endTag('host');
-	$writer->endTag('hosts');
-	$writer->endTag('environment');
-	$writer->endTag('environments');
+    $writer->endTag('software');
 
-	$writer->end();
+    $writer->endTag('host');
+    $writer->endTag('hosts');
+    $writer->endTag('environment');
+    $writer->endTag('environments');
 
-	return $environment_xml;	
+    $writer->end();
+
+    return $environment_xml;
 }
 
 sub getInfo()
 {
 
-  # lets see what OS type we have
-  if ($^O eq 'linux')
-  {
-    #$hostName = `hostname`;
-    #my $tmpHost = `host $hostName`;
-    #if ($tmpHost =~ m/has address/){
-    #  ($hostName,my $trash) = split(/has address/,$tmpHost);
-    #  $hostName = trim($hostName);
-    #}
-    # Get the CPU info
-    $cpu = trim(`cat /proc/cpuinfo | grep -i "model name" | head -n 1 | cut -b 14-`);
-    my $numOfP = trim(`cat /proc/cpuinfo | grep processor |wc -l`);
-    $cpu ="$numOfP"."x"."$cpu";
-
-    #try to get OS Information
-    if (-e "/etc/SuSE-release"){$osVer=`cat /etc/SuSE-release  |head -n 1`;}
-    elsif (-e "/etc/redhat-release"){$osVer=`cat /etc/redhat-release  |head -n 1`;}
-    elsif (-e "/etc/debian_version"){$osVer=`cat /etc/debian_version  |head -n 1`;}
-    else {$osVer="linux-unknown";}
-    $osVer=trim($osVer);
-    if (-e "/etc/SuSE-release"){$patch=`cat /etc/SuSE-release  |tail -n 1`;}
-    elsif (-e "/etc/redhat-release"){$patch=`cat /etc/redhat-release  |tail -n 1`;}
-    elsif (-e "/etc/debian_version"){$patch=`cat /etc/debian_version  |tail -n 1`;}
-    else {$patch="unknown";}
-    (my $trash, $patch) = split(/=/,$patch);
-    $osType="Linux";
-    $arch=trim(`uname -m`);
-    ($trash, $bit) = split(/_/,$arch);
-    $kernel=trim(`uname -r`);
-
-    #Memory
-    $memory = trim(`cat /proc/meminfo | grep -i memtotal`);
-    $memory =~ s/MemTotal: //;
-    ($memory, $trash) =  split(/k/,$memory);
-    $memory = trim(`cat /proc/meminfo |grep -i memtotal`);
-    $memory =~ /MemTotal:\s*(\d+)/;
-    $memory = sprintf("%.2f",($1/1024000))."GB";
-
-    #locale
-    if (defined ($locale=`locale |grep LC_CTYPE| cut -b 10-`))
+    # lets see what OS type we have
+    if ($^O eq 'linux')
     {
-      ($locale,$encoding) = split(/\./,$locale);
+        #$hostName = `hostname`;
+        #my $tmpHost = `host $hostName`;
+        #if ($tmpHost =~ m/has address/){
+        #  ($hostName,my $trash) = split(/has address/,$tmpHost);
+        #  $hostName = trim($hostName);
+        #}
+        # Get the CPU info
+        $cpu = trim(`cat /proc/cpuinfo | grep -i "model name" | head -n 1 | cut -b 14-`);
+        my $numOfP = trim(`cat /proc/cpuinfo | grep processor |wc -l`);
+        $cpu ="$numOfP"."x"."$cpu";
+
+        #try to get OS Information
+        if (-e "/etc/SuSE-release"){$osVer=`cat /etc/SuSE-release  |head -n 1`;}
+        elsif (-e "/etc/redhat-release"){$osVer=`cat /etc/redhat-release  |head -n 1`;}
+        elsif (-e "/etc/debian_version"){$osVer=`cat /etc/debian_version  |head -n 1`;}
+        else {$osVer="linux-unknown";}
+        $osVer=trim($osVer);
+        if (-e "/etc/SuSE-release"){$patch=`cat /etc/SuSE-release  |tail -n 1`;}
+        elsif (-e "/etc/redhat-release"){$patch=`cat /etc/redhat-release  |tail -n 1`;}
+        elsif (-e "/etc/debian_version"){$patch=`cat /etc/debian_version  |tail -n 1`;}
+        else {$patch="unknown";}
+        (my $trash, $patch) = split(/=/,$patch);
+        $osType="Linux";
+        $arch=trim(`uname -m`);
+        ($trash, $bit) = split(/_/,$arch);
+        $kernel=trim(`uname -r`);
+
+        #Memory
+        $memory = trim(`cat /proc/meminfo | grep -i memtotal`);
+        $memory =~ s/MemTotal: //;
+        ($memory, $trash) =  split(/k/,$memory);
+        $memory = trim(`cat /proc/meminfo |grep -i memtotal`);
+        $memory =~ /MemTotal:\s*(\d+)/;
+        $memory = sprintf("%.2f",($1/1024000))."GB";
+
+        #locale
+        if (defined ($locale=`locale |grep LC_CTYPE| cut -b 10-`))
+        {
+            ($locale,$encoding) = split(/\./,$locale);
+        }
+        else
+        {
+            $locale   = "UNKNOWN";
+            $encoding = "UNKNOWN";
+        }
+
+        #TimeZone
+        $tz = trim(`date +%Z`);
+    }
+    elsif($^O eq 'solaris')
+    {
+        #$hostName = `hostname`;
+        #my $tmpHost = `host $hostName`;
+        #if ($tmpHost =~ m/has address/){
+        #  ($hostName,my $trash) = split(/has address/,$tmpHost);
+        #  $hostName = trim($hostName);
+        #}
+        # Get the CPU info
+        my $tmpVar = `/usr/sbin/psrinfo -v | grep -i "operates" | head -1`;
+        ($cpu, my $speed) = split(/processor operates at/,$tmpVar);
+        $cpu =~ s/The//;
+        $speed =~ s/MHz//;
+        $cpu = trim($cpu);
+        $speed = trim($speed);
+        if ($speed => 1000)
+        {
+            $speed = $speed/1000;
+            $speed = "$speed"."GHz";
+        }
+        else
+        {
+            $speed = "$speed"."MHz";
+        }
+
+        my $numOfP = `/usr/sbin/psrinfo -v | grep -i "operates" |wc -l`;
+        $numOfP = trim($numOfP);
+        $cpu ="$numOfP"."x"."$cpu"."$speed";
+
+        #try to get OS Information
+        ($osType,$osVer,$arch,$kernel) = split (/ /, trim(`uname -srpm`));
+
+        #Memory
+        $memory = `/usr/sbin/prtconf | grep Memory`;
+        $memory =~ s/Memory size://;
+        $memory =~ s/Megabytes//;
+        $memory = trim($memory);
+        $memory = $memory/1024;
+        ($memory, my $trash) = split(/\./,$memory);
+        $memory = "$memory"."GB";
+
+        #locale
+        if (defined ($locale=`locale |grep LC_CTYPE| cut -b 10-`))
+        {
+            ($locale,$encoding) = split(/\./,$locale);
+        }
+        else
+        {
+            $locale   = "UNKNOWN";
+            $encoding = "UNKNOWN";
+        }
+
+        #TimeZone
+        $tz = trim(`date +%Z`);
+    }
+    elsif($^O eq 'cygwin' || $^O eq 'MSWin32' || $^O eq 'MSWin64')
+    {
+        #$hostName = `hostname`;
+        my @tmpData;
+        if ($^O eq 'cygwin')
+        {
+            # Assuming cygwin on Windows at this point
+            @tmpData = `cmd /c systeminfo 2>&1`;
+        }
+        else
+        {
+            # Assuming Windows at this point
+            @tmpData = `systeminfo 2>&1`;
+        }
+
+        if ($? != 0)
+        {
+            carp "systeminfo command failed with: $?";
+            $cpu    = "UNKNOWN";
+            $osType = "UNKNOWN";
+            $osVer  = "UNKNOWN";
+            $arch   = "UNKNOWN";
+            $kernel = "UNKNOWN";
+            $memory = "UNKNOWN";
+            $locale = "UNKNOWN";
+            $tz     = "UNKNOWN";
+        }
+        else
+        {
+            $kernel = "UNKOWN";
+            my $cpuFlag = 0;
+            # Time to get busy and grab what we need.
+            foreach my $line (@tmpData)
+            {
+                if ($cpuFlag == 1)
+                {
+                    (my $numP, $cpu) = split(/\:/,$line);
+                    $numP = trim($numP);
+                    (my $trash, $numP) = split(/\[/,$numP);
+                    ($numP, $trash) = split(/\]/,$numP);
+                    $cpu = "$numP"."$cpu";
+                    $cpu = trim($cpu);
+                    $cpuFlag=0;
+                }
+                elsif ($line =~ /OS Name:\s+(.*?)\s*$/)
+                {
+                    $osType = $1;
+                }
+                elsif ($line =~ /^OS Version:\s+(.*?)\s*$/)
+                {
+                    $osVer = $1;
+                }
+                elsif ($line =~ /System type:\s/i)
+                {
+                    (my $trash, $arch) = split(/\:/,$line);
+                    ($arch,$trash) = split(/\-/,$arch);
+                    $arch = trim($arch);
+                }
+                elsif ($line =~ /^Processor/)
+                {
+                    $cpuFlag = 1;
+                    next;
+                }
+                elsif ($line =~ /^Total Physical Memory:\s+(.*?)\s*$/)
+                {
+                    $memory = $1;
+                }
+                elsif ($line =~ /Locale:/)
+                {
+                    (my $trash, $locale) = split(/\:/,$line);
+                    ($locale, $trash) = split(/\;/,$locale);
+                    $locale = trim($locale);
+                }
+                elsif ($line =~ /Time Zone:\s+(.*?)\s*$/)
+                {
+                    $tz = $1;
+                }
+            }
+        }
     }
     else
     {
-      $locale   = "UNKNOWN";
-      $encoding = "UNKNOWN";
+        confess "\n\nUnable to figure out OS!!\n\n";
     }
 
-    #TimeZone
-    $tz = trim(`date +%Z`);
-  }
-  elsif($^O eq 'solaris')
-  {
-    #$hostName = `hostname`;
-    #my $tmpHost = `host $hostName`;
-    #if ($tmpHost =~ m/has address/){
-    #  ($hostName,my $trash) = split(/has address/,$tmpHost);
-    #  $hostName = trim($hostName);
-    #}
-    # Get the CPU info
-    my $tmpVar = `/usr/sbin/psrinfo -v | grep -i "operates" | head -1`;
-    ($cpu, my $speed) = split(/processor operates at/,$tmpVar);
-    $cpu =~ s/The//;
-    $speed =~ s/MHz//;
-    $cpu = trim($cpu);
-    $speed = trim($speed);
-    if ($speed => 1000)
+    if ($DEBUG)
     {
-      $speed = $speed/1000;
-      $speed = "$speed"."GHz";
+        print "cpu    = $cpu\n";
+        print "os     =  $osType\n";
+        print "OS ver = $osVer\n";
+        print "Arch   = $arch\n";
+        print "Kernel = $kernel\n";
+        print "memory = $memory\n";
+        print "locale = $locale\n";
+        print "Timezone = $tz\n";
     }
-    else
-    {
-      $speed = "$speed"."MHz";
-    }
-
-    my $numOfP = `/usr/sbin/psrinfo -v | grep -i "operates" |wc -l`;
-    $numOfP = trim($numOfP);
-    $cpu ="$numOfP"."x"."$cpu"."$speed";
-
-    #try to get OS Information
-     ($osType,$osVer,$arch,$kernel) = split (/ /, trim(`uname -srpm`));
-
-    #Memory
-    $memory = `/usr/sbin/prtconf | grep Memory`;
-    $memory =~ s/Memory size://;
-    $memory =~ s/Megabytes//;
-    $memory = trim($memory);
-    $memory = $memory/1024;
-    ($memory, my $trash) = split(/\./,$memory);
-    $memory = "$memory"."GB";
-
-    #locale
-    if (defined ($locale=`locale |grep LC_CTYPE| cut -b 10-`))
-    {
-      ($locale,$encoding) = split(/\./,$locale);
-    }
-    else
-    {
-      $locale   = "UNKNOWN";
-      $encoding = "UNKNOWN";
-    }
-
-    #TimeZone
-    $tz = trim(`date +%Z`);
-  }
-  elsif($^O eq 'cygwin' || $^O eq 'MSWin32' || $^O eq 'MSWin64')
-  {
-    #$hostName = `hostname`;
-    my @tmpData;
-    if ($^O eq 'cygwin')
-    {
-      # Assuming cygwin on Windows at this point
-      @tmpData = `cmd /c systeminfo 2>&1`;
-    }
-    else
-    {
-      # Assuming Windows at this point
-      @tmpData = `systeminfo 2>&1`;
-    }
-
-    if ($? != 0)
-    {
-      carp "systeminfo command failed with: $?";
-      $cpu    = "UNKNOWN";
-      $osType = "UNKNOWN";
-      $osVer  = "UNKNOWN";
-      $arch   = "UNKNOWN";
-      $kernel = "UNKNOWN";
-      $memory = "UNKNOWN";
-      $locale = "UNKNOWN";
-      $tz     = "UNKNOWN";
-    }
-    else
-    {
-      $kernel = "UNKOWN";
-      my $cpuFlag = 0;
-      # Time to get busy and grab what we need.
-      foreach my $line (@tmpData)
-      {
-        if ($cpuFlag == 1)
-        {
-     	  (my $numP, $cpu) = split(/\:/,$line);
-     	  $numP = trim($numP);
-     	  (my $trash, $numP) = split(/\[/,$numP);
-     	  ($numP, $trash) = split(/\]/,$numP);
-      	  $cpu = "$numP"."$cpu";
-      	  $cpu = trim($cpu);
-     	  $cpuFlag=0;
-        }
-        elsif ($line =~ /OS Name:\s+(.*?)\s*$/)
-        {
-          $osType = $1;
-        }
-        elsif ($line =~ /^OS Version:\s+(.*?)\s*$/)
-        {
-          $osVer = $1;
-        }
-        elsif ($line =~ /System type:\s/i)
-        {
-     	  (my $trash, $arch) = split(/\:/,$line);
-     	  ($arch,$trash) = split(/\-/,$arch);
-     	  $arch = trim($arch);
-        }
-        elsif ($line =~ /^Processor/)
-        {
-      	  $cpuFlag = 1;
-      	  next;
-        }
-        elsif ($line =~ /^Total Physical Memory:\s+(.*?)\s*$/)
-        {
-      	  $memory = $1;
-        }
-        elsif ($line =~ /Locale:/)
-        {
-      	  (my $trash, $locale) = split(/\:/,$line);
-      	  ($locale, $trash) = split(/\;/,$locale);
-      	  $locale = trim($locale);
-        }
-        elsif ($line =~ /Time Zone:\s+(.*?)\s*$/)
-        {
-      	  $tz = $1;
-        }
-      }
-    }
-  }
-  else
-  {
-    confess "\n\nUnable to figure out OS!!\n\n";
-  }
-
-  if ($DEBUG)
-  {
-    print "cpu    = $cpu\n";
-    print "os     =  $osType\n";
-    print "OS ver = $osVer\n";
-    print "Arch   = $arch\n";
-    print "Kernel = $kernel\n";
-    print "memory = $memory\n";
-    print "locale = $locale\n";
-    print "Timezone = $tz\n";
-  }
 }
 
 sub trim($)
 {
-	my $string = shift;
-	$string =~ s/^\s+//;
-	$string =~ s/\s+$//;
-	return $string;
+    my $string = shift;
+    $string =~ s/^\s+//;
+    $string =~ s/\s+$//;
+    return $string;
 }
 
 1;
