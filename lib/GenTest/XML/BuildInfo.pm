05033229cb0254241a72e1fde66e3c72d9dce9ed
John H. Embretsen <john.embretsen@sun.com>
2009-11-17 15:54:37 +0100

    XML: Fix bad removal of braces due to automatic formatting (after tabs-->space switch) and some other wrongdoings.

--- lib/GenTest/XML/BuildInfo.pm
+++ lib/GenTest/XML/BuildInfo.pm
@@ -25,29 +25,31 @@ sub new {
 
     $buildinfo->[BUILDINFO_SERVERS] = [];
 
-    foreach my $id (0..$#{$buildinfo->[BUILDINFO_DSNS]}) {
-    my $dsn = $buildinfo->[BUILDINFO_DSNS]->[$id];
-    next if $dsn eq '';
-    my $dbh = DBI->connect($dsn);
+    foreach my $id (0..$#{$buildinfo->[BUILDINFO_DSNS]})
+    {
+        my $dsn = $buildinfo->[BUILDINFO_DSNS]->[$id];
+        next if $dsn eq '';
+        my $dbh = DBI->connect($dsn);
 
-    my $server;
+        my $server;
 
-    $server->[BUILDINFO_SERVER_VERSION] = $dbh->selectrow_array('SELECT @@version');
-    $server->[BUILDINFO_SERVER_PACKAGE] = $dbh->selectrow_array('SELECT @@version_comment');
-    $server->[BUILDINFO_SERVER_BIT] = $dbh->selectrow_array('SELECT @@version_compile_machine');
-    $server->[BUILDINFO_SERVER_PATH] = $dbh->selectrow_array('SELECT @@basedir');
-    $server->[BUILDINFO_SERVER_VARIABLES] = [];
+        $server->[BUILDINFO_SERVER_VERSION] = $dbh->selectrow_array('SELECT @@version');
+        $server->[BUILDINFO_SERVER_PACKAGE] = $dbh->selectrow_array('SELECT @@version_comment');
+        $server->[BUILDINFO_SERVER_BIT] = $dbh->selectrow_array('SELECT @@version_compile_machine');
+        $server->[BUILDINFO_SERVER_PATH] = $dbh->selectrow_array('SELECT @@basedir');
+        $server->[BUILDINFO_SERVER_VARIABLES] = [];
 
-    my $sth = $dbh->prepare("SHOW VARIABLES");
-    $sth->execute();
-    while (my ($name, $value) = $sth->fetchrow_array()) {
-        push @{$server->[BUILDINFO_SERVER_VARIABLES]}, [ $name , $value ];
+        my $sth = $dbh->prepare("SHOW VARIABLES");
+        $sth->execute();
+        while (my ($name, $value) = $sth->fetchrow_array()) {
+            push @{$server->[BUILDINFO_SERVER_VARIABLES]}, [ $name , $value ];
+        }
+        $sth->finish();
+
+        $dbh->disconnect();
+
+        $buildinfo->[BUILDINFO_SERVERS]->[$id] = $server;
     }
-    $sth->finish();
-
-    $dbh->disconnect();
-
-    $buildinfo->[BUILDINFO_SERVERS]->[$id] = $server;
 
     return $buildinfo;
 }
@@ -66,41 +68,45 @@ sub xml {
     $writer->dataElement('name','mysql');
     $writer->startTag('builds');
 
-    foreach my $id (0..$#{$buildinfo->[BUILDINFO_DSNS]}) {
-    my $server = $buildinfo->[BUILDINFO_SERVERS]->[$id];
-    next if not defined $server;
+    foreach my $id (0..$#{$buildinfo->[BUILDINFO_DSNS]})
+    {
+        my $server = $buildinfo->[BUILDINFO_SERVERS]->[$id];
+        next if not defined $server;
 
-    $writer->startTag('build', id => $id);
-    $writer->dataElement('version', $server->[BUILDINFO_SERVER_VERSION]);
-    $writer->dataElement('package', $server->[BUILDINFO_SERVER_PACKAGE]);
-    $writer->dataElement('bit', $server->[BUILDINFO_SERVER_BIT]);
-    $writer->dataElement('path', $server->[BUILDINFO_SERVER_PATH]);
-    # <compile_options>
-    $writer->endTag('build');
+        $writer->startTag('build', id => $id);
+        $writer->dataElement('version', $server->[BUILDINFO_SERVER_VERSION]);
+        $writer->dataElement('package', $server->[BUILDINFO_SERVER_PACKAGE]);
+        $writer->dataElement('bit', $server->[BUILDINFO_SERVER_BIT]);
+        $writer->dataElement('path', $server->[BUILDINFO_SERVER_PATH]);
+        # <compile_options>
+        $writer->endTag('build');
+    }
 
 
     $writer->endTag('builds');
 
     $writer->startTag('binaries');
 
-    foreach my $id (0..$#{$buildinfo->[BUILDINFO_DSNS]}) {
-    my $server = $buildinfo->[BUILDINFO_SERVERS]->[$id];
-    next if not defined $server;
+    foreach my $id (0..$#{$buildinfo->[BUILDINFO_DSNS]})
+    {
+        my $server = $buildinfo->[BUILDINFO_SERVERS]->[$id];
+        next if not defined $server;
 
-    $writer->startTag('binary');
-    $writer->dataElement('name', 'mysqld');
-    $writer->startTag('commandline_options');
+        $writer->startTag('binary');
+        $writer->dataElement('name', 'mysqld');
+        $writer->startTag('commandline_options');
 
-    foreach my $option (@{$server->[BUILDINFO_SERVER_VARIABLES]}) {
-        $writer->startTag('option');
-        $writer->dataElement('name', $option->[0]);
-        $writer->dataElement('value', $option->[1]);
-        $writer->endTag('option');
+        foreach my $option (@{$server->[BUILDINFO_SERVER_VARIABLES]})
+        {
+            $writer->startTag('option');
+            $writer->dataElement('name', $option->[0]);
+            $writer->dataElement('value', $option->[1]);
+            $writer->endTag('option');
+        }
+
+        $writer->endTag('commandline_options');
+        $writer->endTag('binary');
     }
-
-    $writer->endTag('commandline_options');
-    $writer->endTag('binary');
-
 
     $writer->endTag('binaries');
     $writer->endTag('product');
