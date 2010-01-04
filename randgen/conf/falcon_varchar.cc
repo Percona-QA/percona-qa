dd3e7dabee661b954a88d7bef130600ac259b92c
John H. Embretsen <john.embretsen@sun.com>
2010-01-04 15:07:22 +0100

    Replace all instances of --skip-safemalloc with --loose-skip-safemalloc. This is to counteract a recent change in the server code, specifically revision serg@mysql.com-20091222093556-8aocy9n92pdp0rwr which would cause server startup issues if this mysqld option was used without --loose in some code branches.

--- randgen/conf/falcon_varchar.cc
+++ randgen/conf/falcon_varchar.cc
@@ -5,7 +5,7 @@ $combinations = [
 		--mysqld=--loose-falcon-lock-wait-timeout=1
 		--mysqld=--loose-innodb-lock-wait-timeout=1
 		--mysqld=--log-output=none
-		--mysqld=--skip-safemalloc
+		--mysqld=--loose-skip-safemalloc
 	'],
 	[
 		'--mysqld=--falcon-page-size=2K',
