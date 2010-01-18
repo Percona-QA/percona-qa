0cbecf3fa8d90a1a29a1eb80e87816ca8f25415a
Bernt M. Johnsen <bernt.johnsen@sun.com>
2010-01-18 15:52:29 +0100

    Centralized handling of error message supression in separate process

---
+++ randgen/lib/GenTest/ErrorFilter.pm
@@ -0,0 +1,37 @@
+package GenTest::ErrorFilter;
+
+use GenTest;
+use GenTest::IPC::Channel;
+
+use strict;
+
+sub new {
+    my $class = shift;
+    my $self = {};
+
+    $self->{CHANNEL} = shift;
+    $self->{CACHE} = {};
+
+    bless ($self, $class);
+
+    return $self;
+}
+
+sub run {
+    my ($self,@args) = @_;
+    $self->{CHANNEL}->reader;
+    while (1) {
+        my $msg = $self->{CHANNEL}->recv;
+        if (defined $msg) {
+            my ($query, $err, $errstr) = @$msg;
+            if (not defined $self->{CACHE}->{$errstr}) {
+                say("Query: $query failed: $err $errstr. Further errors of this kind will be suppressed.");
+            }
+            $self->{CACHE}->{$errstr}++;
+        }
+        last if !$self->{CHANNEL}->more;
+    }
+    $self->{CHANNEL}->close;
+}
+
+1;
