adc7411ea4968bc6ab4bc010466e04258b43654d
Bernt M. Johnsen <bernt.johnsen@sun.com>
2010-02-17 10:13:22 +0100

    Handle spaces in files

--- randgen/unit/TestScripts.pm
+++ randgen/unit/TestScripts.pm
@@ -81,7 +81,17 @@ sub test_runall {
     ## This test requires RQG_MYSQL_BASE to point to a in source Mysql database
     if ($ENV{RQG_MYSQL_BASE}) {
         $ENV{LD_LIBRARY_PATH}=$ENV{RQG_MYSQL_BASE}."/libmysql/.libs";
-        my $status = system("perl ./runall.pl --grammar=conf/example.yy --gendata=conf/example.zz --basedir=".$ENV{RQG_MYSQL_BASE});
+        my $status = system("perl ./runall.pl --grammar=conf/example.yy --gendata=conf/example.zz --queries=1 --threads=1 --basedir=".$ENV{RQG_MYSQL_BASE});
+        $self->assert_equals(0, $status);
+    }
+}
+
+sub test_runall_new {
+    my $self = shift;
+    ## This test requires RQG_MYSQL_BASE to point to a in source Mysql database
+    if ($ENV{RQG_MYSQL_BASE}) {
+        $ENV{LD_LIBRARY_PATH}=$ENV{RQG_MYSQL_BASE}."/libmysql/.libs";
+        my $status = system("perl ./runall-new.pl --mtr-build-thread=1212 --grammar=conf/example.yy --gendata=conf/example.zz --queries=1 --threads=1 --basedir=".$ENV{RQG_MYSQL_BASE});
         $self->assert_equals(0, $status);
     }
 }
