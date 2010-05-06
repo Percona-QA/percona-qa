0d96856757e8dc7a1652461451391550d3b17e64
John H. Embretsen <john.embretsen@sun.com>
2010-05-06 10:03:33 +0200

    Add a Heartbeat reporter printing a message every 20 mins, and add this to range_access test in pb2gentest.pl. This to avoid 30-minute timeouts enforced by Pushbuild.

--- randgen/pb2gentest.pl
+++ randgen/pb2gentest.pl
@@ -650,6 +650,7 @@ if ($test =~ m{falcon_.*transactions}io ) {
         --gendata='.$conf.'/optimizer/range_access.zz
         --grammar='.$conf.'/optimizer/range_access.yy
         --duration=2700
+        --reporters=Shutdown,ErrorLog,Backtrace,Heartbeat
 	';
 } elsif ($test =~ m{^opt_subquery}io ) {
     # Produces large and time consuming queries, so we use a larger than default 
