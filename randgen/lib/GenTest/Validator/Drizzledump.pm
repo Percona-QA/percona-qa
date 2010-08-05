39c7674406d9e181803006e050083e598c6c1beb
Patrick Crews <gleebix@gmail.com>
2010-08-05 18:12:21 -0400

    Updated drizzledump validator to only work on the end of a multi-query statement via a signal query - SELECT 1.  Will look into using query comments to be less hackish.  Still need to get comparison and attendant meta-data queries working

--- randgen/lib/GenTest/Validator/Drizzledump.pm
+++ randgen/lib/GenTest/Validator/Drizzledump.pm
@@ -32,62 +32,62 @@ sub validate {
 	my ($validator, $executors, $results) = @_;
 
         my $query_value = $results->[0]->[0] ;
-
-        if ($query_value == "SELECT 1")
+        say ("$query_value");
+        if ($query_value eq ' SELECT 1')
         {
-        say ("Woohoo!!  $query_value"); 
-        # do some setup and whatnot
-	my @files;
-	my $port = '9306';
+          say ("Woohoo!!  $query_value"); 
+          # do some setup and whatnot
+          my @files;
+	  my $port = '9306';
 
-	my $database = 'drizzledump_db' ;
-        my $restored_database = $database."_restore" ;
-        my @basedir = $executors->[0]->dbh()->selectrow_array('SELECT @@basedir') ;
-        # little kludge to get the proper basedir if drizzle was started via test-run.pl
-        # such a situation sets basedir to the drizzle/tests directory and can
-        # muck up efforts to get to the client directory
-        my @basedir_split = split(/\//, @basedir->[0]) ;
-        if (@basedir_split[-1] == 'tests')
-        {
-          pop(@basedir_split); 
-          @basedir = join('/',@basedir_split);
-        }
+	  my $database = 'drizzledump_db' ;
+          my $restored_database = $database."_restore" ;
+          my @basedir = $executors->[0]->dbh()->selectrow_array('SELECT @@basedir') ;
+          # little kludge to get the proper basedir if drizzle was started via test-run.pl
+          # such a situation sets basedir to the drizzle/tests directory and can
+          # muck up efforts to get to the client directory
+          my @basedir_split = split(/\//, @basedir->[0]) ;
+          if (@basedir_split[-1] == 'tests')
+          {
+            pop(@basedir_split); 
+            @basedir = join('/',@basedir_split);
+          }
        
-        my $drizzledump = @basedir->[0].'/client/drizzledump' ;
-        my $drizzle_client = @basedir->[0].'/client/drizzle' ;
+          my $drizzledump = @basedir->[0].'/client/drizzledump' ;
+          my $drizzle_client = @basedir->[0].'/client/drizzle' ;
 
         
-        # dump our database under test
-	my $drizzledump_file = tmpdir()."/dump_".$$."_".$port.".sql";
-        say("drizzledump file is:  $drizzledump_file");
-	my $drizzledump_result = system("$drizzledump --compact --order-by-primary --host=127.0.0.1 --port=$port --user=root $database  > $drizzledump_file") ;
-	return STATUS_UNKNOWN_ERROR if $drizzledump_result > 0 ;
+          # dump our database under test
+	  my $drizzledump_file = tmpdir()."/dump_".$$."_".$port.".sql";
+          say("drizzledump file is:  $drizzledump_file");
+	  my $drizzledump_result = system("$drizzledump --compact --order-by-primary --host=127.0.0.1 --port=$port --user=root $database  > $drizzledump_file") ;
+	  return STATUS_UNKNOWN_ERROR if $drizzledump_result > 0 ;
 
-        # restore test database from dumpfile
-        say("Restoring from dumpfile...");
-        my $drizzle_restore_result = system("$drizzle_client --host=127.0.0.1 --port=$port --user=root $restored_database <  $drizzledump_file") ;
-        return STATUS_UNKNOWN_ERROR if $drizzle_restore_result > 0 ;
+          # restore test database from dumpfile
+          say("Restoring from dumpfile...");
+          my $drizzle_restore_result = system("$drizzle_client --host=127.0.0.1 --port=$port --user=root $restored_database <  $drizzledump_file") ;
+          return STATUS_UNKNOWN_ERROR if $drizzle_restore_result > 0 ;
 
-        # compare original + restored databases
-        # 1) We get the list of columns for the original table (use original as it is the standard)
-        # 2) Use said column list in the comparison query (will report on any rows / values not in both tables)  
-        # 3) Check the rows returned
-        # TODO:  Loop through all tables in the database.  Current testing / experiment stage only wanting one table at a time
+          # compare original + restored databases
+          # 1) We get the list of columns for the original table (use original as it is the standard)
+          # 2) Use said column list in the comparison query (will report on any rows / values not in both tables)  
+          # 3) Check the rows returned
+          # TODO:  Loop through all tables in the database.  Current testing / experiment stage only wanting one table at a time
 
-        my $get_table_names = " SELECT DISTINCT(table_name) ".
-                               " FROM data_dictionary.tables ". 
-                               " WHERE table_schema = '".$database."'" ;
+          my $get_table_names =  " SELECT DISTINCT(table_name) ".
+                                 " FROM data_dictionary.tables ". 
+                                 " WHERE table_schema = '".$database."'" ;
 
-        my $table_name = $executors->[0]->dbh()->selectall_arrayref($get_table_names) ;
+          my $table_name = $executors->[0]->dbh()->selectall_arrayref($get_table_names) ;
   
-        my $get_table_columns = " SELECT column_name FROM data_dictionary.tables INNER JOIN ".
+          my $get_table_columns = " SELECT column_name FROM data_dictionary.tables INNER JOIN ".
                                   " DATA_DICTIONARY.columns USING (table_schema, table_name) ".
                                   " WHERE table_schema = '".$database."'". 
                                   " AND table_name = '".$table_name."'"   ;
 
-        my $table_columns = $executors->[0]->dbh()->selectall_arrayref($get_table_columns) ; 
+          my $table_columns = $executors->[0]->dbh()->selectall_arrayref($get_table_columns) ; 
 
-        my $compare_orig_and_restored =     "SELECT MIN(TableName) as TableName, a, b ".
+          my $compare_orig_and_restored =     "SELECT MIN(TableName) as TableName, a, b ".
                                  " FROM ".
                                  " ( ".
                                  "   SELECT 'Table A' as TableName, a, b ".
@@ -99,20 +99,20 @@ sub validate {
                                  " GROUP BY a, b ".
                                  " HAVING COUNT(*) = 1 ORDER BY `pk` " ;
 
-        my $diff_result = $executors->[0]->dbh()->selectall_arrayref($compare_orig_and_restored) ;
+          my $diff_result = $executors->[0]->dbh()->selectall_arrayref($compare_orig_and_restored) ;
 
-	if ($diff_result ) {
+	  if ($diff_result ) {
 		say("Differences between the two databases were found after dump/restore from file ".$drizzledump_file );
                 say("Comparison query: ".$compare_orig_and_restored ) ;
                 say("Returned:  ".$diff_result) ;
 		return STATUS_DATABASE_CORRUPTION ;
-	} else {
+	  } else {
 		unlink($drizzledump_file);
 		return STATUS_OK;
 	}
 
   }
-
+say("I'm lonely");
 return STATUS_OK ;
 }
 
