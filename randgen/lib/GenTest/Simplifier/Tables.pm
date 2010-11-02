31526245b0a72fbc43afaac632ea4d2df114d240
eve <philips@eve>
2010-11-02 09:51:32 +0200

    Rename tables as t1 , t2 , t3, etc when dumping a test case

--- randgen/lib/GenTest/Simplifier/Tables.pm
+++ randgen/lib/GenTest/Simplifier/Tables.pm
@@ -51,18 +51,21 @@ sub simplify {
 
 	my $orig_database = $simplifier->[SIMPLIFIER_ORIG_DATABASE];
 	my $new_database = $simplifier->[SIMPLIFIER_NEW_DATABASE];
+	my $new_query = $initial_query;
 
-	my @tables_named = $initial_query =~ m{(table[a-z0-9_]*)}sgio;
+	my @tables_named = $initial_query =~ m{(table[a-z0-9_]+)}sgio;
 	my @tables_quoted = $initial_query =~ m{`(.*?)`}sgio;
-	my @tables_letters = $initial_query =~ m{[ `]([A-Z]{1,3})[ `]}sgo;
+	my @tables_letters = $initial_query =~ m{[ `]([A-Z]|AA|BB|CC|DD|EE|FF|AAA|BBB|CCC|DDD|EEE|FFF)[ `]}sgo;
 	
 	my @participating_tables = (@tables_named, @tables_quoted, @tables_letters);
+
 	my %participating_tables;
+	map {$participating_tables{$_} = 1 } @participating_tables;
 
 	my @fields_quoted = $initial_query =~ m{`(.*?)`}sgio;
 	my @fields_named = $initial_query =~ m{((?:char|varchar|int|set|enum|blob|date|time|datetime|pk)(?:`|\s|_key|_nokey))}sgo;
 
-	my @participating_fields = (@fields_quoted, @fields_named);
+	my @participating_fields = (@fields_quoted, map {'col_'.$_} @fields_named);
 	my %participating_fields;
 	map { $participating_fields{$_} = 1 } @participating_fields;
 
@@ -71,45 +74,50 @@ sub simplify {
 	$dbh->do("DROP DATABASE IF EXISTS $new_database");
 	$dbh->do("CREATE DATABASE $new_database");
 
-	foreach my $participating_table (@participating_tables) {
+	my $table_index = 0;
+
+	foreach my $participating_table (keys %participating_tables) {
 		my ($table_exists) = $dbh->selectrow_array("SHOW TABLES IN $orig_database LIKE '$participating_table'");
 		next if not defined $table_exists;
-		next if exists $participating_tables{$participating_table};
-		$participating_tables{$participating_table} = 1;
-		$dbh->do("CREATE TABLE $new_database . $participating_table LIKE $orig_database . `$participating_table`");
-		$dbh->do("INSERT INTO $new_database . $participating_table SELECT * FROM $orig_database . `$participating_table`");
 
-        ## Find all fields in table
-		my $actual_fields = $dbh->selectcol_arrayref("SHOW FIELDS FROM `$participating_table` IN $new_database");
-        ## Find indexed fields in table
-        my %indices;
-        map {$indices{$_->[4]}=$_->[2]} @{$dbh->selectall_arrayref("SHOW INDEX FROM `$participating_table` IN $new_database")};
+		my $new_table_name = 't'.++$table_index;
+		$dbh->do("CREATE TABLE $new_database . $new_table_name LIKE $orig_database . `$participating_table`");
+		$dbh->do("INSERT INTO $new_database . $new_table_name SELECT * FROM $orig_database . `$participating_table`");
+		$new_query =~ s{`$participating_table`}{`$new_table_name`}sg;
+		$new_query =~ s{ $participating_table }{ $new_table_name }sg;
+		$new_query =~ s{ $participating_table$}{ $new_table_name}sg;
 
-        ## Calculate which fields to keep
-        my %keep;
+	        ## Find all fields in table
+			my $actual_fields = $dbh->selectcol_arrayref("SHOW FIELDS FROM `$new_table_name` IN $new_database");
+	        ## Find indexed fields in table
+	        my %indices;
+	        map {$indices{$_->[4]}=$_->[2]} @{$dbh->selectall_arrayref("SHOW INDEX FROM `$new_table_name` IN $new_database")};
+
+	        ## Calculate which fields to keep
+	        my %keep;
 		foreach my $actual_field (@$actual_fields) {
-            if (not exists $participating_fields{$actual_field}) {
-                ## Not used field, but may be part of multi-column index where other column is used
-                if (exists $indices{$actual_field}) {
-                    foreach my $x (keys %indices) {
-                        $keep{$actual_field} = 1 
-                            if (exists $participating_fields{$x}) and
-                            ($indices{$x} eq $indices{$actual_field});
-                    }
-                }
-            } else {
-                ## Explicitely used field
-                $keep{$actual_field}=1;
-            }
-        }
+	            if (not exists $participating_fields{$actual_field}) {
+	                ## Not used field, but may be part of multi-column index where other column is used
+	                if (exists $indices{$actual_field}) {
+	                    foreach my $x (keys %indices) {
+	                        $keep{$actual_field} = 1 
+	                            if (exists $participating_fields{$x}) and
+	                            ($indices{$x} eq $indices{$actual_field});
+	                    }
+	                }
+	            } else {
+	                ## Explicitely used field
+	                $keep{$actual_field}=1;
+	            }
+	        }
 
-        ## Remove the fields we do not want to keep
+	        ## Remove the fields we do not want to keep
 		foreach my $actual_field (@$actual_fields) {
-			$dbh->do("ALTER TABLE $new_database . `$participating_table` DROP COLUMN `$actual_field`") if not $keep{$actual_field};
-        }
+			$dbh->do("ALTER TABLE $new_database . `$new_table_name` DROP COLUMN `$actual_field`") if not $keep{$actual_field};
+	        }
 	}
-
-	return keys %participating_tables;
+	
+	return [ map { 't'.$_ } (1..$table_index) ], $new_query;
 }
 
 1;
