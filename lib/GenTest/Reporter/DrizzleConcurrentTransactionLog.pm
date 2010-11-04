434e0178f8b12baf77ba0f53f3398e8389dfa14c
Patrick Crews <gleebix@gmail.com>
2010-11-04 17:04:41 -0400

    Fix of typo in the translog reporter

--- lib/GenTest/Reporter/DrizzleConcurrentTransactionLog.pm
+++ lib/GenTest/Reporter/DrizzleConcurrentTransactionLog.pm
@@ -15,7 +15,7 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
 # USA
 
-package GenTest::Reporter::DrizzleConcurrentTransactionLog1;
+package GenTest::Reporter::DrizzleConcurrentTransactionLog;
 
 require Exporter;
 @ISA = qw(GenTest::Reporter);
