82ca42f7aa220dc036c76d91b2708acd5769a44a
Patrick Crews <gleebix@gmail.com>
2011-02-08 15:51:16 -0500

    Update to DrizzleInnoTrxLog

--- randgen/lib/GenTest/Reporter/DrizzleInnoTrxLog.pm
+++ randgen/lib/GenTest/Reporter/DrizzleInnoTrxLog.pm
@@ -15,7 +15,7 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
 # USA
 
-package GenTest::Reporter::DrizzleTransactionLog;
+package GenTest::Reporter::DrizzleInnoTrxLog;
 
 require Exporter;
 @ISA = qw(GenTest::Reporter);
