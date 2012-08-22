4109183a80b715ab3cdab52ea087d2f7d4d4afab
Roel Van de Paar <roel.vandepaar@percona.com>
2012-08-22 15:00:21 +1000

    Adding Percona QA grammars

---
+++ randgen/conf/percona_qa/percona_qa.cc
@@ -0,0 +1,61 @@
+# Copyright (c) 2012 Oracle and/or its affiliates. All rights reserved.
+# Use is subject to license terms.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
+# USA
+
+# Certain parts (c) Percona Inc
+
+$combinations=
+[
+ [
+  '--seed=random',
+  '--seed=epoch5'
+ ],['
+  --duration=1200 --querytimeout=60
+  --short_column_names --sqltrace
+  --reporter=Shutdown,Backtrace,QueryTimeout,ErrorLog,ErrorLogAlarm
+  --mysqld=--log-output=none --mysqld=--sql_mode=ONLY_FULL_GROUP_BY'
+ ],[
+  '',
+  '--mysqld=--slow_query_log'
+ ],[
+  '',
+  '--mysqld=--userstat'
+ ],[
+  '',
+  '--mysqld=--innodb_lazy_drop_table=1'
+ ],[
+  '',
+  '--mysqld=--innodb_file_per_table=1',
+  '--mysqld=--innodb_file_per_table=1 --innodb-file-format=barracuda'
+ ],[
+  '--threads=1',
+  '--threads=10',
+  '--threads=20'
+ ],[
+  '',
+  '--views',
+  '--notnull',
+  '--views --notnull',
+  '--validator=Transformer',
+  '--notnull --validator=Transformer',
+  '--views --notnull --validator=Transformer'
+ ],[
+  '--basedir=/percona-server/Percona-Server-5.5.27-rel28.0-288.Linux.x86_64',
+  '--basedir=/percona-server/Percona-Server-5.5.27-rel28.0-288.Linux.x86_64 --valgrind --reporter=ValgrindErrors --validator=MarkErrorLog'
+ ],[
+  '--grammar=conf/percona_qa/percona_qa.yy --gendata=conf/percona_qa/percona_qa.zz',
+ ]
+]
