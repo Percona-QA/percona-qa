73383e48ad900101e1be941fb5a575c0fc011d67
Roel Van de Paar <roel.vandepaar@percona.com>
2012-10-25 20:37:57 +1100

    Enabling mysql_multi_statements => 1 for connect strings. Discussed with and reviewed by PhillipS

--- randgen/server.pl
+++ randgen/server.pl
@@ -230,7 +230,7 @@ if ($rpl_mode ne '') {
             }
             
             if ((defined $dsns[$server_id]) && (defined $engine)) {
-                my $dbh = DBI->connect($dsns[$server_id], undef, undef, { RaiseError => 1 } );
+                my $dbh = DBI->connect($dsns[$server_id], undef, undef, { mysql_multi_statements => 1, RaiseError => 1 } );
                 $dbh->do("SET GLOBAL storage_engine = '$engine'");
             }
         }
