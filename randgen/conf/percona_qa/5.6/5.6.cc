5369bdeb7be5eeb27a505e47e6035318b73723a8
Roel Van de Paar <roel.vandepaar@percona.com>
2013-02-21 16:23:09 +1100

    Some changes and additions to 5.6 base grammar Percona

--- randgen/conf/percona_qa/5.6/5.6.cc
+++ randgen/conf/percona_qa/5.6/5.6.cc
@@ -22,6 +22,10 @@
 #  '',
 #  '--mysqld=--innodb_lazy_drop_table=1'
 
+# No innodb_log_block_size in 5.6 yet
+# --mysqld=--innodb_log_block_size=512
+# --mysqld=--innodb_log_block_size=4096
+
 $combinations=
 [
  ['
@@ -49,6 +53,22 @@ $combinations=
  ],[
   '',
   '--mysqld=--userstat'
+ ],[
+  '',
+  '--mysqld=--innodb_log_file_size=1048576 --mysqld=--innodb_log_files_in_group=10
+    --mysqld=--innodb_log_buffer_size=10485761',
+  '--mysqld=--innodb_log_file_size=10485761 --mysqld=--innodb_log_files_in_group=3
+    --mysqld=--innodb_log_buffer_size=1048577
+    --mysqld=--innodb_fast_shutdown=0 --mysqld=--innodb_adaptive_flushing_method=keep_average
+    --mysqld=--skip-innodb_doublewrite --mysqld=--userstat',
+  '--mysqld=--innodb_log_file_size=1048576 --mysqld=--innodb_log_files_in_group=2
+    --mysqld=--innodb_log_buffer_size=1048576
+    --mysqld=--innodb_fast_shutdown=2 --mysqld=--innodb_adaptive_flushing_method=native
+    --mysqld=--innodb_use_global_flush_log_at_trx_commit=0 --mysqld=--userstat'
+ ],[
+  '',
+  '--mysqld=--innodb_flush_method=O_DSYNC',
+  '--mysqld=--innodb_flush_method=O_DIRECT'
  ],[
   '',
   '--mysqld=--innodb_file_per_table=1',
