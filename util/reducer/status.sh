d52c9cdbdf7fb258ac482ff8ee9f655eb902f397
Roel Van de Paar <roel.vandepaar@percona.com>
2014-05-27 19:39:38 +1000

    Reducer; large update to status.sh, making it much more functional - easy mysql client access, input lines count, verify stage reporting

--- util/reducer/status.sh
+++ util/reducer/status.sh
@@ -1,7 +1,33 @@
-# Quick reducer status output (number of lines processed by each mysqld) 
-# Compare this with the number of original input lines, and you can workout how far along things are
+#!/bin/bash
+
+echo "Quick reducer status output (number of lines processed by each mysqld versus lines to be processed)"
+echo "For initial simplification during the verify stage ([V]), note that there are 6 verify stages (each time with less simplification),"
+echo "and each time all queries are processed. So you may think about this as Verify stage 1 x (as reported here) out of y (as reported"
+echo "by reducer.sh) queries, and this 6 times, if necessary."
+
+# This can be improved by checking if verify [V] stage is past (if so, there is no need for this reporting nor message/info above
+echo -e "\n=== Verify stages progress per mysqld, only relevant for initial simplification during the verify stage ([V])"
 ps -ef | grep mysqld | grep subreducer | \
-  sed "s/.*:[0-9][0-9] //;s/d .*--socket/ --socket/;s/\.sock .*/.sock -uroot -e\"show global status like 'Queries'\" | grep Queries/" > /tmp/_qstat1.sh
+  sed 's/.*--datadir=/sort -r /;s/data .*/reducer.log | grep -m1 "Verify attempt #" | sed "s\/.*Verify\/Verify\/;s\/:.*\/\/"/' > /tmp/_qstat1.sh
 chmod +x /tmp/_qstat1.sh
 /tmp/_qstat1.sh
 rm -f /tmp/_qstat1.sh
+
+INPUT_LINES=$(ps -ef | grep mysqld | grep subreducer | \
+  sed 's/.*--datadir=//;s/data .*/reducer.log/' | xargs grep "Number of lines in input file" | sed 's/.*: //' | head -n1)
+
+echo -e "\n=== Queries processed per mysqld"
+ps -ef | grep mysqld | grep subreducer | \
+  sed "s/.*:[0-9][0-9] //;s/d .*--socket/ --socket/;s/\.sock .*/.sock -uroot -e\"show global status like 'Queries'\" | grep Queries/" > /tmp/_qstat1.sh
+chmod +x /tmp/_qstat1.sh
+if [ "$INPUT_LINES" == "" ]; then  # Error protection
+  /tmp/_qstat1.sh
+else
+  /tmp/_qstat1.sh | sed "s|$|/$INPUT_LINES|"
+fi
+rm -f /tmp/_qstat1.sh
+
+echo -e "\n=== Client version strings for easy access"
+ps -ef | grep mysqld | grep subreducer | \
+  sed "s/.*:[0-9][0-9] //;s/d .*--socket/ --socket/;s/\.sock .*/.sock -uroot/" 
+
