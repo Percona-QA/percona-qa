cb657768b385fd7db55b52390ffd2d4ce06322f3
Roel Van de Paar <roel.vandepaar@percona.com>
2014-08-28 20:38:02 +1000

    Percona: reset default SE to XtraDB

--- conf/percona_qa/5.6/TokuDB.sql
+++ conf/percona_qa/5.6/TokuDB.sql
@@ -4,5 +4,3 @@ INSTALL PLUGIN tokudb_fractal_tree_block_map SONAME 'ha_tokudb.so';
 INSTALL PLUGIN tokudb_trx SONAME 'ha_tokudb.so'; 
 INSTALL PLUGIN tokudb_locks SONAME 'ha_tokudb.so'; 
 INSTALL PLUGIN tokudb_lock_waits SONAME 'ha_tokudb.so'; 
-SET GLOBAL DEFAULT_STORAGE_ENGINE = TOKUDB;
-SET SESSION DEFAULT_STORAGE_ENGINE = TOKUDB;
