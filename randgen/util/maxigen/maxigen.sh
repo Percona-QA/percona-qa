a7095de6cb7c950589975c22648fa0da6f5a897f
Roel Van de Paar <roel.vandepaar@percona.com>
2014-09-24 04:14:52 +1000

    Maxigen filter fixes

--- randgen/util/maxigen/maxigen.sh
+++ randgen/util/maxigen/maxigen.sh
@@ -93,8 +93,8 @@ for GRAMMAR in $(find /tmp/$RND_DIR/ -name '*.yy'); do
   # First filter below is PERL NO (no perl) filter, second filter is PERL YES (leave perl in). Currently set to PERL YES (do not filter any Perl)
   # FILTER="^$|^[; \t]*$|set.*[globalsession]*[ \.\t]*debug.*=| table1 |Sentence is now longer|information_schema[ \.\t]*[global_]*temporary_tables|return undef|no strict|{|}"
 
-  FILTER="^$|^[; \t]*$|set.*[globalsession]*[ \.\t]*debug.*=| table1 |Sentence is now longer|information_schema[ \.\t]*[global_]*temporary_tables"
-  FILTER="${FILTER}|set.*[globalsession]*[ \.\t]*innodb_track_redo_log_nowi.*=|set.*[globalsession]*[ \.\t]*innodb_track_changed_pages.*=" # See PS bug 1368530
+  FILTER="^$|^[; \t]*$|set[ @globalsession\.\t]*debug[ \.\t]*=| table1 |Sentence is now longer|information_schema[ \.\t]*[global_]*temporary_tables"
+  FILTER="${FILTER}|set[ @globalsession\.\t]*innodb_track_changed_pages[ \.\t]*=|set[ @globalsession\.\t]*innodb_track_redo_log_now[ \.\t]*=" # See PS bug 1368530
   egrep -vi "$FILTER" $GRAMMAR > ${GRAMMAR}.new
 
   rm ${GRAMMAR}
