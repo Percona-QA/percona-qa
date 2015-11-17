b041a1ce9903bb9db8387d56c1684227cf3f8fc6
Ramesh Sivaraman <rameshvs02@gmail.com>
2015-11-17 16:32:34 +0530

    Recovery testing updates

--- pquery-recovery.sh
+++ pquery-recovery.sh
@@ -13,41 +13,41 @@ fi
 ps -ef | grep $TRIAL |grep mysqld | grep -v grep | awk '{print $2}' | xargs kill -9 > /dev/null 2>&1
 
 # Start mysqld for recovery testing
-$SCRIPT_PWD/$TRIAL/start_recovery > /dev/null &
-BASEDIR=`grep BASEDIR $SCRIPT_PWD/$TRIAL/start_recovery |  sed 's|^[ \t]*BASEDIR[ \t]*=[ \t]*[ \t]*||'`
+$PQUERY_PWD/$TRIAL/start_recovery > /dev/null &
+BASEDIR=`grep BASEDIR $PQUERY_PWD/$TRIAL/start_recovery |  sed 's|^[ \t]*BASEDIR[ \t]*=[ \t]*[ \t]*||'`
 for X in $(seq 0 ${MYSQLD_START_TIMEOUT}); do
   sleep 1
-  if ${BASEDIR}/bin/mysqladmin -uroot -S${SCRIPT_PWD}/${TRIAL}/socket.sock ping > /dev/null 2>&1; then
+  if ${BASEDIR}/bin/mysqladmin -uroot -S${PQUERY_PWD}/${TRIAL}/socket.sock ping > /dev/null 2>&1; then
     break
   fi
 done
 
 # Check server startup
-if egrep -q  "registration as a STORAGE ENGINE failed" $SCRIPT_PWD/$TRIAL/log/master.err; then
+if egrep -q  "registration as a STORAGE ENGINE failed" $PQUERY_PWD/$TRIAL/log/master.err; then
   echo "Recovery error : Storage engine registration failed."
   exit 1
-elif egrep -q  "corrupt|crashed" $SCRIPT_PWD/$TRIAL/log/master.err; then
+elif egrep -q  "corrupt|crashed" $PQUERY_PWD/$TRIAL/log/master.err; then
   echo "Recovery error : Log message '$_' indicates database corruption."
   exit 1
-elif egrep -q  "device full error|no space left on device" $SCRIPT_PWD/$TRIAL/log/master.err; then
+elif egrep -q  "device full error|no space left on device" $PQUERY_PWD/$TRIAL/log/master.err; then
   echo "Recovery error : Check disk space."
   exit 1
-elif egrep -q  "ready for connections" $SCRIPT_PWD/$TRIAL/log/master.err; then
+elif egrep -q  "ready for connections" $PQUERY_PWD/$TRIAL/log/master.err; then
   echo "Recovery info : Server Recovery was apparently successful."
 fi
 
-dbs=$(${BASEDIR}/bin/mysql --socket=${SCRIPT_PWD}/${TRIAL}/socket.sock -uroot -Bse "select schema_name from information_schema.schemata where schema_name not in ('mysql','information_schema','performance_schema')" )
+dbs=$(${BASEDIR}/bin/mysql --socket=${PQUERY_PWD}/${TRIAL}/socket.sock -uroot -Bse "select schema_name from information_schema.schemata where schema_name not in ('mysql','information_schema','performance_schema')" )
 # Check tables
 for i in "${dbs[@]}"; do
-  tbs=$(${BASEDIR}/bin/mysql --socket=${SCRIPT_PWD}/${TRIAL}/socket.sock -uroot -Bse "select table_name from information_schema.tables where table_schema='$i'")
+  tbs=$(${BASEDIR}/bin/mysql --socket=${PQUERY_PWD}/${TRIAL}/socket.sock -uroot -Bse "select table_name from information_schema.tables where table_schema='$i'")
   tbl_array=( $( for i in $tbs ; do echo $i ; done ) )
   for j in "${tbl_array[@]}"; do
     echo -e "\nVerifying table: $j; database: $i\n"
-    chk_result=$((${BASEDIR}/bin/mysql --socket=${SCRIPT_PWD}/${TRIAL}/socket.sock -uroot -Bse $i "CHECK TABLE $j EXTENDED") 2>&1)
-    analyze_result=$((${BASEDIR}/bin/mysql --socket=${SCRIPT_PWD}/${TRIAL}/socket.sock -uroot -Bse $i "ANALYZE TABLE $j") 2>&1)
-    opt_result=$((${BASEDIR}/bin/mysql --socket=${SCRIPT_PWD}/${TRIAL}/socket.sock -uroot -Bse $i "OPTIMIZE TABLE $j") 2>&1)
-    repair_result=$((${BASEDIR}/bin/mysql --socket=${SCRIPT_PWD}/${TRIAL}/socket.sock -uroot -Bse $i "REPAIR TABLE $j EXTENDED") 2>&1)
-    alter_result=$((${BASEDIR}/bin/mysql --socket=${SCRIPT_PWD}/${TRIAL}/socket.sock -uroot -Bse $i "ALTER TABLE $j ENGINE = TokuDB") 2>&1)
+    chk_result=$((${BASEDIR}/bin/mysql --socket=${PQUERY_PWD}/${TRIAL}/socket.sock -uroot -Bse $i "CHECK TABLE $j EXTENDED") 2>&1)
+    analyze_result=$((${BASEDIR}/bin/mysql --socket=${PQUERY_PWD}/${TRIAL}/socket.sock -uroot -Bse $i "ANALYZE TABLE $j") 2>&1)
+    opt_result=$((${BASEDIR}/bin/mysql --socket=${PQUERY_PWD}/${TRIAL}/socket.sock -uroot -Bse $i "OPTIMIZE TABLE $j") 2>&1)
+    repair_result=$((${BASEDIR}/bin/mysql --socket=${PQUERY_PWD}/${TRIAL}/socket.sock -uroot -Bse $i "REPAIR TABLE $j EXTENDED") 2>&1)
+    alter_result=$((${BASEDIR}/bin/mysql --socket=${PQUERY_PWD}/${TRIAL}/socket.sock -uroot -Bse $i "ALTER TABLE $j ENGINE = TokuDB") 2>&1)
     ERROR_INFO=""
     if echo $chk_result| egrep -q -i "error|corrupt|repaired|invalid|crashed" ; then
       ERROR_INFO="Check table error : $chk_result\n"
