0b3f4b0052b33dfd3008af6682277ee554bb8bcb
Roel Van de Paar <roel.vandepaar@percona.com>
2015-11-28 17:12:51 +1100

    Update to randgen_to_sql.sh

--- randgen_to_sql.sh
+++ randgen_to_sql.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Created by Roel Van de Paar, Percona LLC
 
+RANDOM_RUNS_PER_GRAMMAR=50
+QUERIES_PER_GRAMMAR=50
+
 if [ ! -r gensql.pl ]; then
   echo "Assert: we're not in a randgen directory, as ./gensql.pl was not found"
 fi
@@ -9,13 +12,16 @@ if [ $(egrep "t1|c1" lib/GenTest/Generator/FromGrammar.pm | grep -v "prng" | wc 
   echo "Assert: required randgen patch is not in place, ref ./pquery/randgen_sql_generation.txt in percona-qa (i.e. this tree)"
 fi
 
-rm /tmp/newsql.sql
+rm -f /tmp/newsql.sql
+touch /tmp/newsql.sql
 
 for FILE in $(find . | grep "\.yy$"); do
   echo "Processing ${FILE}..."
-  for LOOP in $(seq 0 1000); do
-     RANDOM=$(date +%s%N | cut -b14-19)  # RANDOM: Random entropy pool init. 
+  for LOOP in $(seq 0 ${RANDOM_RUNS_PER_GRAMMAR}); do
+     RANDOM=$(date +%s%N | cut -b14-19)  # RANDOM: Random entropy pool init
      SEED=$(echo $RANDOM$RANDOM$RANDOM | sed 's/..\(......\).*/\1/')  # Random number generator (6 digits)
-     ./gensql.pl --grammar=./conf/engines/blobs.yy --seed=${SEED} --queries=100 --mask-level=0 --mask=0 >> /tmp/newsql.sql
+     MASK=$(echo $RANDOM$RANDOM$RANDOM | sed 's/..\(......\).*/\1/')  # Random number generator (6 digits)
+     MASK_LEVEL=$(echo $[ $RANDOM % 3 ])
+     ./gensql.pl --grammar=${FILE} --seed=${SEED} --queries=${QUERIES_PER_GRAMMAR} --mask-level=${MASK_LEVEL} --mask=${MASK} >> /tmp/newsql.sql
   done
 done
