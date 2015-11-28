410ec6c641e7f527571ffd292b0874dc90c68ea6
Roel Van de Paar <roel.vandepaar@percona.com>
2015-11-28 18:29:38 +1100

    Updating randgen_to_sql.sh

--- randgen_to_sql.sh
+++ randgen_to_sql.sh
@@ -3,13 +3,11 @@
 
 RANDOM_RUNS_PER_GRAMMAR=50
 QUERIES_PER_GRAMMAR=50
+DSN="dummy:print"
+#DSN="dbi:mysql:host=127.0.0.1:port=14270:user=root:database=sakila"
 
 if [ ! -r gensql.pl ]; then
   echo "Assert: we're not in a randgen directory, as ./gensql.pl was not found"
-fi
-
-if [ $(egrep "t1|c1" lib/GenTest/Generator/FromGrammar.pm | grep -v "prng" | wc -l) -ne 2 ]; then
-  echo "Assert: required randgen patch is not in place, ref ./pquery/randgen_sql_generation.txt in percona-qa (i.e. this tree)"
 fi
 
 rm -f /tmp/newsql.sql
@@ -23,9 +21,9 @@ for FILE in $(find . | grep "\.yy$"); do
      MASK=$(echo $RANDOM$RANDOM$RANDOM | sed 's/..\(......\).*/\1/')  # Random number generator (6 digits)
      MASK_LEVEL=$(echo $[ $RANDOM % 3 ])
      if [ ${LOOP} -eq 0 ]; then  # Only show errors for the first run, much less screen filling
-       ./gensql.pl --grammar=${FILE} --seed=${SEED} --queries=${QUERIES_PER_GRAMMAR} --mask-level=${MASK_LEVEL} --mask=${MASK} >> /tmp/newsql.sql
+       ./gensql.pl --dsn=${DSN} --grammar=${FILE} --seed=${SEED} --queries=${QUERIES_PER_GRAMMAR} --mask-level=${MASK_LEVEL} --mask=${MASK} >> /tmp/newsql.sql
      else
-       ./gensql.pl --grammar=${FILE} --seed=${SEED} --queries=${QUERIES_PER_GRAMMAR} --mask-level=${MASK_LEVEL} --mask=${MASK} >> /tmp/newsql.sql 2>/dev/null
+       ./gensql.pl --dsn=${DSN} --grammar=${FILE} --seed=${SEED} --queries=${QUERIES_PER_GRAMMAR} --mask-level=${MASK_LEVEL} --mask=${MASK} >> /tmp/newsql.sql 2>/dev/null
      fi
   done
 done
