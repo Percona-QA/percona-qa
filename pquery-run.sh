d5bc591ad5d112a40cee8aedf6c2431e9feec603
Roel Van de Paar <roel.vandepaar@percona.com>
2016-03-16 20:59:02 +1100

    QC_FB #14

--- pquery-run.sh
+++ pquery-run.sh
@@ -522,11 +522,11 @@ pquery_test(){
         shuf --random-source=/dev/urandom ${INFILE} | head -n${QC_NR_OF_STATEMENTS_PER_TRIAL} > ${RUNDIR}/${TRIAL}/${TRIAL}.sql
         echoit "Further processing testcase into two testcases against primary (${QC_PRI_ENGINE}) and secondary (${QC_SEC_ENGINE}) engines..."
         cat ${RUNDIR}/${TRIAL}/${TRIAL}.sql | \
-         grep -vi "^SET" \
-         > ${RUNDIR}/${TRIAL}/${TRIAL}.sql.${QC_PRI_ENGINE}
+         grep -vi "show variables" | \
+         grep -vi "^SET" > ${RUNDIR}/${TRIAL}/${TRIAL}.sql.${QC_PRI_ENGINE}
         cat ${RUNDIR}/${TRIAL}/${TRIAL}.sql | \
-         grep -vi "^SET" \
-         > ${RUNDIR}/${TRIAL}/${TRIAL}.sql.${QC_SEC_ENGINE}
+         grep -vi "show variables" | \
+         grep -vi "^SET" > ${RUNDIR}/${TRIAL}/${TRIAL}.sql.${QC_SEC_ENGINE}
         sed -i "s|innodb|${QC_PRI_ENGINE}|gi"     ${RUNDIR}/${TRIAL}/${TRIAL}.sql.${QC_PRI_ENGINE};
         sed -i "s|innodb|${QC_SEC_ENGINE}|gi"     ${RUNDIR}/${TRIAL}/${TRIAL}.sql.${QC_SEC_ENGINE};
         sed -i "s|tokudb|${QC_PRI_ENGINE}|gi"     ${RUNDIR}/${TRIAL}/${TRIAL}.sql.${QC_PRI_ENGINE};
