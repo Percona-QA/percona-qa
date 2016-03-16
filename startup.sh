c3344c2d1e9c87f873fea6d85d6656c41c7ff5d1
Roel Van de Paar <roel.vandepaar@percona.com>
2016-03-16 19:29:39 +1100

    QC_FB #5

--- startup.sh
+++ startup.sh
@@ -141,6 +141,7 @@ if [ "0" == "$1" ]; then
   else
     TOKUDB=""
   fi
+  echo '# REMEMBER TO ADD --tokudb-check-jemalloc=0 for JEMALLOC 4 version'
   echo 'MYEXTRA=" --no-defaults"' > ./start
   echo '#MYEXTRA=" --no-defaults --default-tmp-storage-engine=MyISAM --rocksdb --skip-innodb --default-storage-engine=RocksDB"' >> ./start
   echo '#MYEXTRA=" --no-defaults --event-scheduler=ON --maximum-bulk_insert_buffer_size=1M --maximum-join_buffer_size=1M --maximum-max_heap_table_size=1M --maximum-max_join_size=1M --maximum-myisam_max_sort_file_size=1M --maximum-myisam_mmap_size=1M --maximum-myisam_sort_buffer_size=1M --maximum-optimizer_trace_max_mem_size=1M --maximum-preload_buffer_size=1M --maximum-query_alloc_block_size=1M --maximum-query_prealloc_size=1M --maximum-range_alloc_block_size=1M --maximum-read_buffer_size=1M --maximum-read_rnd_buffer_size=1M --maximum-sort_buffer_size=1M --maximum-tmp_table_size=1M --maximum-transaction_alloc_block_size=1M --maximum-transaction_prealloc_size=1M --log-output=none --sql_mode=ONLY_FULL_GROUP_BY"' >> ./start
