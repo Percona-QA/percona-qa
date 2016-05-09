6b1f6a58620f8e37da1397198af131912ab42165
rameshvs02 <rameshvs02@gmail.com>
2016-05-09 10:47:18 +0530

    Adding pquery/pquery-cluster.cfg and modified pxc-garbd.sh script

--- pxc-garbd.sh
+++ pxc-garbd.sh
@@ -1,140 +1,190 @@
-#!/bin/bash
+#!/bin/bash 
+# Created by Ramesh Sivaraman, Percona LLC
 
-SYSBENCH_LOC="/usr/share/doc/sysbench/tests"
-dbuser="root"
-dbpass="root"
-MYQ_GADGETS="/home/ramesh/pxc/myq_gadgets"
-header="|%-10s |%-25s |%-25s |\n"
+# User Configurable Variables
+SBENCH="sysbench"
+PORT=$[50000 + ( $RANDOM % ( 9999 ) ) ]
+WORKDIR=$1
+ROOT_FS=$WORKDIR
+SCRIPT_PWD=$(cd `dirname $0` && pwd)
+LPATH="/usr/share/doc/sysbench/tests/db"
+PXC_START_TIMEOUT=200
 
-BASE="/usr"
-DATADIR="/var/lib/mysql"
-pxc_nodes=("208.88.225.243" "208.88.225.160")
-pxc_gardb="208.88.225.240"
+# For local run - User Configurable Variables
+if [ -z ${BUILD_NUMBER} ]; then
+  BUILD_NUMBER=1001
+fi
+if [ -z ${SDURATION} ]; then
+  SDURATION=30
+fi
+if [ -z ${TSIZE} ]; then
+  TSIZE=500
+fi
+if [ -z ${NUMT} ]; then
+  NUMT=16
+fi
+if [ -z ${TCOUNT} ]; then
+  TCOUNT=10
+fi
 
-function verifyPXC {
- # Check PXC nodes are up
- for i in "${pxc_nodes[@]}"
- do
-   check=`ssh $i "$BASE/bin/mysqladmin -uroot ping 2>/dev/null"`
-   if [ -z "$check" ]; then
-     echo "PXC is not running on $i"
-     exit 1
-   fi
- done
- echo "[Checked] : PXC nodes are running!"
-}
-function verifygarbd {
-  # Check PXC garbd is running
-  garbd_check=`ssh $pxc_gardb  pgrep "garbd"`
-  if [ ! "$garbd_check" ]; then
-   echo "garbd is not running on $pxc_gardb"
-   exit 1
-  fi
-  echo "[Checked] : garbd is running!"
-}
+#Kill existing mysqld process
+ps -ef | grep 'n[0-9].sock' | grep ${BUILD_NUMBER} | grep -v grep | awk '{print $2}' | xargs kill -9 >/dev/null 2>&1 || true
 
-
-# Initiate sysbench run
-verifyPXC
-# Sysbench Prepare run
-mysql -u$dbuser -h${pxc_nodes[0]} -e "drop database if exists pxc_test;create database pxc_test" 2>/dev/null
-sysbench --test=$SYSBENCH_LOC/db/parallel_prepare.lua  --mysql-host=${pxc_nodes[0]}  --num-threads=10    --oltp-tables-count=10 --oltp-table-size=5000  --mysql-db=pxc_test --mysql-user=$dbuser  --db-driver=mysql run > sysbench_prepare.log
-
-# Sysbench OLTP run
-sysbench --test=$SYSBENCH_LOC/db/oltp.lua --mysql-host=${pxc_nodes[0]} --mysql-user=$dbuser   --mysql-password=$dbpass --num-threads=10    --oltp-tables-count=10 --mysql-db=pxc_test --oltp-table-size=50000 --report-interval=1 --max-requests=0 --tx-rate=100 run > /dev/null 2>&1 & 
-SYSBENCH_PID="$!"
-
-function checkPXC {
- wsrep_cluster_status=()
- wsrep_local_state_comment=()
- status_node1=`mysql -u $dbuser -h${pxc_nodes[0]} -Bse"show global status like 'wsrep_cluster_status'" 2>/dev/null | awk '{print $2}'`
- if [ -z $status_node1 ] ;  then   wsrep_cluster_status+=("MySQL_is_not_running!") ;  else wsrep_cluster_status+=($status_node1) ; fi
- status_node2=`mysql -u $dbuser -h${pxc_nodes[1]} -Bse"show global status like 'wsrep_cluster_status'" 2>/dev/null | awk '{print $2}'`
- if [ -z $status_node2 ] ;  then   wsrep_cluster_status+=("MySQL_is_not_running!") ;  else wsrep_cluster_status+=($status_node2) ; fi
- comment_node1=`mysql -u $dbuser -h${pxc_nodes[0]} -Bse"show global status like 'wsrep_local_state_comment'" 2>/dev/null | awk '{print $2}'`
- if [ -z $comment_node1 ] ;  then wsrep_local_state_comment+=('MySQL_is_not_running!') ; else wsrep_local_state_comment+=($comment_node1) ; fi
- comment_node2=`mysql -u $dbuser -h${pxc_nodes[1]} -Bse"show global status like 'wsrep_local_state_comment'" 2>/dev/null | awk '{print $2}'`
- if [ -z $comment_node2 ] ;  then wsrep_local_state_comment+=('MySQL_is_not_running!') ; else wsrep_local_state_comment+=($comment_node2) ; fi
- 
- #printf '%s\n' "${wsrep_cluster_status[*]}"
- cluster_status=(${wsrep_cluster_status[@]})
- state_comment=(${wsrep_local_state_comment[@]})
+cleanup(){
+  tar cvzf $ROOT_FS/results-${BUILD_NUMBER}.tar.gz $WORKDIR/logs || true
 }
 
-function verifyPXC_start {
-  ssh ${pxc_nodes[0]} "killall  mysqld  2>/dev/null" 
-  ssh ${pxc_nodes[1]} "killall  mysqld  2>/dev/null" 
-  sleep 10
-  ssh ${pxc_nodes[0]} "rm -rf /var/lib/mysql/* ; $BASE/bin/mysql_install_db --basedir=$BASE --user=mysql --datadir=/var/lib/mysql > /dev/null 2>&1; $BASE/sbin/mysqld --basedir=$BASE --user=mysql --wsrep-new-cluster --skip-grant-tables  --log-error=/var/lib/mysql/error.log > /dev/null 2>&1 &"
-  for X in $(seq 0 300); do
-    sleep 1
-    if ssh ${pxc_nodes[0]} "$BASE/bin/mysqladmin -uroot ping 2>/dev/null" ; then
-      break 
-    fi
-  done
-  ssh ${pxc_nodes[1]} "rm -rf /var/lib/mysql/* ; $BASE/bin/mysql_install_db --basedir=$BASE --user=mysql  > /dev/null 2>&1; $BASE/sbin/mysqld --basedir=$BASE --user=mysql --skip-grant-tables --wsrep_cluster_address=gcomm://${pxc_nodes[0]}  --wsrep_node_name=node2  --log-error=/var/lib/mysql/error.log > /dev/null 2>&1 &"
+trap cleanup EXIT KILL
+
+PXC_TAR=`ls -1td ?ercona-?tra??-?luster* | grep ".tar" | head -n1`
+
+if [ ! -z $PXC_TAR ];then
+  tar -xzf $PXC_TAR
+  PXCBASE=`ls -1td ?ercona-?tra??-?luster* | grep -v ".tar" | head -n1`
+  export PATH="$ROOT_FS/$PXCBASE/bin:$PATH"
+fi
+
+if [ ! -a $ROOT_FS/garbd ];then
+  wget http://jenkins.percona.com/job/pxc56.buildandtest.galera3/Btype=release,label_exp=centos6-32/lastSuccessfulBuild/artifact/garbd
+  cp garbd $ROOT_FS/$PXCBASE/bin/
+  export PATH="$ROOT_FS/$PXCBASE/bin:$PATH"
+fi
+
+ADDR="127.0.0.1"
+RPORT=$(( RANDOM%21 + 10 ))
+RBASE1="$(( RPORT*1000 ))"
+RADDR1="$ADDR:$(( RBASE1 + 7 ))"
+LADDR1="$ADDR:$(( RBASE1 + 8 ))"
   
-  for X in $(seq 0 300); do
+RBASE2="$(( RBASE1 + 100 ))"
+RADDR2="$ADDR:$(( RBASE2 + 7 ))"
+LADDR2="$ADDR:$(( RBASE2 + 8 ))"
+  
+RBASE3="$(( RBASE1 + 200 ))"
+RADDR3="$ADDR:$(( RBASE3 + 7 ))"
+LADDR3="$ADDR:$(( RBASE3 + 8 ))"
+
+GARBDP="$(( LADDR3 + 100 ))"
+
+SUSER=root
+SPASS=
+
+WORKDIR="${ROOT_FS}/$BUILD_NUMBER"
+BASEDIR="${ROOT_FS}/$PXCBASE"
+mkdir -p $WORKDIR  $WORKDIR/logs
+
+
+pxc_startup(){
+  if [ "$(${BASEDIR}/bin/mysqld --version | grep -oe '5\.[567]' | head -n1)" == "5.7" ]; then
+    MID="${BASEDIR}/bin/mysqld --no-defaults --initialize-insecure --basedir=${BASEDIR}"
+  elif [ "$(${BASEDIR}/bin/mysqld --version | grep -oe '5\.[567]' | head -n1)" == "5.6" ]; then
+    MID="${BASEDIR}/scripts/mysql_install_db --no-defaults --basedir=${BASEDIR}"
+  fi
+  node1="${WORKDIR}/node1"
+  node2="${WORKDIR}/node2"
+  node3="${WORKDIR}/node3"
+
+  if [ "$(${BASEDIR}/bin/mysqld --version | grep -oe '5\.[567]' | head -n1)" != "5.7" ]; then
+    mkdir -p $node1 $node2 $node3
+  fi
+
+  ${MID} --datadir=$node1  > ${WORKDIR}/startup_node1.err 2>&1 || exit 1;
+
+  ${BASEDIR}/bin/mysqld --no-defaults --defaults-group-suffix=.1 \
+    --basedir=${BASEDIR} --datadir=$node1 \
+    --loose-debug-sync-timeout=600 --skip-performance-schema \
+    --innodb_file_per_table $PXC_MYEXTRA --innodb_autoinc_lock_mode=2 --innodb_locks_unsafe_for_binlog=1 \
+    --wsrep-provider=${BASEDIR}/lib/libgalera_smm.so \
+    --wsrep_cluster_address=gcomm:// \
+    --wsrep_node_incoming_address=$ADDR \
+    --wsrep_provider_options=gmcast.listen_addr=tcp://$LADDR1 \
+    --wsrep_sst_method=rsync --wsrep_sst_auth=$SUSER:$SPASS \
+    --wsrep_node_address=$ADDR --innodb_flush_method=O_DIRECT \
+    --core-file --loose-new --sql-mode=no_engine_substitution \
+    --loose-innodb --secure-file-priv= --loose-innodb-status-file=1 \
+    --log-error=$node1/node1.err \
+    --socket=$node1/node1_socket.sock --log-output=none \
+    --port=$RBASE1 --server-id=1 --wsrep_slave_threads=2 > $node1/node1.err 2>&1 &
+
+  for X in $(seq 0 ${PXC_START_TIMEOUT}); do
     sleep 1
-    if ssh ${pxc_nodes[1]} "$BASE/bin/mysqladmin -uroot ping 2>/dev/null" ; then
+    if ${BASEDIR}/bin/mysqladmin -uroot -S$node1/node1_socket.sock ping > /dev/null 2>&1; then
       break
     fi
-  done 
+  done
+  
+  ${MID} --datadir=$node2  > ${WORKDIR}/startup_node2.err 2>&1 || exit 1;
 
+  ${BASEDIR}/bin/mysqld --no-defaults --defaults-group-suffix=.2 \
+    --basedir=${BASEDIR} --datadir=$node2 \
+    --loose-debug-sync-timeout=600 --skip-performance-schema \
+    --innodb_file_per_table $PXC_MYEXTRA --innodb_autoinc_lock_mode=2 --innodb_locks_unsafe_for_binlog=1 \
+    --wsrep-provider=${BASEDIR}/lib/libgalera_smm.so \
+    --wsrep_cluster_address=gcomm://$LADDR1,gcomm://$LADDR3 \
+    --wsrep_node_incoming_address=$ADDR \
+    --wsrep_provider_options=gmcast.listen_addr=tcp://$LADDR2 \
+    --wsrep_sst_method=rsync --wsrep_sst_auth=$SUSER:$SPASS \
+    --wsrep_node_address=$ADDR --innodb_flush_method=O_DIRECT \
+    --core-file --loose-new --sql-mode=no_engine_substitution \
+    --loose-innodb --secure-file-priv= --loose-innodb-status-file=1 \
+    --log-error=$node2/node2.err \
+    --socket=$node2/node2_socket.sock --log-output=none \
+    --port=$RBASE2 --server-id=2 --wsrep_slave_threads=2 > $node2/node2.err 2>&1 &
+
+  for X in $(seq 0 ${PXC_START_TIMEOUT}); do
+    sleep 1
+    if ${BASEDIR}/bin/mysqladmin -uroot -S$node2/node2_socket.sock ping > /dev/null 2>&1; then
+      break
+    fi
+  done
+
+  ${MID} --datadir=$node3  > ${WORKDIR}/startup_node3.err 2>&1 || exit 1;
+
+  ${BASEDIR}/bin/mysqld --no-defaults --defaults-group-suffix=.3 \
+    --basedir=${BASEDIR} --datadir=$node3 \
+    --loose-debug-sync-timeout=600 --skip-performance-schema \
+    --innodb_file_per_table $PXC_MYEXTRA --innodb_autoinc_lock_mode=2 --innodb_locks_unsafe_for_binlog=1 \
+    --wsrep-provider=${BASEDIR}/lib/libgalera_smm.so \
+    --wsrep_cluster_address=gcomm://$LADDR1,gcomm://$LADDR2 \
+    --wsrep_node_incoming_address=$ADDR \
+    --wsrep_provider_options=gmcast.listen_addr=tcp://$LADDR3 \
+    --wsrep_sst_method=rsync --wsrep_sst_auth=$SUSER:$SPASS \
+    --wsrep_node_address=$ADDR --innodb_flush_method=O_DIRECT \
+    --core-file --loose-new --sql-mode=no_engine_substitution \
+    --loose-innodb --secure-file-priv= --loose-innodb-status-file=1 \
+    --log-error=$node3/node3.err \
+    --socket=$node3/node3_socket.sock --log-output=none \
+    --port=$RBASE3 --server-id=3 --wsrep_slave_threads=2 > $node3/node3.err 2>&1 &
+
+  for X in $(seq 0 ${PXC_START_TIMEOUT}); do
+    sleep 1
+    if ${BASEDIR}/bin/mysqladmin -uroot -S$node3/node3_socket.sock ping > /dev/null 2>&1; then
+      ${BASEDIR}/bin/mysql -uroot -S$node1/node1_socket.sock -e "create database if not exists test" > /dev/null 2>&1
+      sleep 2
+      break
+    fi
+  done
 }
 
-rm -Rf garbd.log
-function testReplwithgarbd {
-#  verifyPXC_start
-  verifygarbd
-  checkPXC
-  echo -e "PXC replication through garbd" >> garbd.log
-  echo -e "----------------------------------------------------------------------\n"  >> garbd.log
-  echo -e "Current PXC Status:\n" >> garbd.log
-  printf "$header" "Node 1" ${cluster_status[0]} ${state_comment[0]} >> garbd.log
-  printf "$header" "Node 2" ${cluster_status[1]} ${state_comment[1]} >> garbd.log
+garbd_run(){
+  #Sysbench data load
+  $SBENCH --test=$LPATH/parallel_prepare.lua --report-interval=10 --mysql-engine-trx=yes --mysql-table-engine=innodb --oltp-table-size=$TSIZE \
+    --oltp_tables_count=$TCOUNT --mysql-db=test --mysql-user=root  --num-threads=$NUMT --db-driver=mysql \
+    --mysql-socket=$node1/node1_socket.sock prepare  2>&1 | tee $WORKDIR/logs/sysbench_prepare.txt
 
-  #Kill PXC node to test garbd
+  $ROOT_FS/garbd --address gcomm://$LADDR1,$LADDR2,$LADDR3 --group "my_wsrep_cluster" --options "gmcast.listen_addr=tcp://$GARBDP" --log /tmp/garbd.log --daemon
 
-  ssh ${pxc_nodes[1]} "pkill -9 -f mysqld  >/dev/null 2>&1;"
-  echo -e "\nKilled mysqld from ${pxc_nodes[1]} for garbd testing\n" >> garbd.log
-  checkPXC
-  echo -e "PXC Status:\n" >> garbd.log
-  printf "$header" "Node 1" ${cluster_status[0]} ${state_comment[0]} >> garbd.log
-  printf "$header" "Node 2" ${cluster_status[1]} ${state_comment[1]} >> garbd.log
- # kill -9 ${SYSBENCH_PID} >/dev/null 2>&1
-  pkill -9 -f sysbench >/dev/null 2>&1
+  #OLTP RW run
+  $SBENCH --mysql-table-engine=innodb --num-threads=$NUMT --report-interval=10 --max-time=$SDURATION --max-requests=1870000000 \
+    --test=$LPATH/oltp.lua --init-rng=on --oltp_index_updates=10 --oltp_non_index_updates=10 --oltp_distinct_ranges=15 \
+    --oltp_order_ranges=15 --oltp_tables_count=$TCOUNT --mysql-db=test --mysql-user=root --db-driver=mysql \
+    --mysql-socket=$node1/node1_socket.sock run  2>&1 | tee $WORKDIR/logs/sysbench_rw.log
+
+  #Shutdown PXC servers
+  $BASEDIR/bin/mysqladmin  --socket=$node1/node1_socket.sock -u root shutdown
+  $BASEDIR/bin/mysqladmin  --socket=$node2/node2_socket.sock -u root shutdown
+  $BASEDIR/bin/mysqladmin  --socket=$node3/node3_socket.sock -u root shutdown
 }
 
-function testReplwithoutgarbd {
-  echo "PXC replication without garbd"
-  ssh $pxc_gardb  "killall  garbd >/dev/null 2>&1;"
-  verifyPXC_start
-
-   # Sysbench OLTP run
-  sysbench --test=$SYSBENCH_LOC/db/oltp.lua --mysql-host=${pxc_nodes[0]} --mysql-user=$dbuser   --mysql-password=$dbpass --num-threads=10    --oltp-tables-count=10 --mysql-db=pxc_test --oltp-table-size=50000 --report-interval=1 --max-requests=0 --tx-rate=100 run > /dev/null 2>&1 &
-  SYSBENCH_PID="$!"
-  sleep 2 
-  checkPXC
-  echo -e "\nPXC replication without garbd" >> garbd.log
-  echo -e "---------------------------------------------------------------------\n"  >> garbd.log
-  echo -e "Current PXC Status:\n" >> garbd.log
-  printf "$header" "Node 1" ${cluster_status[0]} ${state_comment[0]} >> garbd.log
-  printf "$header" "Node 2" ${cluster_status[1]} ${state_comment[1]} >> garbd.log
-
-  #Kill PXC node to test garbd
-
-  ssh ${pxc_nodes[1]} "pkill -9 -f mysqld" 2>/dev/null
-  echo -e "\nKilled mysqld from ${pxc_nodes[1]} to check PXC node status (without garbd)\n" >> garbd.log
-  sleep 10
-  checkPXC
-  echo -e "PXC Status:\n" >> garbd.log
-  printf "$header" "Node 1" ${cluster_status[0]} ${state_comment[0]} >> garbd.log
-  printf "$header" "Node 2" ${cluster_status[1]} ${state_comment[1]} >> garbd.log
-  kill -9 ${SYSBENCH_PID} >/dev/null 2>&1;
-}
-testReplwithgarbd 
-testReplwithoutgarbd
-
-echo "Done! garbd result set log : ${PWD}/garbd.log"
-
+pxc_startup
+garbd_run
