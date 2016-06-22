fa6c88f7074f981254b9ae94d48139b26da8d28f
Roel Van de Paar <roel.vandepaar@percona.com>
2016-06-22 17:00:26 +1000

    Adding plugins_MS.sql

---
+++ plugins_MS.sql
@@ -0,0 +1,17 @@
+INSTALL PLUGIN rpl_semi_sync_master SONAME 'semisync_master.so';
+INSTALL PLUGIN rpl_semi_sync_slave SONAME 'semisync_slave.so';
+INSTALL PLUGIN validate_password SONAME 'validate_password.so';
+INSTALL PLUGIN version_tokens SONAME 'version_token.so';
+CREATE FUNCTION version_tokens_set RETURNS STRING SONAME 'version_token.so';
+CREATE FUNCTION version_tokens_show RETURNS STRING SONAME 'version_token.so';
+CREATE FUNCTION version_tokens_edit RETURNS STRING SONAME 'version_token.so';
+CREATE FUNCTION version_tokens_delete RETURNS STRING SONAME 'version_token.so';
+CREATE FUNCTION version_tokens_lock_shared RETURNS INT SONAME 'version_token.so';
+CREATE FUNCTION version_tokens_lock_exclusive RETURNS INT SONAME 'version_token.so';
+CREATE FUNCTION version_tokens_unlock RETURNS INT SONAME 'version_token.so';
+INSTALL PLUGIN auth_socket SONAME 'auth_socket.so';
+INSTALL PLUGIN mysql_no_login SONAME 'mysql_no_login.so';
+INSTALL PLUGIN rewriter SONAME 'rewriter.so';
+CREATE FUNCTION service_get_read_locks RETURNS INT SONAME 'locking_service.so';
+CREATE FUNCTION service_get_write_locks RETURNS INT SONAME 'locking_service.so';
+CREATE FUNCTION service_release_locks RETURNS INT SONAME 'locking_service.so';
