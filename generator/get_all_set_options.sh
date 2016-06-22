34d71b3cba2a750bf866cf4770e71ec84b302f42
Roel Van de Paar <roel.vandepaar@percona.com>
2016-06-22 12:01:03 +1000

    Major update to get_all_options.sh, starting to work well

--- generator/get_all_set_options.sh
+++ generator/get_all_set_options.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 # Created by Ramesh Sivaraman & Roel Van de Paar, Percona LLC
-# This script processed dynamic variables only
+# This script processes dynamic variables (global + session) and writes generator.sh compatible files for them into /tmp/get_all_options
 
 SCRIPT_PWD=$(cd `dirname $0` && pwd)
-VERSION_CHECK=$1; 
-if [ "`echo $VERSION_CHECK | grep -oe '5\.[67]'`" != "5.6" ] && [ "`echo $VERSION_CHECK | grep -oe '5\.[67]'`" != "5.7" ];then
+if [ "$1" == "" ]; then
   VERSION_CHECK="5.7"
+elif [ "`echo $1 | grep -oe '5\.[67]'`" != "5.6" ] && [ "`echo $1 | grep -oe '5\.[67]'`" != "5.7" ];then
+  echo "Invalid option. Valid options are: 5.6 and 5.7, e.g. ./get_all_set_options.sh 5.7"
+  echo "If no option is given, 5.7 will be used by default"
+  exit 1
 fi
 
 #rm -Rf /tmp/get_all_options
@@ -25,29 +28,129 @@ grep -o "Command-Line Format.*\-\-[^<]\+" innodb-parameters.html | grep -o "\-\-
 
 # varlist syntax:   Name (1) Cmd-Line (2)  Option File (3)  System Var (4)  Var Scope (5)  Dynamic (6)  (can use awk '{print $x}')
 VERSION_CHECK=`echo $VERSION_CHECK | sed 's|\.||g'`  # Only change this after retrieving the pages above
+sed -i "s/=\[={OFF|ON}\]/[={OFF|ON}]/" commandlines.txt
 
+rm as_yet_to_handle.txt todo.txt 2>/dev/null
 parse_set_vars(){
-  rm ${VARFILE}.out
+  rm ${VARFILE}.out 2>/dev/null
   while read line; do
-    if grep -q $line ./commandlines.txt; then
-      PRLINE=$(grep $line ./commandlines.txt)
-      echo ${PRLINE} | if grep -q '={OFF|ON}'; then 
-        PRLINE=$(echo ${PRLINE} | sed 's/={OFF|ON}//')
-        echo "${PRLINE}=0" >> ${VARFILE}.out
-        echo "${PRLINE}=1" >> ${VARFILE}.out
+    if grep -q $line ./commandlines.txt; then  # ================= STAGE 0: Line is present in "Command-Line Format" capture file; use this to handle setting variables
+      PRLINE=$(grep $line ./commandlines.txt); HANDLED=0
+      if ! [[ "${PRLINE}" == *"="* ]]; then  # Variable without any options, for example --general-log
+        echo "${PRLINE}" >> ${VARFILE}.out
+        HANDLED=1
       fi
-      echo ${PRLINE} | if grep -q '=[{OFF|ON}]'; then 
-        PRLINE=$(echo ${PRLINE} | sed 's/=\[{OFF|ON}\]//')
+      if [[ "${PRLINE}" == *"[={OFF|ON}]"* ]]; then
+        PRLINE=$(echo ${PRLINE} | sed 's/\[={OFF|ON}\]//')
         echo "${PRLINE}" >> ${VARFILE}.out  # Without anything (as [x] means x is optional)
         echo "${PRLINE}=0" >> ${VARFILE}.out
         echo "${PRLINE}=1" >> ${VARFILE}.out
+        HANDLED=1
       fi
-      echo ${PRLINE} | if grep -q '=#'; then
-        echo "${PRLINE}" | sed 's|=#|=DUMMY|' >> ${VARFILE}.out
+      if [ ${HANDLED} -eq 0 ]; then
+        if [[ "${PRLINE}" == *"={OFF|ON}"* ]]; then
+          PRLINE=$(echo ${PRLINE} | sed 's/={OFF|ON}//')
+          echo "${PRLINE}=0" >> ${VARFILE}.out
+          echo "${PRLINE}=1" >> ${VARFILE}.out
+          HANDLED=1
+        fi
       fi
-      echo ${PRLINE} | if grep -q '=[#]'; then
-        echo "${PRLINE}" | sed 's|=[#]|=DUMMY|' >> ${VARFILE}.out
-        echo "${PRLINE}" | sed 's|=[#]||' >> ${VARFILE}.out  # Without anything, ref above
+      if [[ "${PRLINE}" == *"[=#]"* ]]; then
+        echo "${PRLINE}" | sed 's|\[=#\]|=DUMMY|' >> ${VARFILE}.out
+        echo "${PRLINE}" | sed 's|\[=#\]||' >> ${VARFILE}.out  # Without anything, ref above
+        HANDLED=1
+      fi
+      if [ ${HANDLED} -eq 0 ]; then
+        if [[ "${PRLINE}" == *"=#"* ]]; then
+          echo "${PRLINE}" | sed 's|=#|=DUMMY|' >> ${VARFILE}.out
+          HANDLED=1
+        fi
+      fi
+      if [[ "${PRLINE}" == *"=engine[,engine]..."* ]]; then
+        echo "${PRLINE}" | sed 's|=engine\[,engine\]...|=MyISAM|' >> ${VARFILE}.out
+        echo "${PRLINE}" | sed 's|=engine\[,engine\]...|=InnoDB|' >> ${VARFILE}.out
+        echo "${PRLINE}" | sed 's|=engine\[,engine\]...|=CSV|' >> ${VARFILE}.out
+        echo "${PRLINE}" | sed 's|=engine\[,engine\]...|=MRG_MYISAM|' >> ${VARFILE}.out
+        echo "${PRLINE}" | sed 's|=engine\[,engine\]...|=TokuDB|' >> ${VARFILE}.out
+        echo "${PRLINE}" | sed 's|=engine\[,engine\]...|=RocksDB|' >> ${VARFILE}.out
+        echo "${PRLINE}" | sed 's|=engine\[,engine\]...|=BLACKHOLE|' >> ${VARFILE}.out
+        echo "${PRLINE}" | sed 's|=engine\[,engine\]...|=PERFORMANCE_SCHEMA|' >> ${VARFILE}.out
+        echo "${PRLINE}" | sed 's|=engine\[,engine\]...|=MEMORY|' >> ${VARFILE}.out
+        echo "${PRLINE}" | sed 's|=engine\[,engine\]...|=FEDERATED|' >> ${VARFILE}.out
+        echo "${PRLINE}" | sed 's|=engine\[,engine\]...|=CSV,FEDERATED|' >> ${VARFILE}.out
+        echo "${PRLINE}" | sed 's|=engine\[,engine\]...|=CSV,MRG_MYISAM|' >> ${VARFILE}.out
+        HANDLED=1
+      fi
+      if [[ "${PRLINE}" == *"default-authentication-plugin=plugin-name"* ]]; then
+        echo "${PRLINE}" | sed 's|=plugin-name|=mysql_native_password|' >> ${VARFILE}.out
+        echo "${PRLINE}" | sed 's|=plugin-name|=sha256_password|' >> ${VARFILE}.out
+        HANDLED=1
+      fi
+      if [[ "${PRLINE}" == *"=dir-name"* ]] || [[ "${PRLINE}" == *"=path"* ]] || [[ "${PRLINE}" == *"=directory"* ]]; then
+        # These variables (--datadir, --character-sets-dir, --basedir etc.) are skipped as they do not make sense to modify, and they are unlikely to be dynamic;
+        # Note that this ifthen may not even be hit for all cases present in commandlines.txt. For example, --datadir is not dynamic and so it will not be in global/session txt
+        HANDLED=1
+      fi
+      if [[ "${PRLINE}" == *"event-scheduler[=value]"* ]]; then
+        echo "${PRLINE}" | sed 's|=[value]|=ON|' >> ${VARFILE}.out
+        echo "${PRLINE}" | sed 's|=[value]|=OFF|' >> ${VARFILE}.out
+        echo "${PRLINE}" | sed 's|=[value]|=DISABLED|' >> ${VARFILE}.out
+        HANDLED=1
+      fi
+      if [[ "${PRLINE}" == *"7"* ]]; then
+        echo "${PRLINE}" | sed 's|=||' >> ${VARFILE}.out
+        HANDLED=1
+      fi
+      if [[ "${PRLINE}" == *"7"* ]]; then
+        echo "${PRLINE}" | sed 's|=||' >> ${VARFILE}.out
+        HANDLED=1
+      fi
+      if [[ "${PRLINE}" == *"7"* ]]; then
+        echo "${PRLINE}" | sed 's|=||' >> ${VARFILE}.out
+        HANDLED=1
+      fi
+      if [[ "${PRLINE}" == *"7"* ]]; then
+        echo "${PRLINE}" | sed 's|=||' >> ${VARFILE}.out
+        HANDLED=1
+      fi
+      if [[ "${PRLINE}" == *"7"* ]]; then
+        echo "${PRLINE}" | sed 's|=||' >> ${VARFILE}.out
+        HANDLED=1
+      fi
+      if [[ "${PRLINE}" == *"7"* ]]; then
+        echo "${PRLINE}" | sed 's|=||' >> ${VARFILE}.out
+        HANDLED=1
+      fi
+      if [[ "${PRLINE}" == *"7"* ]]; then
+        echo "${PRLINE}" | sed 's|=||' >> ${VARFILE}.out
+        HANDLED=1
+      fi
+      if [[ "${PRLINE}" == *"7"* ]]; then
+        echo "${PRLINE}" | sed 's|=||' >> ${VARFILE}.out
+        HANDLED=1
+      fi
+      if [[ "${PRLINE}" == *"7"* ]]; then
+        echo "${PRLINE}" | sed 's|=||' >> ${VARFILE}.out
+        HANDLED=1
+      fi
+      if [[ "${PRLINE}" == *"7"* ]]; then
+        echo "${PRLINE}" | sed 's|=||' >> ${VARFILE}.out
+        HANDLED=1
+      fi
+      if [[ "${PRLINE}" == *"7"* ]]; then
+        echo "${PRLINE}" | sed 's|=||' >> ${VARFILE}.out
+        HANDLED=1
+      fi
+      if [[ "${PRLINE}" == *"7"* ]]; then
+        echo "${PRLINE}" | sed 's|=||' >> ${VARFILE}.out
+        HANDLED=1
+      fi
+      if [[ "${PRLINE}" == *"7"* ]]; then
+        echo "${PRLINE}" | sed 's|=||' >> ${VARFILE}.out
+        HANDLED=1
+      fi
+      if [ ${HANDLED} -eq 0 ]; then
+        echo $PRLINE >> todo.txt
       fi
     else
       echo $line >> as_yet_to_handle.txt
