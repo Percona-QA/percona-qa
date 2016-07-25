6077eb57663a2d59b0081d7e9e28f0ef21dd4b2d
Roel Van de Paar <roel.vandepaar@percona.com>
2016-07-25 10:18:11 +1000

    Adding pquery-eliminate-dups.sh & pquery-go-expert.sh update

---
+++ pquery-eliminate-dups.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Created by Roel Van de Paar, Percona LLC
+
+# This script eliminates duplicate trials where at least 10 trials are present for a given issue, and 10 trials are kept for each such issue where duplicates are eliminated. Execute from within the pquery workdir.
+
+# Internal variables
+SCRIPT_PWD=$(cd `dirname $0` && pwd)
+
+${SCRIPT_PWD}/pquery-results.sh | grep -o "Seen[ \t]\+[0-9][0-9]\+ times.*" | sed 's|.*reducers ||;s|[0-9]\+,[0-9]\+,[0-9]\+,[0-9]\+,[0-9]\+,[0-9]\+,[0-9]\+,[0-9]\+,[0-9]\+,[0-9]\+||;s|)||;s|,|\n|g' | grep -v "^[ \t]*$" | xargs -I{} ${SCRIPT_PWD}/pquery-del-trial.sh {}
