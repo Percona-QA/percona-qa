ee032bb38862a4193c3ee2bcf7056641b747d0b2
Roel Van de Paar <roel.vandepaar@percona.com>
2016-10-25 14:39:14 +1100

    Adding pmm-info.sh - a script which will display most relevant PMM info for inclusion with bug reports, pmm_info.txt - just a handy info file with most used commands to get PMM up and running, pquery-run-temp-pmm.sh - temporary hack copy for pmm testing, will be removed soon

---
+++ pmm-info.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Created by Roel Van de Paar, Percona LLC
+
+echo 'QA PMM Info Script v0.02'
+echo '==================== uname -a'
+uname -a 2>&1 | sed 's|^|  |'
+echo '==================== /proc/version'
+cat /proc/version 2>&1 | sed 's|^|  |'
+echo '==================== OS Release (filtered cat /etc/*-release):'  # With thanks, http://www.cyberciti.biz/faq/find-linux-distribution-name-version-number/
+cat /etc/*-release 2>&1 | grep -Ev '^$|^CENTOS_|^REDHAT_|^CPE_|^BUG_|^ANSI_' | sed 's|^|  |'
+echo '==================== Docker release (docker --version):'
+docker --version 2>&1 | sed 's|^|  |'
+echo '==================== SELinux status (sestatus):'
+sestatus 2>&1 | sed 's|^|  |'
+echo '==================== PMM server (sudo docker images | grep pmm):'
+sudo docker images 2>&1 | grep pmm | sed 's|^|  |'
+echo '==================== PMM agent (sudo pmm-admin --version):'
+sudo pmm-admin --version 2>&1 | sed 's|^|  |'
+
