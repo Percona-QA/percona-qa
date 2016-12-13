12136b70e955e62677859685f6cd56ff0b9e5b29
Przemek <przemek@mysqlmaniac.com>
2016-12-13 09:33:14 +0100

    Adding support for pmm-admin 1.0.7
    #  pmm-admin --version
    1.0.7
    #  pmm-admin check-network --no-emoji
    Error: unknown flag: --no-emoji
    
    Also removed pmm-admin --version output as version is already included in pmm-admin info.

--- pmm-info.sh
+++ pmm-info.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Created by Roel Van de Paar, Percona LLC
 
-echo 'QA PMM Info Script v0.07'
+echo 'QA PMM Info Script v0.08'
 echo '==================== uname -a'
 uname -a 2>&1 | sed 's|^|  |'
 echo '==================== /proc/version'
@@ -18,12 +18,17 @@ echo '==================== PMM server state (sudo docker ps -a | grep pmm):'
 sudo docker ps -a 2>&1 | grep pmm | sed 's|^|  |'
 echo '==================== Exporter status (ps -ef | grep exporter):'
 ps -ef | grep -v grep | grep exporter | sed 's|^|  |'
-echo '==================== PMM agent (sudo pmm-admin --version):'
-sudo pmm-admin --version 2>&1 | sed 's|^|  |'
 echo '==================== PMM info (sudo pmm-admin info):'
 sudo pmm-admin info 2>&1 | grep -v '^$' | sed 's|^|  |'
 echo '==================== PMM network check (sudo pmm-admin check-network):'
-sudo pmm-admin check-network --no-emoji 2>&1 | grep -v '^$' | sed 's|^|  |'
+function version { echo "$@" | gawk -F. '{ printf("%03d%03d%03d\n", $1,$2,$3); }'; }
+my_version=$(sudo pmm-admin --version)
+emoji_version=1.0.6
+if [ "$(version "$my_version")" -gt "$(version "$emoji_version")" ]; then
+        sudo pmm-admin check-network 2>&1 | grep -v '^$' | sed 's|^|  |'
+else
+        sudo pmm-admin check-network --no-emoji 2>&1 | grep -v '^$' | sed 's|^|  |'
+fi
 echo '==================== PMM list (sudo pmm-admin list):'
 sudo pmm-admin list 2>&1 | grep -v '^$' | sed 's|^|  |'
 
