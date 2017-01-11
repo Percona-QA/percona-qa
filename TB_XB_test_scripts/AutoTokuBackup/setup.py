1b7c4c7bc1b52d454229cdb78455df24293988f1
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-01-11 14:16:58 +0400

    Preliminary fix for issue #3

--- TB_XB_test_scripts/AutoTokuBackup/setup.py
+++ TB_XB_test_scripts/AutoTokuBackup/setup.py
@@ -14,7 +14,7 @@ setup(
     description='Commandline tool written in Python3 for using Percona TokuBackup',
     install_requires=[
         'click>=3.3',
-        'mysql-connector-python>=2.0.2',
+        'mysql-connector>=2.0.2',
         'watchdog>=0.8.3'
     ],
     entry_points='''
