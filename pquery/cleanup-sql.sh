b8c71105e7ef442d9504ba24cb9a029f8225e56b
Roel Van de Paar <roel@vandepaar.com>
2017-02-20 11:44:16 +1100

    Minor updates

--- pquery/cleanup-sql.sh
+++ pquery/cleanup-sql.sh
@@ -1,5 +1,8 @@
-cat main-ms-ps-md.sql | grep -v "+d," >> main-ms-ps-md.sql2
-cat main-ms-ps-md.sql2 | grep -vi "debug.*\-d," > main-ms-ps-md.sql3
-cat main-ms-ps-md.sql3 | grep -vi "set.*debug.*=" > main-ms-ps-md.sql4
-cat main-ms-ps-md.sql4 | grep -vi "rm -rf" > main-ms-ps-md.sql5
-cat main-ms-ps-md.sql5 | grep -vi " mv " > main-ms-ps-md.sql6
+FILE=main-ms-ps-md.sql
+cat ${FILE} | grep -v "+d," >> ${FILE}2
+cat ${FILE}2 | grep -vi "debug.*\-d," > ${FILE}3
+cat ${FILE}3 | grep -vi "set.*debug.*=" > ${FILE}4
+cat ${FILE}4 | grep -vi "rm -rf" > ${FILE}5
+cat ${FILE}5 | grep -vi " mv " > ${FILE}6
+rm ${FILE} ${FILE}2 ${FILE}3 ${FILE}4 ${FILE}5
+mv ${FILE}6 ${FILE}
