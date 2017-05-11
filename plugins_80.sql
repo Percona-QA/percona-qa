914f1e488d7ebda618cb6af368ca2c11ea66c8ff
Roel Van de Paar <roel@vandepaar.com>
2017-05-11 22:24:39 +1000

    8.0 Provisioning

---
+++ plugins_80.sql
@@ -0,0 +1,39 @@
+# Remarked items will be added later (all or part) by Laurynas. They are remarked for early version testing.
+system rmdir data/test  # Hack, ref bug https://bugs.mysql.com/bug.php?id=86282
+CREATE DATABASE test;   # Hack, idem
+INSTALL PLUGIN rpl_semi_sync_master SONAME 'semisync_master.so';
+INSTALL PLUGIN rpl_semi_sync_slave SONAME 'semisync_slave.so';
+#INSTALL PLUGIN auth_pam SONAME 'auth_pam.so';
+#INSTALL PLUGIN auth_pam_compat SONAME 'auth_pam_compat.so';
+#INSTALL PLUGIN QUERY_RESPONSE_TIME SONAME 'query_response_time.so';
+#INSTALL PLUGIN QUERY_RESPONSE_TIME_AUDIT SONAME 'query_response_time.so';
+#INSTALL PLUGIN QUERY_RESPONSE_TIME_READ SONAME 'query_response_time.so';
+#INSTALL PLUGIN QUERY_RESPONSE_TIME_WRITE SONAME 'query_response_time.so';
+INSTALL PLUGIN validate_password SONAME 'validate_password.so';
+INSTALL PLUGIN version_tokens SONAME 'version_token.so';
+CREATE FUNCTION version_tokens_set RETURNS STRING SONAME 'version_token.so';
+CREATE FUNCTION version_tokens_show RETURNS STRING SONAME 'version_token.so';
+CREATE FUNCTION version_tokens_edit RETURNS STRING SONAME 'version_token.so';
+CREATE FUNCTION version_tokens_delete RETURNS STRING SONAME 'version_token.so';
+CREATE FUNCTION version_tokens_lock_shared RETURNS INT SONAME 'version_token.so';
+CREATE FUNCTION version_tokens_lock_exclusive RETURNS INT SONAME 'version_token.so';
+CREATE FUNCTION version_tokens_unlock RETURNS INT SONAME 'version_token.so';
+INSTALL PLUGIN auth_socket SONAME 'auth_socket.so';
+INSTALL PLUGIN mysql_no_login SONAME 'mysql_no_login.so';
+#SOURCE share/install_rewriter.sql  # Do not use: INSTALL PLUGIN rewriter SONAME 'rewriter.so' ref http://bugs.mysql.com/bug.php?id=83407
+#CREATE FUNCTION service_get_read_locks RETURNS INT SONAME 'locking_service.so';
+CREATE FUNCTION service_get_write_locks RETURNS INT SONAME 'locking_service.so';
+CREATE FUNCTION service_release_locks RETURNS INT SONAME 'locking_service.so';
+#CREATE FUNCTION fnv1a_64 RETURNS INTEGER SONAME 'libfnv1a_udf.so';
+#CREATE FUNCTION fnv_64 RETURNS INTEGER SONAME 'libfnv_udf.so';
+#CREATE FUNCTION murmur_hash RETURNS INTEGER SONAME 'libmurmur_udf.so';
+# INSTALL PLUGIN tokudb SONAME 'ha_tokudb.so';  # Disabled, because pquery-run.sh preloads this (it does so to enable TokuDB --options to be used with mysqld) (if set in MYEXTRA)
+#INSTALL PLUGIN tokudb_file_map SONAME 'ha_tokudb.so';
+#INSTALL PLUGIN tokudb_fractal_tree_info SONAME 'ha_tokudb.so';
+#INSTALL PLUGIN tokudb_fractal_tree_block_map SONAME 'ha_tokudb.so';
+#INSTALL PLUGIN tokudb_trx SONAME 'ha_tokudb.so';
+#INSTALL PLUGIN tokudb_locks SONAME 'ha_tokudb.so';
+#INSTALL PLUGIN tokudb_lock_waits SONAME 'ha_tokudb.so';
+#INSTALL PLUGIN tokudb_background_job_status SONAME 'ha_tokudb.so';
+#INSTALL PLUGIN audit_log SONAME 'audit_log.so';
+INSTALL PLUGIN mysqlx SONAME 'mysqlx.so';
