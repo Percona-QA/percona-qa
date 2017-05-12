d57cadbacf4cb7eff299193a7ac87b7bb4db844d
Roel Van de Paar <roel@vandepaar.com>
2017-05-12 21:38:37 +1000

    Bug update, plugin fixes, MYEXTRA fixes, RocksDB provisioning, pquery-run test 8.0 provisioning

--- plugins_56.sql
+++ plugins_56.sql
@@ -12,7 +12,6 @@ INSTALL PLUGIN mysql_no_login SONAME 'mysql_no_login.so';
 CREATE FUNCTION fnv1a_64 RETURNS INTEGER SONAME 'libfnv1a_udf.so';
 CREATE FUNCTION fnv_64 RETURNS INTEGER SONAME 'libfnv_udf.so';
 CREATE FUNCTION murmur_hash RETURNS INTEGER SONAME 'libmurmur_udf.so';
-# INSTALL PLUGIN tokudb SONAME 'ha_tokudb.so';  # Disabled, because pquery-run.sh preloads this (it does so to enable TokuDB --options to be used with mysqld) (if set in MYEXTRA)
 INSTALL PLUGIN tokudb_file_map SONAME 'ha_tokudb.so';
 INSTALL PLUGIN tokudb_fractal_tree_info SONAME 'ha_tokudb.so';
 INSTALL PLUGIN tokudb_fractal_tree_block_map SONAME 'ha_tokudb.so';
@@ -21,3 +20,6 @@ INSTALL PLUGIN tokudb_locks SONAME 'ha_tokudb.so';
 INSTALL PLUGIN tokudb_lock_waits SONAME 'ha_tokudb.so';
 INSTALL PLUGIN tokudb_background_job_status SONAME 'ha_tokudb.so';
 INSTALL PLUGIN audit_log SONAME 'audit_log.so';
+# INSTALL PLUGIN tokudb SONAME 'ha_tokudb.so';  # Disabled, because pquery-run.sh preloads this (it does so to enable TokuDB --options to be used with mysqld) (if set in MYEXTRA) ([re]moved from above tokudb_file_map to here because;)
+# This file has odd syntax requirements, ref https://bugs.mysql.com/bug.php?id=86303
+# Do not have remark lines at the top or middle of this file, only at the end
