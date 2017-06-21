9c394a970faa4727628363d28f2552e7fe88dc12
Shahriyar Rzayev <rzayev.shahriyar@yandex.com>
2017-06-21 16:22:36 +0400

    Fixed #107 (#112)
    * Finalizing this feature for issue #107

---
+++ pmm-tests/create_database.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+CLIENT_NAME=$1
+DB_COUNT=$2
+WORKDIR="${PWD}"
+MYSQL_USER=root
+
+
+if [[ "${CLIENT_NAME}" == "ps" ]]; then
+  BASEDIR=$(ls -1td ?ercona-?erver-5.* | grep -v ".tar" | head -n1)
+  BASEDIR="$WORKDIR/$BASEDIR"
+
+elif [[ "${CLIENT_NAME}" == "ms" ]]; then
+  BASEDIR=$(ls -1td mysql-5.* | grep -v ".tar" | head -n1)
+  BASEDIR="$WORKDIR/$BASEDIR"
+
+elif [[ "${CLIENT_NAME}" == "pxc" ]]; then
+  BASEDIR=$(ls -1td Percona-XtraDB-Cluster-5.* | grep -v ".tar" | head -n1)
+  BASEDIR="$WORKDIR/$BASEDIR"
+fi
+
+for i in $(sudo pmm-admin list | grep "mysql:metrics" | sed 's|.*(||;s|)||') ; do
+	MYSQL_SOCK=${i}
+  echo "Creating databases using MYSQL_SOCK=${MYSQL_SOCK}"
+  for num in $(seq 1 1 ${DB_COUNT}) ; do
+	    ${BASEDIR}/bin/mysql --user=${MYSQL_USER} --socket=${MYSQL_SOCK} -e "create database pmm_stress_test_${num}"
+  done
+done
