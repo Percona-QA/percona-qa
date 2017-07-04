ad280390b6af02ae84bec2863a4f326324fc095b
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-07-04 11:25:50 +0400

    NameError: global name 'randomized_intances' is not defined

--- pmm-tests/pmm_stress_test_py_tests/test_module01.py
+++ pmm-tests/pmm_stress_test_py_tests/test_module01.py
@@ -9,5 +9,5 @@ class TestPMMStress:
     def test_pmm_framework_add_client(self):
         """Checking return value from function"""
         print("\nIn test_pmm_framework_add_client()...")
-        return_value = randomized_intances.pmm_framework_add_client("ps", 3)
+        return_value = randomized_instances.pmm_framework_add_client("ps", 3)
         assert return_value == 0
