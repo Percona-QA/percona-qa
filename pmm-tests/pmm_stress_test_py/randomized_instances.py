45cf901a2681b0c39ac72125f294a5ae3107aac0
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-07-06 17:23:39 +0400

    Tried to fix python syntax error again

--- pmm-tests/pmm_stress_test_py/randomized_instances.py
+++ pmm-tests/pmm_stress_test_py/randomized_instances.py
@@ -356,7 +356,7 @@ def run_all(threads, instance_type,
             create_unique_query(create_unique_queries, instance_type)
         if insert_blobs:
             insert_blob(insert_blobs, instance_type)
-        if insert_longtexs:
+        if insert_longtexts:
             insert_longtext(insert_longtexts, instance_type)
 
 
