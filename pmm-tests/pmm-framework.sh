f02756955edebd2cc03416bc755b202433821d00
Shahriyar Rzayev <rzayev.shahriyar@yandex.com>
2017-07-12 14:02:12 +0400

    Preliminary fix for issue #139 (#42)

--- pmm-tests/pmm-framework.sh
+++ pmm-tests/pmm-framework.sh
@@ -831,7 +831,7 @@ clean_clients(){
    exit 1
   fi
   #Shutdown all mysql client instances
-  for i in $(sudo pmm-admin list | grep "mysql:metrics[ \t]*PS_NODE" | awk -F[\(\)] '{print $2}') ; do
+  for i in $(sudo pmm-admin list | grep "mysql:metrics[ \t].*_NODE" | awk -F[\(\)] '{print $2}') ; do
     echo -e "Shutting down mysql instance (--socket=${i})"
     ${MYSQLADMIN_CLIENT} -uroot --socket=${i} shutdown
     sleep 2
