c84f354c9d5724b1126754e5cbb3cf50cf8c3216
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-08-17 14:14:22 +0400

    Testing the directories

--- myrocks-tests/generated_columns.bats
+++ myrocks-tests/generated_columns.bats
@@ -1,3 +1,16 @@
 # Created by Shahriyar Rzayev from Percona
 
 # Bats tests for generated columns with MyRocks.
+
+# Will be passed from myrocks-testsuite.sh
+DIRNAME=$1
+BASEDIR=$2
+
+function execute_sql() {
+  # General function to pass sql statement to mysql client
+   ${DIRNAME}/${BASEDIR}/cl -e "$1"
+}
+
+@test "Adding virtual generated column" {
+  result=execute_sql "alter table generated_columns_test.sbtest1 add column json_test_v json generated always as (json_array(k,c,pad)) virtual;"
+}
--- myrocks-tests/myrocks-testsuite.sh
+++ myrocks-tests/myrocks-testsuite.sh
@@ -15,23 +15,50 @@ function clone_and_build() {
 }
 
 function run_startup() {
-  cd ${1}/${2}
+  cd $1/$2
   ~/percona-qa/startup.sh
 }
 
 function start_server() {
-  cd ${1}/${2}
-  ./start
+  $1/$2/start
+}
+
+function execute_sql() {
+  # General function to pass sql statement to mysql client
+   $1/$2/cl -e "$3"
 }
 
 # Run clone and build here
-clone_and_build ${DIRNAME}
+echo "Cloning and Building server from repo"
+#clone_and_build ${DIRNAME}
 
 # Get BASEDIR here
 BASEDIR=$(ls -1td PS* | grep -v ".tar" | grep PS[0-9])
+echo ${DIRNAME}
+echo ${BASEDIR}
 
 # Run startup.sh here
-run_startup ${DIRNAME} ${BASEDIR}
-
-# Start server here
-start_server ${DIRNAME} ${BASEDIR}
+# echo "Running startup.sh from percona-qa"
+# run_startup ${DIRNAME} ${BASEDIR}
+#
+# # Start server here
+# echo "Starting Server!"
+# start_server ${DIRNAME} ${BASEDIR}
+#
+# # Create sample database here
+# DB="create database generated_columns_test"
+# execute_sql ${DIRNAME} ${BASEDIR} ${DB}
+#
+# # Create sample table here
+# TABLE="CREATE TABLE generated_columns_test.sbtest1 (
+#   id int(11) NOT NULL AUTO_INCREMENT,
+#   k int(11) NOT NULL DEFAULT '0',
+#   c char(120) NOT NULL DEFAULT '',
+#   pad char(60) NOT NULL DEFAULT '',
+#   PRIMARY KEY (`id`)
+# ) ENGINE=InnoDB"
+# execute_sql ${DIRNAME} ${BASEDIR} ${TABLE}
+#
+# # Altering table engine to MyRocks here
+# ALTER="alter table generated_columns_test.sbtest1 engine=rocksdb"
+# execute_sql ${DIRNAME} ${BASEDIR} ${ALTER}
