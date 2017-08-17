28f1e4dada94f43b5f6c0a70b807ed9ddf888667
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-08-17 13:39:14 +0400

    Changing script to use functions for issue #159

--- myrocks-tests/myrocks-testsuite.sh
+++ myrocks-tests/myrocks-testsuite.sh
@@ -10,15 +10,28 @@ DIRNAME=$(dirname "$0")
 
 function clone_and_build() {
   git clone --recursive --depth=1 https://github.com/percona/percona-server.git -b 5.7 PS-5.7-trunk
-  cd ${DIRNAME}/PS-5.7-trunk
+  cd $1/PS-5.7-trunk
   ~/percona-qa/build_5.x_debug.sh
 }
 
-function startup() {
-  cd ${DIRNAME}/PS-5.7-trunk
+function run_startup() {
+  cd ${1}/${2}
   ~/percona-qa/startup.sh
 }
 
-clone_and_build
+function start_server() {
+  cd ${1}/${2}
+  ./start
+}
 
-startup
+# Run clone and build here
+clone_and_build ${DIRNAME}
+
+# Get BASEDIR here
+BASEDIR=$(ls -1td PS* | grep -v ".tar" | grep PS[0-9])
+
+# Run startup.sh here
+run_startup ${DIRNAME} ${BASEDIR}
+
+# Start server here
+start_server ${DIRNAME} ${BASEDIR}
