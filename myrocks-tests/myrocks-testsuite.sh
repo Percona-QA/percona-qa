129e9fb268cc5f6d1e31de3241bbbb3413865608
Shahriyar Rzayev <rzayev.shahriyar@yandex.com>
2017-08-21 20:37:22 +0400

    Bugfix issue160 (#163)
    * Preliminary fix for issue #160
    
    * Disabling ssl mode to not get an error
    
    * removing redundand code portion
    
    * Trying to fix SSL error
    
    * Trying to fix InterfaceError
    
    * Assertion failed assert(True == collection.exists_in_database())
    
    * Added the procedure to alter table engine inside Python code for issue #160
    
    * using session.sql()
    
    * Added try/except block to catch intented exception
    
    * Fixing syntax error
    
    * Fixing failing assertion
    
    * Printing actual exception message
    
    * Fixing assert message
    
    * Catching exception message
    
    * Catching exception message n2
    
    * Finalizing the issue implementation

--- myrocks-tests/myrocks-testsuite.sh
+++ myrocks-tests/myrocks-testsuite.sh
@@ -33,6 +33,7 @@ function execute_sql() {
 }
 
 function run_generated_columns_test() {
+  # Calling bats file
   if [[ $tap == 1 ]] ; then
     bats --tap $DIRNAME/generated_columns.bats
   else
@@ -41,11 +42,29 @@ function run_generated_columns_test() {
 }
 
 function run_json_test() {
+  # Calling bats file
   if [[ $tap == 1 ]] ; then
     bats --tap $DIRNAME/json.bats
   else
     bats $DIRNAME/json.bats
   fi
+}
+
+function install_mysql_connector() {
+  # Downloading rpm package for CentOS 7
+  # For now installing it globally
+  # TODO: Install this package inside Python virtualenv to not affect whole system globally...
+  IF_INSTALLED=$(rpm -qa | grep mysql-connector-python-8.0)
+  if [ -z $IF_INSTALLED ] ; then
+    wget https://dev.mysql.com/get/Downloads/Connector-Python/mysql-connector-python-8.0.4-0.1.dmr.el7.x86_64.rpm
+    sudo yum install mysql-connector-python-8.0.4-0.1.dmr.el7.x86_64.rpm
+  else
+    echo "Already Installed"
+  fi
+}
+
+function run_mysqlx_plugin_test() {
+  python $DIRNAME/myrocks_mysqlx_plugin.py
 }
 
 # Run clone and build here
@@ -95,3 +114,26 @@ run_generated_columns_test
 # Calling json.bats file here
 echo "Running json.bats"
 run_json_test
+
+# Installing mysql-connector-python
+echo "Installing mysql-connector-python"
+install_mysql_connector
+
+# Installing mysqlx plugin
+echo "Installing mysqlx plugin"
+MYSQLX="INSTALL PLUGIN mysqlx SONAME 'mysqlx.so'"
+execute_sql ${BASEDIR} "${MYSQLX}"
+
+# Creating user for X Plugin tests
+echo "Creating sample user"
+USER="create user bakux@localhost identified by 'Baku12345'"
+execute_sql ${BASEDIR} "${USER}"
+
+# Giving "all" grants for new user
+echo "Granting sample user"
+GRANT="grant all on *.* to bakux@localhost"
+execute_sql ${BASEDIR} "${GRANT}"
+
+# Calling myrocks_mysqlx_plugin.py file here
+echo "Running X Plugin test"
+run_mysqlx_plugin_test
---
+++ myrocks-tests/myrocks_mysqlx_plugin.py
@@ -0,0 +1,39 @@
+# Connecting to MySQL and working with a Session
+import mysqlx
+
+# Connect to a dedicated MySQL server
+session = mysqlx.get_session({
+    'host': 'localhost',
+    'port': 33060,
+    'user': 'bakux',
+    'password': 'Baku12345',
+    'ssl-mode': mysqlx.SSLMode.DISABLED
+})
+
+schema = session.get_schema('generated_columns_test')
+
+# Create 'my_collection' in schema
+print "Creating collection"
+schema.create_collection('my_collection')
+
+# Get 'my_collection' from schema
+collection = schema.get_collection('my_collection')
+print "Checking assert(True == collection.exists_in_database())"
+assert(True == collection.exists_in_database())
+
+# You can also add multiple documents at once
+print "Inserting 3 rows into collection"
+collection.add({'_id': '2', 'name': 'Sakila', 'age': 15},
+            {'_id': '3', 'name': 'Jack', 'age': 15},
+            {'_id': '4', 'name': 'Clare', 'age': 37}).execute()
+
+collection.remove('_id = 1').execute()
+print "Checking assert(3 == collection.count())"
+assert(3 == collection.count())
+
+print "Altering default collection engine from InnoDB to MyRocks [Should raise an OperationalError]"
+try:
+    sql = session.sql("alter table generated_columns_test.my_collection engine=rocksdb")
+    sql.execute()
+except mysqlx.errors.OperationalError as e:
+    print e
