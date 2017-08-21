29eff119c16fd8e0bcac1dfd89cb160319daa413
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-08-21 19:59:36 +0400

    Added the procedure to alter table engine inside Python code for issue #160

--- myrocks-tests/myrocks_mysqlx_plugin.py
+++ myrocks-tests/myrocks_mysqlx_plugin.py
@@ -13,18 +13,24 @@ session = mysqlx.get_session({
 schema = session.get_schema('generated_columns_test')
 
 # Create 'my_collection' in schema
+print "Creating collection"
 schema.create_collection('my_collection')
 
 # Get 'my_collection' from schema
 collection = schema.get_collection('my_collection')
-
+print "assert(True == collection.exists_in_database())"
 assert(True == collection.exists_in_database())
 
 # You can also add multiple documents at once
+print "Inserting 3 rows into collection"
 collection.add({'_id': '2', 'name': 'Sakila', 'age': 15},
             {'_id': '3', 'name': 'Jack', 'age': 15},
             {'_id': '4', 'name': 'Clare', 'age': 37}).execute()
 
 collection.remove('_id = 1').execute()
-
+print "assert(3 == collection.count())"
 assert(3 == collection.count())
+
+print "Altering default collection engine from InnoDB to MyRocks"
+sql = mysqlx.SqlStatement(session, "alter table generated_columns_test.my_collection engine=rocksdb")
+sql.execute()
