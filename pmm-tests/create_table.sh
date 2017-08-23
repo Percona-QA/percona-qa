5a6f7570954e39509b4e3bce8cae15e446c0dd3f
Shahriyar Rzayev <rzayev.shahriyar@yandex.com>
2017-08-23 19:22:07 +0400

    Bugfix issue168 (#169)
    * Preliminary fix for issue #168
    
    * Trying to exclude other nodes for creating tables for issue #168
    
    * Finalizing  the feature

--- pmm-tests/create_table.sh
+++ pmm-tests/create_table.sh
@@ -18,11 +18,20 @@ elif [[ "${CLIENT_NAME}" == "pxc" ]]; then
   BASEDIR="$WORKDIR/$BASEDIR"
 fi
 
-for i in $(sudo pmm-admin list | grep 'mysql:metrics[ \t]*PS_NODE' | awk -F[\(\)] '{print $2}') ; do
-	MYSQL_SOCK=${i}
+if [[ "${CLIENT_NAME}" == "pxc" ]]; then
+  MYSQL_SOCK=$(sudo pmm-admin list | grep 'mysql:metrics[ \t].*_NODE-' | awk -F[\(\)] '{print $2}' | head -n 1)
   echo "Creating tables using MYSQL_SOCK=${MYSQL_SOCK}"
   ${BASEDIR}/bin/mysql --user=${MYSQL_USER} --socket=${MYSQL_SOCK} -e "create database pmm_stress_test"
   for num in $(seq 1 1 ${TABLE_COUNT}) ; do
 	    ${BASEDIR}/bin/mysql --user=${MYSQL_USER} --socket=${MYSQL_SOCK} -e "create table pmm_stress_test.t${num}(id int not null)"
   done
-done
+else  
+  for i in $(sudo pmm-admin list | grep 'mysql:metrics[ \t].*_NODE-' | awk -F[\(\)] '{print $2}') ; do
+  	MYSQL_SOCK=${i}
+    echo "Creating tables using MYSQL_SOCK=${MYSQL_SOCK}"
+    ${BASEDIR}/bin/mysql --user=${MYSQL_USER} --socket=${MYSQL_SOCK} -e "create database pmm_stress_test"
+    for num in $(seq 1 1 ${TABLE_COUNT}) ; do
+  	    ${BASEDIR}/bin/mysql --user=${MYSQL_USER} --socket=${MYSQL_SOCK} -e "create table pmm_stress_test.t${num}(id int not null)"
+    done
+  done
+fi
