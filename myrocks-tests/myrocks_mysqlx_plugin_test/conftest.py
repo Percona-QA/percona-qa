a72557bbe02d42fd618af550ef91832e90a3b78f
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-08-24 14:08:19 +0400

    The fix for import error

--- myrocks-tests/myrocks_mysqlx_plugin_test/conftest.py
+++ myrocks-tests/myrocks_mysqlx_plugin_test/conftest.py
@@ -1,5 +1,5 @@
 from myrocks_mysqlx_plugin.myrocks_mysqlx_plugin import MyXPlugin
-
+import pytest
 # schema_name = "generated_columns_test"
 # collection_name = "my_collection"
 plugin_obj = MyXPlugin("generated_columns_test", "my_collection")
