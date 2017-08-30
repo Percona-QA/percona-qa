c062699a3907e97117d981338efbc34aed6600d3
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-08-30 10:42:14 +0400

    Changed back to check_command and printing the intended output

--- myrocks-tests/myrocks_mysqlx_plugin/myrocks_mysqlsh.py
+++ myrocks-tests/myrocks_mysqlx_plugin/myrocks_mysqlsh.py
@@ -9,12 +9,13 @@ def mysqlsh_db_get_collections(user, passw, port):
     command = "mysqlsh {}:{}@localhost/generated_columns_test --port={} --py --interactive --execute 'db.get_collections()'"
     new_command = command.format(user, passw, port)
     try:
-        process = Popen(
-                    split(new_command), stdout=PIPE)
-        output, error = process.communicate()
-        print "The output:"
-        print output.split()
-        # prc = check_output(new_command, shell=True)
+        # process = Popen(
+        #             split(new_command), stdout=PIPE)
+        # output, error = process.communicate()
+        # print "The output:"
+        # print output.split()
+        prc = check_output(new_command, shell=True)
+        print prc
         # print prc.split()
     except Exception as e:
         print(e)
