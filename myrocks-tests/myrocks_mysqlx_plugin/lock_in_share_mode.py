40ced6330c9032be0da29f3303b1bc30ef23b935
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-08-31 18:59:59 +0400

    Preliminary fix for issue #184

---
+++ myrocks-tests/myrocks_mysqlx_plugin/lock_in_share_mode.py
@@ -0,0 +1,68 @@
+import mysqlx
+
+class MyXPluginLocks:
+    # The Class for using X Plugin to run SQL statements
+    def __init__(self, schema_name, table_name):
+        # Connect to a dedicated MySQL server
+        self.session = mysqlx.get_session({
+            'host': 'localhost',
+            'port': 33060,
+            'user': 'bakux',
+            'password': 'Baku12345',
+            'ssl-mode': mysqlx.SSLMode.DISABLED
+        })
+
+
+    def __del__(self):
+        self.session.close()
+
+    def create_schema(self, schema_name):
+        # Creating schema
+        self.db_obj = self.session.create_schema(schema_name)
+        return self.db_obj.get_name()
+
+    def create_table(self, schema_name, table_name):
+        try:
+            command = "create table {}.{}(id int, name varchar)"
+            sql = self.session.sql(command.format(schema_name, table_name))
+            sql.execute()
+        except Exception as e:
+            raise
+        else:
+            return 0
+
+    def insert_dummy_data_into_table(self, schema_name, table_name, value_id, value_name):
+        try:
+            command = "insert into {}.{}(id, name) values({},{})"
+            sql = self.session.sql(command.format(schema_name, table_name, value_id, value_name))
+            sql.execute()
+        except Exception as e:
+            raise
+        else:
+            return 0
+
+    def run_lock_in_share_select(self, schema_name, table_name, value_id):
+        try:
+            command = "select name from {}.{} where id={} LOCK IN SHARE MODE"
+            sql = self.session.sql(command.format(schema_name, table_name, value_id))
+            sql.execute()
+        except Exception as e:
+            raise
+        else:
+            return 0
+
+    def run_update_statement(self, schema_name, table_name, value_id, value_name):
+        try:
+            command = "update {}.{} set name={} where id={}"
+            sql = self.session.sql(command.format(schema_name, table_name, value_name, value_id))
+            sql.execute()
+        except Exception as e:
+            raise
+        else:
+            return 0
+
+    def start_transaction(self):
+        self.session.start_transaction()
+
+    def commit_transaction(self):
+        self.session.commit()
