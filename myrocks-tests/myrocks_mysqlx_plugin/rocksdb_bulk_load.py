11a5eaaec72c3bb8cd534433f978fd0680447554
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-09-15 18:57:13 +0400

    Finalizing the feature implementation

--- myrocks-tests/myrocks_mysqlx_plugin/rocksdb_bulk_load.py
+++ myrocks-tests/myrocks_mysqlx_plugin/rocksdb_bulk_load.py
@@ -21,6 +21,7 @@ class RocksBulk:
         self.session.commit()
 
     def run_set_sql_log_bin(self, value):
+        # Setting sql log bin
         try:
             command = "SET session sql_log_bin={}"
             sql = self.session.sql(command.format(value))
@@ -31,6 +32,7 @@ class RocksBulk:
             return 0
 
     def run_set_rocksdb_bulk_load(self, value):
+        # Setting rocksdb bulk load
         try:
             command = "SET session rocksdb_bulk_load={}"
             sql = self.session.sql(command.format(value))
@@ -41,6 +43,7 @@ class RocksBulk:
             return 0
 
     def run_alter_drop_foreign_key(self, schema_name, table_name):
+        # Droppping foreign key from salaries table
         try:
             command = "alter table {}.{} drop foreign key salaries_ibfk_1"
             sql = self.session.sql(command.format(schema_name, table_name))
@@ -51,6 +54,7 @@ class RocksBulk:
             return 0
 
     def run_alter_storage_engine(self, schema_name, table_name, engine):
+        # Altering storage engine
         try:
             command = "alter table {}.{} engine={}"
             sql = self.session.sql(command.format(schema_name, table_name, engine))
@@ -61,6 +65,7 @@ class RocksBulk:
             return 0
 
     def run_create_table(self, schema_name, table_name, from_schema_name, from_table_name):
+        # Creating table from given table
         try:
             command = "create table {}.{} like {}.{}"
             sql = self.session.sql(command.format(schema_name, table_name, from_schema_name, from_table_name))
@@ -71,6 +76,7 @@ class RocksBulk:
             return 0
 
     def run_insert_statement(self, schema_name, table_name, emp_no, from_date):
+        # Running insert statement
         try:
             command = "insert into {}.{} select * from {}.salaries where emp_no={} and from_date='{}'"
             sql = self.session.sql(command.format(schema_name, table_name, schema_name, emp_no, from_date))
@@ -81,6 +87,7 @@ class RocksBulk:
             return 0
 
     def run_select_statement(self, schema_name, table_name):
+        # Selecting count()
         try:
             command = "select count(*) from {}.{}"
             sql = self.session.sql(command.format(schema_name, table_name))
