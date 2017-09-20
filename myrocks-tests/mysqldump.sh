de2d841adcba3ba14faa7e200e125bac67912a15
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-09-20 11:43:14 +0400

    Trying to spot the user name from cl_noprompt_binary

--- myrocks-tests/mysqldump.sh
+++ myrocks-tests/mysqldump.sh
@@ -4,3 +4,12 @@
 
 WORKDIR="${PWD}"
 DIRNAME=$(dirname "$0")
+
+BASEDIR=$(ls -1td ${WORKDIR}/PS* | grep -v ".tar" | grep PS[0-9])
+
+function extract_user() {
+  user_conn="$(cat $1/cl_noprompt) | awk {'print $3'}"
+  echo ${user_conn}
+}
+
+extract_user $BASEDIR
