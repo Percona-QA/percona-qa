d4b7caa266d1c616a0ad948e252b704f8f3dd7c3
Tomislav Plavcic <tomislav.plavcic@percona.com>
2017-09-20 15:09:26 +0200

    Update filters for QC

--- qc_filter.sh
+++ qc_filter.sh
@@ -34,6 +34,14 @@ move
 DIRLIST=$(grep -i "ERROR.*1105.*Unique index support is disabled when the table has no primary key" */pquery_thread-0.RocksDB.sql|awk -F '/' '{print $1}'|sort -u)
 move
 
+# MyRocks doesn't support generated columns
+DIRLIST=$(grep -i "ERROR.*3106.*is not supported for generated columns" */pquery_thread-0.RocksDB.sql|awk -F '/' '{print $1}'|sort -u)
+move
+
+# MyRocks doesn't support native partitioning
+DIRLIST=$(grep -i "Please use native partitioning instead." */pquery_thread-0.RocksDB.out|awk -F '/' '{print $1}'|sort -u)
+move
+
 # MyRocks doesn't support Gap Lock
 DIRLIST=$(grep -i "ERROR: 1105 - Using Gap Lock" */pquery_thread-0.RocksDB.sql|awk -F '/' '{print $1}'|sort -u)
 move
