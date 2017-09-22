fe0aa19e9dbaabfdff67b5d1d63230e76e078d00
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-09-22 18:30:04 +0400

    Adding command into try/except block

--- pmm-tests/pmm_stress_test_py/randomized_instances.py
+++ pmm-tests/pmm_stress_test_py/randomized_instances.py
@@ -304,9 +304,9 @@ def clean_env(i_type):
     abspath = os.path.abspath(__file__)
     dname = os.path.dirname(abspath)
     for sock in sockets:
-        get_databases = '{}/get_databases.sh {} {}'
-        get_db_list = get_databases.format(dname[:-18], i_type, sock)
         try:
+            get_databases = '{}/get_databases.sh {} {}'
+            get_db_list = get_databases.format(dname[:-18], i_type, sock)
             prc = check_output(get_db_list, shell=True)
             result_databases = prc.split()[1:]
             drop_databases = '{}/drop_databases.sh {} {} {}'
