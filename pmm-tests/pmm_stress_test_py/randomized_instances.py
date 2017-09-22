5c1f0ebae2ac2ab56e452c19da634f47c335c4fd
Shahriyar Rzayev <rzayev.shahriyar@yandex.com>
2017-09-22 20:22:55 +0400

    Bugfix issue205 (#206)
    * Trying to spot the reason of fail
    
    * Trying to fix passed arguments
    
    * Trying to fix failed python
    
    * Finalizing the bug fix

--- pmm-tests/pmm_stress_test_py/randomized_instances.py
+++ pmm-tests/pmm_stress_test_py/randomized_instances.py
@@ -180,7 +180,7 @@ def creating_sleep_query(sock, i_type, query_count):
 
     abspath = os.path.abspath(__file__)
     dname = os.path.dirname(abspath)
-    bash_command = '{}/create_sleep_queries.sh {} {} {}'
+    bash_command = '{}create_sleep_queries.sh {} {} {}'
     new_command = bash_command.format(dname[:-18], i_type, query_count, sock)
     try:
          process = Popen(
@@ -379,7 +379,7 @@ def print_version(ctx, param, value):
     help="How many tables to create per added instance for stress test?")
 @click.option(
     "--create_sleep_queries",
-    nargs=3,
+    nargs=2,
     help="How many 'select sleep()' queries to run? 1->query count, 2->instance type, 3->thread count")
 @click.option(
     "--create_unique_queries",
@@ -442,7 +442,7 @@ def run_all(threads, instance_type,
                 if create_tables:
                     create_table(create_tables, instance_type)
                 if create_sleep_queries:
-                    run_sleep_query(int(create_sleep_queries[0]), str(create_sleep_queries[1]), int(create_sleep_queries[2]))
+                    run_sleep_query(int(create_sleep_queries[0]), instance_type, int(create_sleep_queries[1]))
                 if create_unique_queries:
                     create_unique_query(create_unique_queries, instance_type)
                 if insert_blobs:
