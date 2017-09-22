bf520835759e084b06c0ccd17dc3030dd89ce0f0
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-09-22 19:03:48 +0400

    Finalizing the bug fix

--- pmm-tests/create_database.sh
+++ pmm-tests/create_database.sh
@@ -33,12 +33,3 @@ else
     done
   done
 fi
-
-
-# for i in $(sudo pmm-admin list | grep 'mysql:metrics[ \t]*PS_NODE' | awk -F[\(\)] '{print $2}') ; do
-# 	MYSQL_SOCK=${i}
-#   echo "Creating databases using MYSQL_SOCK=${MYSQL_SOCK}"
-#   for num in $(seq 1 1 ${DB_COUNT}) ; do
-# 	    ${BASEDIR}/bin/mysql --user=${MYSQL_USER} --socket=${MYSQL_SOCK} -e "create database pmm_stress_test_${num}"
-#   done
-# done
