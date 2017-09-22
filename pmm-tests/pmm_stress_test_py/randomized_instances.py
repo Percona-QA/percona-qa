7c2e86a73bd6708685b54f4e29b6031e956ffdca
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-09-22 18:52:03 +0400

    Finalizing bug

--- pmm-tests/pmm_stress_test_py/randomized_instances.py
+++ pmm-tests/pmm_stress_test_py/randomized_instances.py
@@ -307,14 +307,11 @@ def clean_env(i_type):
         try:
             get_databases = '{}get_databases.sh {} {}'
             get_db_list = get_databases.format(dname[:-18], i_type, sock)
-            print get_db_list
             prc = check_output(get_db_list, shell=True)
             result_databases = prc.split()[1:]
-            print result_databases
             drop_databases = '{}drop_databases.sh {} {} {}'
             for database in result_databases:
                 drop_db = drop_databases.format(dname[:-18], i_type, sock, database)
-                print drop_db
                 process = Popen(
                                 split(drop_db),
                                 stdin=None,
