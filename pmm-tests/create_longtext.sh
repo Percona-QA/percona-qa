9ca056d6cb5da5da5b3e8aca7241845dbc222df6
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-09-25 16:20:36 +0400

    Added primary key to dummy table

--- pmm-tests/create_longtext.sh
+++ pmm-tests/create_longtext.sh
@@ -43,7 +43,7 @@ if [[ "${CLIENT_NAME}" == "pxc" ]]; then
   echo "Create database using MYSQL_SOCK=${MYSQL_SOCK}"
   ${BASEDIR}/bin/mysql --user=${MYSQL_USER} --socket=${MYSQL_SOCK} -e "create database pmm_stress_longtext_test"
   echo "Create table using MYSQL_SOCK=${MYSQL_SOCK}"
-  ${BASEDIR}/bin/mysql --user=${MYSQL_USER} --socket=${MYSQL_SOCK} -e "create table pmm_stress_longtext_test.t_longtext(id int not null, ltext longtext)"
+  ${BASEDIR}/bin/mysql --user=${MYSQL_USER} --socket=${MYSQL_SOCK} -e "create table pmm_stress_longtext_test.t_longtext(id int not null, ltext longtext, primary key(id))"
   for num in $(seq 1 1 ${INSERT_COUNT}) ; do
       echo "Inserting long text into table"
 	    ${BASEDIR}/bin/mysql --user=${MYSQL_USER} --socket=${MYSQL_SOCK} -e "insert into pmm_stress_longtext_test.t_longtext(id, ltext) values(1, '${str}')"
