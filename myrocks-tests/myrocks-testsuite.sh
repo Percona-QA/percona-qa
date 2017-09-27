ae499d395fa11f4d53d69493511321781654fe27
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-09-27 19:45:54 +0400

    Fixing the insert statement

--- myrocks-tests/myrocks-testsuite.sh
+++ myrocks-tests/myrocks-testsuite.sh
@@ -337,7 +337,7 @@ CRTTBL="CREATE TABLE proxysql_test_db.sbtest1 (
 mysql --user=root --host=localhost --port=6033 --protocol=tcp -e "${CRTTBL}"
 
 echo "Inserting dummy data into this table over ProxySQL"
-INSRT="insert into proxysql_test_db.sbtest1(id, pad) values('We are the warriors of true!')"
+INSRT="insert into proxysql_test_db.sbtest1(pad) values('We are the warriors of true!')"
 mysql --user=root --host=localhost --port=6033 --protocol=tcp -e "${INSRT}"
 
 echo "Running proxysql.bats"
