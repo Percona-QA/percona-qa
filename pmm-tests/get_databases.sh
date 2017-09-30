3b4e67e82b5640813ef5c62c96803b77889c3bf9
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-09-30 16:29:28 +0400

    Wrong BASEDIR setting

--- pmm-tests/get_databases.sh
+++ pmm-tests/get_databases.sh
@@ -8,15 +8,26 @@ MYSQL_USER=root
 
 if [[ "${CLIENT_NAME}" == "ps" ]]; then
   BASEDIR=$(ls -1td ?ercona-?erver-5.* | grep -v ".tar" | head -n1)
-  BASEDIR="$WORKDIR/$BASEDIR"
-
+  if [[ -z "$BASEDIR" ]]; then
+    echo "Started fresh run!"
+  else
+    BASEDIR="$WORKDIR/$BASEDIR"
+  fi
 elif [[ "${CLIENT_NAME}" == "ms" ]]; then
   BASEDIR=$(ls -1td mysql-5.* | grep -v ".tar" | head -n1)
-  BASEDIR="$WORKDIR/$BASEDIR"
+  if [[ -z "$BASEDIR" ]]; then
+    echo "Started fresh run!"
+  else
+    BASEDIR="$WORKDIR/$BASEDIR"
+  fi
 
 elif [[ "${CLIENT_NAME}" == "pxc" ]]; then
   BASEDIR=$(ls -1td Percona-XtraDB-Cluster-5.* | grep -v ".tar" | head -n1)
-  BASEDIR="$WORKDIR/$BASEDIR"
+  if [[ -z "$BASEDIR" ]]; then
+    echo "Started fresh run!"
+  else
+    BASEDIR="$WORKDIR/$BASEDIR"
+  fi
 fi
 
 echo "echoing basedir"
