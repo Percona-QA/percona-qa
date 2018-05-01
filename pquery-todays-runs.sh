098ff363b1a0ae6dcf4ca387cc1d0544c955a1c1
Roel Van de Paar <roel.vandepaar@percona.com>
2018-05-02 07:41:15 +1000

    pquery-todays-runs.sh bug fix for single digit days, handy_gnu.txt update

--- pquery-todays-runs.sh
+++ pquery-todays-runs.sh
@@ -1,1 +1,1 @@
-date | awk '{print $2" "$3}' | xargs -I{} sh -c 'ls -ld [0-9]* | grep "{}"'
+date | awk '{print $2" "$3}' | sed 's|\([a-z]\) \([0-9]\)$|\1  \2|' | xargs -I{} sh -c 'ls -ld [0-9]* | grep "{}"'
