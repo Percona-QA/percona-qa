d87103cbd1d7304f246512953e4dca77f63478ba
Roel Van de Paar <roel.vandepaar@percona.com>
2018-09-22 17:52:38 +1000

    Many bug updates (mainly 8.0), handy_gnu update, Improved prs homedir script to report issues better & more bug-free, Improved ts homedir script to auto-see trial dirs, Many improvements and fixes to pquery-reach++.sh and pquery-reach.sh, added tsm and tsm+ (ts-multi and ts-multi search) homedir scripts

--- pquery-reach.sh
+++ pquery-reach.sh
@@ -8,8 +8,10 @@ THREADS=1
 WORKDIR=/dev/shm
 COPYDIR=/sda
 STATIC_PQUERY_BIN=/home/roel/percona-qa/pquery/pquery2-ps8  # Leave empty to use a random binary, i.e. percona-qa/pquery/pquery*
-MYEXTRA="--early-plugin-load=keyring_file.so --keyring_file_data=keyring --innodb_sys_tablespace_encrypt=ON"  # Variables to add to pquery run
-MYMID="--early-plugin-load=keyring_file.so --keyring_file_data=keyring --innodb_sys_tablespace_encrypt=ON"    # Variables to add to MID (MySQL init) (changes INIT_OPT in pquery-run.sh)
+#MYINIT="--early-plugin-load=keyring_file.so --keyring_file_data=keyring --innodb_sys_tablespace_encrypt=ON"    # Variables to add to MID (MySQL init) (changes INIT_OPT in pquery-run.sh)
+#MYEXTRA="--early-plugin-load=keyring_file.so --keyring_file_data=keyring --innodb_sys_tablespace_encrypt=ON"  # Variables to add to pquery run
+MYINIT=
+MYEXTRA=
 EARLYCOPY=0  # Make a copy to the COPYDIR before starting reducer. Not strictly required, but handy if your machine may power off and you were using /dev/shm as WORKDIR
 
 # Internal variables: Do not change!
@@ -45,9 +47,9 @@ ctrl-c(){
 }
 
 # Make sure directories set in vars are there
-if [ ! -d "${BASEDIR}" ]; then echo "Assert! Basedir ($BASEDIR) is not a directory!"; exit 1; fi
-if [ ! -d "${WORKDIR}" ]; then echo "Assert! Workdir ($WORKDIR) is not a directory!"; exit 1; fi
-if [ ! -d "${COPYDIR}" ]; then echo "Assert! Copydir ($COPYDIR) is not a directory!"; exit 1; fi
+if [ ! -d "${BASEDIR}" ]; then echo "Assert! Basedir ($BASEDIR) does not exist or is not a directory!"; exit 1; fi
+if [ ! -d "${WORKDIR}" ]; then echo "Assert! Workdir ($WORKDIR) does not exist or is not a directory!"; exit 1; fi
+if [ ! -d "${COPYDIR}" ]; then echo "Assert! Copydir ($COPYDIR) does not exist or is not a directory!"; exit 1; fi
 
 # Make sure we've got all items we need
 if [ ! -r "${SCRIPT_PWD}/reducer.sh" ];            then echo "Assert! reducer.sh not found!"; exit 1; fi
@@ -85,7 +87,7 @@ pquery_run(){
   # pquery-run.sh setup and run
   echoit "Setting up new pquery-run.sh configuration file at ${PQUERY_CONF}..."
   cat ${SCRIPT_PWD}/pquery-run.sh |
-   sed "s|^INIT_OPT=\"|INIT_OPT=\"${MYMID} |" | \
+   sed "s|^INIT_OPT=\"|INIT_OPT=\"${MYINIT} |" | \
    sed "s|\${SCRIPT_PWD}/generator|${SCRIPT_PWD}/generator|g" | \
    sed "s|\${SCRIPT_PWD}/text_string.sh|${SCRIPT_PWD}/text_string.sh|g" | \
    sed "s|\${SCRIPT_PWD}/valgrind_string.sh|${SCRIPT_PWD}/valgrind_string.sh|g" | \
@@ -149,6 +151,9 @@ PQUERY_REACH_COPIED_LOG=${COPYDIR}/${RANDOMR}/${RANDOMD}_pquery-reach.log  # Use
 touch ${PQUERY_REACH_LOG}
 echoit "pquery-reach.sh (PID $$) main working directory: ${WORKDIR} | Logfile: ${PQUERY_REACH_LOG}"
 echoit "pquery-run.sh working directory: ${PQR_WORKDIR} | pquery-run run directory: ${PQR_RUNDIR}"
+echoit "Base directory: ${BASEDIR} | Copy directory: ${COPYDIR} | Work directory: ${WORKDIR}"
+echoit "MYINIT: ${MYINIT}"
+echoit "MYEXTRA: ${MYEXTRA}"
 
 # Main Loop
 while true; do
@@ -165,7 +170,13 @@ while true; do
       if [ -r ${PQR_WORKDIR}/reducer1.sh ]; then
         if grep -qi "^MODE=3" ${PQR_WORKDIR}/reducer1.sh; then
           echoit "New, and specific (MODE=3) bug found! Reducing the same..."
-          sed -i "s|^[ \t]*INIT_OPT=\"|INIT_OPT=\"${MYMID} |" ${PQR_WORKDIR}/reducer1.sh  # Semi-hack to get things like "--early-plugin-load=keyring_file.so --keyring_file_data=keyring --innodb_sys_tablespace_encrypt=ON" in [MY]MID/INIT_OPT to work
+          # Update pquery-reach++.sh found counter (which is reset to 0 on each run of pquery-reach++.sh), if used
+          if [ -r /tmp/pqr_status.cnt ]; then
+            CURRENT_COUNT=$(cat /tmp/pqr_status.cnt)
+            CURRENT_COUNT=$[ $CURRENT_COUNT + 1 ]
+            echo "$CURRENT_COUNT" > /tmp/pqr_status.cnt
+          fi
+          sed -i "s|^[ \t]*INIT_OPT=\"|INIT_OPT=\"${MYINIT} |" ${PQR_WORKDIR}/reducer1.sh  # Semi-hack to get things like "--early-plugin-load=keyring_file.so --keyring_file_data=keyring --innodb_sys_tablespace_encrypt=ON" in [MY]MID/INIT_OPT to work
           # Approximately matching pquery-go-expert.sh settings, with some improvements
           sed -i "s|^FORCE_SKIPV=0|FORCE_SKIPV=1|" ${PQR_WORKDIR}/reducer1.sh  # Setting this means the script will not terminate in some cases; it's a weigh off between STAGE1_LINES (13 set below) being reached in most cases (in which case this script WILL terminate and finish as it will go through the other STAGEs in reducer) or it not being reached (>13 lines left in the testcase in MULTI reduction mode), in which case the script indeed will not terminate as it will stay in MULTI reducion mode.
           sed -i "s|^MULTI_THREADS=[0-9]\+|MULTI_THREADS=3 |" ${PQR_WORKDIR}/reducer1.sh
@@ -181,7 +192,8 @@ while true; do
           ${PQR_WORKDIR}/reducer1.sh | tee -a ${PQUERY_REACH_LOG}
           REDUCER_EXIT_STATUS=${PIPESTATUS[0]}  # With thanks, https://unix.stackexchange.com/a/14276/241016
           echoit "=================================================================================================================="
-          REDUCER_WORKDIR=$(grep '\[Init\] Workdir' ${PQUERY_REACH_LOG} | sed "s|.*:[ \t]*||")
+          # Figure out the reducer workdir, which is stored in the pquery reach log by searching for a reducer-specific text output
+          REDUCER_WORKDIR=$(grep '\[Init\] Workdir' ${PQUERY_REACH_LOG} | sed "s|.*:[ \t]*||")  # "[Init] Workdir" is from reducer
           # Fix reducer so it points now to the COPYDIR instead of the WORKDIR, just before the copy
           # This makes it easier to re-run reducer1.sh directly from the COPYDIR after WORKDIR has been deleted automatically
           sed -i "s|^INPUTFILE=\"${ORIGINAL_WORKDIR}|INPUTFILE=\"${COPYDIR}|" ${PQR_WORKDIR}/reducer1.sh
@@ -212,10 +224,11 @@ while true; do
                   echoit "Copy complete. Review ${COPYDIR}/${RANDOMR}/${RANDOMD}/1 for testcase location (assert: this should not really have happened; the testcase was not found?)"
                 fi
               fi
+              rm -Rf ${REDUCER_WORKDIR}  # Cleanup reducer workdir
               echoit "pquery-reach.sh complete, new bug found and reduced! Exiting normally..."
               exit 0
             else
-              echoit "pquery-reach.sh complete, new bug found and reduced! Copy to copy dir failed (minor). Exiting with status 1..."
+              echoit "pquery-reach.sh complete, new bug found and reduced! Howerver, the copy of the work dir (${WORKDIR}) to the copy dir (${COPYDIR}) failed. Thus, not deleting the workdir nor the reducer directory (${REDUCER_WORKDIR}). Exiting with status 1..."
               exit 1
             fi
             exit 3  # Defensive coding only, exit as 0 or 1 should happen just above
@@ -224,7 +237,7 @@ while true; do
             if [ ${COPY_RESULT} -eq 1 ]; then
               echoit "pquery-reach.sh complete, new bug found and reducer attempted (but was not successful). Exiting with status 1..."
             else
-              echoit "pquery-reach.sh complete, new bug found and reducer attempted (but was not successful). Copy to copy dir failed (minor). Two issues found. Exiting with status 1..."
+              echoit "pquery-reach.sh complete, new bug found and reduced! Howerver, the copy of the work dir (${WORKDIR}) to the copy dir (${COPYDIR}) failed. Thus, not deleting the workdir nor the reducer directory (${REDUCER_WORKDIR}). Exiting with status 1..."
             fi
             exit 1
           fi
@@ -233,6 +246,7 @@ while true; do
           if [ -r ${PQR_WORKDIR}/1/log/master.err ]; then
             if grep -qi "nknown variable" ${PQR_WORKDIR}/1/log/master.err; then
               echoit "This run had an invalid/unknown mysqld variable (dud), cleaning up & trying again..."
+              cleanup
               continue
             else
               if [ `ls ${PQR_WORKDIR}/1/data/*core* 2>/dev/null | wc -l` -lt 1 ]; then
@@ -240,7 +254,7 @@ while true; do
                 cleanup
                 continue
               else
-                echoit "No error log AND no core found. Odd... TODO"
+                echoit "No error log AND no core found. Odd... (out of space perhaps?) TODO"
                 exit 1
               fi
             fi
