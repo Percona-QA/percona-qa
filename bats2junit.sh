1047c544619da53d422e7694975b9b59c7c924ff
Tomislav Plavcic <tplavcic@gmail.com>
2018-10-01 14:34:12 +0200

    Added bats2junit.sh to convert bats results to junit format for jenkins

---
+++ bats2junit.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+# This script transforms bats test results into junit format which jenkins understands
+# run bats test with "bats --tap <test_file> | tee test-output.txt"
+# then run this script like "bats2junit.sh test-output.txt <suite-name>"
+# Created by Tomislav Plavcic, Percona LLC
+
+INPUT_FILE=""
+SUITE_NAME=""
+FAILURE_MODE=0
+
+if [ "$#" -ne 2 ]; then
+  echo "Usage:"
+  echo "  bats2junit.sh <input-filename> <suite-name>"
+fi
+
+INPUT_FILE="$1"
+SUITE_NAME="$2"
+
+echo "<testsuite name=\"${SUITE_NAME}\">"
+
+while read p; do
+  if [[ $p =~ ^(ok [0-9]* # skip \()(.*\) )(.*)$ ]]; then
+    if [[ ${FAILURE_MODE} -eq 1 ]]; then echo -e "]]>\n  </failure>\n</testcase>"; FAILURE_MODE=0; fi
+    echo "<testcase name=\"${BASH_REMATCH[3]}\">"
+    echo -e "  <skipped/>\n  <system-out>Skip reason:\n<![CDATA["
+    echo "${BASH_REMATCH[2]::-2}"
+    echo -e "]]>\n  </system-out>\n</testcase>"
+  elif [[ $p =~ ^(ok [0-9]* # skip )(.*)$ ]]; then
+    if [[ ${FAILURE_MODE} -eq 1 ]]; then echo -e "]]>\n  </failure>\n</testcase>"; FAILURE_MODE=0; fi
+    echo "<testcase name=\"${BASH_REMATCH[2]}\">"
+    echo "  <skipped/>"
+    echo "</testcase>"
+  elif [[ $p =~ ^(ok [0-9]* )(.*)$ ]]; then
+    if [[ ${FAILURE_MODE} -eq 1 ]]; then echo -e "]]>\n  </failure>\n</testcase>"; FAILURE_MODE=0; fi
+    echo "<testcase name=\"${BASH_REMATCH[2]}\"/>"
+  elif [[ $p =~ ^(not ok [0-9]* )(.*)$ ]]; then
+    echo "<testcase name=\"${BASH_REMATCH[2]}\">"
+    echo -e "  <failure>\n<![CDATA["
+    FAILURE_MODE=1
+  elif [[ "${FAILURE_MODE}" -eq 1 ]]; then
+    echo "$p"
+  fi
+done < ${INPUT_FILE}
+
+  echo "</testsuite>"
