15af0c3f49912baa5bc0eb5307110125a48b7e1c
Nailya <nkutlubaeva@gmail.com>
2018-12-05 19:29:03 +0000

    increase time for sysbench; fix search for mysql

--- pmm-tests/pmm-framework.sh
+++ pmm-tests/pmm-framework.sh
@@ -1321,7 +1321,7 @@ upgrade_client(){
 
 sysbench_prepare(){
   if [[ ! -e $(which mysql 2> /dev/null) ]] ;then
-    MYSQL_CLIENT=$(find . -name mysql | head -n1)
+    MYSQL_CLIENT=$(find . -type f -name mysql | head -n1)
   else
     MYSQL_CLIENT=$(which mysql)
   fi
@@ -1344,7 +1344,7 @@ sysbench_run(){
   for i in $(sudo pmm-admin list | grep "mysql:metrics[ \t].*_NODE-" | awk -F[\(\)] '{print $2}'  | sort -r) ; do
     DB_NAME=$(echo ${i}  | awk -F[\/\.] '{print $3}')
 	DB_NAME="${DB_NAME}_${storage_engine}"
-    sysbench /usr/share/sysbench/oltp_read_write.lua --table-size=10000 --tables=16 --mysql-db=${DB_NAME} --mysql-user=root  --mysql-storage-engine=$storage_engine --threads=16 --time=1200 --events=1870000000 --db-driver=mysql --db-ps-mode=disable --mysql-socket=${i} run  > $WORKDIR/logs/sysbench_run_${DB_NAME}.txt 2>&1 &
+    sysbench /usr/share/sysbench/oltp_read_write.lua --table-size=10000 --tables=16 --mysql-db=${DB_NAME} --mysql-user=root  --mysql-storage-engine=$storage_engine --threads=16 --time=3600 --events=1870000000 --db-driver=mysql --db-ps-mode=disable --mysql-socket=${i} run  > $WORKDIR/logs/sysbench_run_${DB_NAME}.txt 2>&1 &
     check_script $? "Failed to run sysbench oltp"
   done
 }
