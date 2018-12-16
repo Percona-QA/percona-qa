798462ecb458811fc9159e8bfdfe47437f141f75
Roel Van de Paar <roel.vandepaar@percona.com>
2018-12-17 10:10:19 +1100

    Minor fix

--- kill_all_procs.sh
+++ kill_all_procs.sh
@@ -2,8 +2,8 @@
 # Created by Roel Van de Paar, Percona LLC
 
 # Terminates all owned QA-relevant processes
-ps -ef | egrep "mysql"    | grep "$(whoami)" | egrep -v "grep|vim" | awk '{print $2}' | xargs kill -9 2>/dev/null
-ps -ef | egrep "reducer"  | grep "$(whoami)" | egrep -v "grep|vim" | awk '{print $2}' | xargs kill -9 2>/dev/null
-ps -ef | egrep "valgrind" | grep "$(whoami)" | egrep -v "grep|vim" | awk '{print $2}' | xargs kill -9 2>/dev/null
-ps -ef | egrep "pquery"   | grep "$(whoami)" | egrep -v "grep|vim" | awk '{print $2}' | xargs kill -9 2>/dev/null
-ps -ef | egrep "expert"   | grep "$(whoami)" | egrep 0v "grep|vim" | awk '{print $2}' | xargs kill -9 2>/dev/null
+ps -ef | egrep "mysql"     | grep "$(whoami)" | egrep -v "grep|vim" | awk '{print $2}' | xargs kill -9 2>/dev/null
+ps -ef | egrep "reducer"   | grep "$(whoami)" | egrep -v "grep|vim" | awk '{print $2}' | xargs kill -9 2>/dev/null
+ps -ef | egrep "valgrind"  | grep "$(whoami)" | egrep -v "grep|vim" | awk '{print $2}' | xargs kill -9 2>/dev/null
+ps -ef | egrep "pquery"    | grep "$(whoami)" | egrep -v "grep|vim" | awk '{print $2}' | xargs kill -9 2>/dev/null
+ps -ef | egrep "go-expert" | grep "$(whoami)" | egrep 0v "grep|vim" | awk '{print $2}' | xargs kill -9 2>/dev/null
