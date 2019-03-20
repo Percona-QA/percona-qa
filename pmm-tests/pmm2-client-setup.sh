b253be559db881d459622e738525bf5c6188d231
Puneet Kala <puneet.0191@gmail.com>
2019-03-20 11:59:24 +0530

    PMM-3711 Adding MongoDB support

--- pmm-tests/pmm2-client-setup.sh
+++ pmm-tests/pmm2-client-setup.sh
@@ -6,11 +6,12 @@ function jsonval {
 }
 
 display_usage() { 
-	echo "Please make sure to pass atleast pmm_server, mysql_server, mysql_user"
-	echo "1) pmm_server    ------------------localhost:80"
-	echo "2) mysql_server  ------------------localhost:3306"
-	echo "3) mysql_user    ------------------root"
-	echo "4) mysql_password------------------secret"
+	echo "Please make sure to pass atleast pmm_server, db, db_server, db_user"
+	echo "1) pmm_server ------------------localhost:80"
+	echo "2) which_db   ------------------mysql/mongodb"
+	echo "3) db_server  ------------------localhost:3306"
+	echo "4) db_user    ------------------root"
+	echo "5) db_password------------------secret"
 }
 
 # check whether user had supplied -h or --help . If yes display usage 
@@ -19,26 +20,22 @@ then
 	display_usage
 	exit 0
 fi
-if [ "$#" -lt 3 ]; then
+if [ "$#" -lt 4 ]; then
     display_usage
     exit 1;
 fi
 
 STR=$1
 IFS=’:’ read -ra pmm_serer_with_port <<< "$STR" 
-MSTR=$2
-IFS=’:’ read -ra mysql_server_with_port <<< "$MSTR"
+MSTR=$3
+IFS=’:’ read -ra db_server_with_port <<< "$MSTR"
 pmm_server=${pmm_serer_with_port[0]}
 pmm_server_port=${pmm_serer_with_port[1]}
-mysql_server=${mysql_server_with_port[0]}
-mysql_server_port=${mysql_server_with_port[1]}
-mysql_user=$3
-mysql_password=$4
-
-if [ -z "$mysql_server_port" ]
-then
-      mysql_server_port='3306'
-fi
+db_server=${db_server_with_port[0]}
+db_server_port=${db_server_with_port[1]}
+db_user=$4
+db_password=$5
+which_db=$2
 
 node_name=node$((1 + RANDOM % 100))
 json=`curl -d '{"address": "'$pmm_server:$pmm_server_port'", "custom_labels": {"custom_label": "for_node"}, "node_name": "'$node_name'"}' http://${pmm_server}:${pmm_server_port}/v1/inventory/Nodes/AddGeneric`
@@ -55,15 +52,39 @@ pmm-agent --address=$pmm_server:443 --insecure-tls --id=$agent_id & > /dev/null 
 
 sleep 10
 
-service_name=mysql-$((1 + RANDOM % 100))
-json=`curl -d '{"address": "'${mysql_server}'", "port": '${mysql_server_port}', "custom_labels": {"custom_label3": "for_service"}, "node_id": "'$node_id'", "service_name": "'$service_name'"}' \
- http://${pmm_server}:${pmm_server_port}/v1/inventory/Services/AddMySQL`
-prop='service_id'
-service_id=`jsonval`
-echo $service_id
 
-json=`curl -d '{"custom_labels": {"custom_label4": "for_exporter"}, "pmm_agent_id": "'$agent_id'", "service_id": "'$service_id'", "username": "'$mysql_user'", "password": "'$mysql_password'"}' \
-http://${pmm_server}:${pmm_server_port}/v1/inventory/Agents/AddMySQLdExporter`
-prop='runs_on_node_id'
-runs_on_node_id=`jsonval`
-echo $runs_on_node_id
+if [ $which_db == "mysql" ]
+then
+	if [ -z "$db_server_port" ]
+	then
+	      db_server_port='3306'
+	fi
+	service_name=mysql-$((1 + RANDOM % 100))
+	json=`curl -d '{"address": "'${db_server}'", "port": '${db_server_port}', "custom_labels": {"custom_label3": "for_service"}, "node_id": "'$node_id'", "service_name": "'$service_name'"}' \
+	http://${pmm_server}:${pmm_server_port}/v1/inventory/Services/AddMySQL`
+	prop='service_id'
+	service_id=`jsonval`
+	echo $service_id
+
+	json=`curl -d '{"custom_labels": {"custom_label4": "for_exporter"}, "pmm_agent_id": "'$agent_id'", "service_id": "'$service_id'", "username": "'$db_user'", "password": "'$db_password'"}' \
+	http://${pmm_server}:${pmm_server_port}/v1/inventory/Agents/AddMySQLdExporter`
+	prop='runs_on_node_id'
+	runs_on_node_id=`jsonval`
+	echo $runs_on_node_id
+fi
+
+if [ $which_db == "mongodb" ]
+then
+	service_name=mongodb-$((1 + RANDOM % 100))
+	json=`curl -d '{"address": "'${db_server}'", "port": '${db_server_port}', "custom_labels": {"custom_label3": "for_service"}, "node_id": "'$node_id'", "service_name": "'$service_name'"}' \
+	http://${pmm_server}:${pmm_server_port}/v1/inventory/Services/AddMongoDB`
+ 	prop='service_id'
+	service_id=`jsonval`
+	echo $service_id
+
+	json=`curl -d '{"custom_labels": {"custom_label4": "for_exporter"}, "pmm_agent_id": "'$agent_id'", "service_id": "'$service_id'", "username": "'$db_user'", "password" "'$db_password'"}' \
+	http://${pmm_server}:${pmm_server_port}/v1/inventory/Agents/AddMongoDBExporter`
+	prop='runs_on_node_id'
+	runs_on_node_id=`jsonval`
+	echo $runs_on_node_id
+fi 
