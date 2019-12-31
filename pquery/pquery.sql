81d705e5c119dcda6f3e53ddb404c5cf65cd1b3b
Mohit Joshi <mohit.joshi@percona.com>
2019-12-31 12:33:58 +0000

    PS-6024 Integrate pquery3 with old pquery framework

--- pquery/pquery.sql
+++ pquery/pquery.sql
@@ -1,3 +1,23 @@
-SELECT 'put your various queries here';
-SELECT 'one per line';
-SELECT 'these queries will be executed, randomingly picked from this file';
+DROP TABLE IF EXISTS t1;
+CREATE TABLE t1 (ID int);
+INSERT INTO t1 VALUES (1);
+INSERT INTO t1 VALUES (2);
+INSERT INTO t1 VALUES (3);
+INSERT INTO t1 VALUES (4);
+INSERT INTO t1 VALUES (5);
+ALTER TABLE t1 ADD PRIMARY KEY pk(ID);
+ALTER TABLE t1 DROP PRIMARY KEY;
+INSERT INTO t1 SELECT * FROM t1;
+DELETE FROM t1 LIMIT 3;
+ALTER TABLE t1 ENGINE=InnoDB;
+UPDATE t1 SET ID=2;
+CALL country_hos('Europe');
+SELECT 'test 1';
+SELECT * FROM t1;
+CREATE DATABASE IF NOT EXISTS test DEFAULT CHARACTER SET="utf8" DEFAULT COLLATE="utf8_bin";
+USE test;
+CREATE TABLE t1 (a int, b int, c int);
+INSERT INTO t1 VALUES (1,2,3);
+SELECT * FROM t1;
+/**/
+#
