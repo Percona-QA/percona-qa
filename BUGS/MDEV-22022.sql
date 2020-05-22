9a3c33c8c252e1487e8ddd20ff2dd3ddee2e964c
Roel Van de Paar <roel.vandepaar@mariadb.com>
2020-05-22 15:09:40 +1000

    More bug updates

--- BUGS/MDEV-22022.sql
+++ BUGS/MDEV-22022.sql
@@ -7,3 +7,6 @@ EXECUTE IMMEDIATE 'if(`systeminfo /FO LIST';
 EXECUTE IMMEDIATE 'if(`systeminfo';
 
 EXECUTE IMMEDIATE 'IF(`SELECT @@a=';
+
+SET CHARACTER_SET_CLIENT=17;
+SELECT doc.`Children`.0 FROM t1;
