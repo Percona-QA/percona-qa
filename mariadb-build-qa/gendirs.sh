f0e1496814eed8d185aa5c59dfb4ffa8be0e2470
Roel Van de Paar <roel.vandepaar@mariadb.com>
2020-05-29 09:48:52 +1000

    Few *SAN improvements, bugs, script patches

--- mariadb-build-qa/gendirs.sh
+++ mariadb-build-qa/gendirs.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 # Created by Roel Van de Paar, MariaDB
 
-ls -d MD*10.[1-5]* 2>/dev/null | grep -v tar | grep -vE "ASAN|GAL"
+ls -d MD*10.[1-6]* 2>/dev/null | grep -v tar | grep -vE "ASAN|GAL"
 ls -d MS*[58].[5670]* 2>/dev/null | grep -v tar | grep -vE "ASAN|GAL"
+
+if [ "${1}" == "ALL" ]; then
+  ls -d ASAN_M* 2>/dev/null | grep -v tar 
+  ls -d GAL_M* 2>/dev/null | grep -v tar 
+fi
