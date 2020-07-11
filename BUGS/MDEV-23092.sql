cc3cb411dfbcca35d0e14ab38fa9f81dd2756cc5
Roel Van de Paar <roel.vandepaar@mariadb.com>
2020-07-11 16:52:19 +1000

    Bug updates/additions

--- BUGS/MDEV-23092.sql
+++ BUGS/MDEV-23092.sql
@@ -3,3 +3,10 @@ SET GLOBAL wsrep_provider='/invalid/libgalera_smm.so';
 SET GLOBAL wsrep_cluster_address='OFF';
 SET GLOBAL wsrep_slave_threads=10;
 SELECT 1;
+
+SET NAMES utf8, collation_connection='utf16le_bin';
+SET @@global.wsrep_provider='/invalid/libgalera_smm.so';
+SET @@global.wsrep_cluster_address=AUTO;
+SET GLOBAL wsrep_slave_threads = 2;
+SELECT SLEEP(2);
+CREATE TABLE t (c INT);
