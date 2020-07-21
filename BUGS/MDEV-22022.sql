1ae688c40da8bb7d729018bb2e3a6a602b594519
Roel Van de Paar <roel.vandepaar@mariadb.com>
2020-07-21 12:59:17 +1000

    Improved memory monitoring/handling script, adding bugs

--- BUGS/MDEV-22022.sql
+++ BUGS/MDEV-22022.sql
@@ -10,3 +10,5 @@ EXECUTE IMMEDIATE 'IF(`SELECT @@a=';
 
 SET CHARACTER_SET_CLIENT=17;
 SELECT doc.`Children`.0 FROM t1;
+
+if (`select count(*) = 0 from information_schema.session_variables where variable_name = 'abcdefghijklmnopqrstuvwxyz' and variable_value = 'abcdefghijklmnopqrstuvwxyz';
