a8f8f5aebeb36b570ac3d0c50ce8b2998c170e6a
Roel Van de Paar <roel.vandepaar@mariadb.com>
2020-09-09 11:24:44 +1000

    dbg/opt additions for bugs

--- BUGS/MDEV-16788.sql
+++ BUGS/MDEV-16788.sql
@@ -5,3 +5,8 @@ SELECT * FROM INFORMATION_SCHEMA.ROUTINES WHERE ROUTINE_NAME='a';
 SET CHARACTER_SET_CONNECTION=ucs2;
 INSERT INTO mysql.proc (db, name, type, specific_name, language, sql_data_access, is_deterministic, security_type, param_list, returns, body, definer, created, modified, sql_mode, comment, character_set_client, collation_connection, db_collation, body_utf8 ) VALUES ('test','bug14233_1','FUNCTION','bug14233_1','SQL','READS_SQL_DATA','NO','DEFINER','','int(10)','SELECT COUNT(*) FROM mysql.user','root@localhost', NOW() , '0000-00-00 00:00:00','','','','','','SELECT COUNT(*) FROM mysql.user');
 SHOW FUNCTION STATUS WHERE db=DATABASE();
+
+CREATE TABLE t1 (k INT);
+CREATE PROCEDURE pr() ALTER TABLE t1 ADD CONSTRAINT CHECK (k != 5);
+CALL pr;
+CALL pr;
