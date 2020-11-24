0c1b54d14c237d8760685b3f9f3babb8edf71eb9
Roel Van de Paar <roel.vandepaar@mariadb.com>
2020-11-24 20:29:08 +1100

    Add TSAN: build script updates, bug updates

--- build_mdpsms_opt_san.sh
+++ build_mdpsms_opt_san.sh
@@ -9,7 +9,8 @@ BOOST_LOCATION=/tmp/boost_043581/
 USE_CUSTOM_COMPILER=0   # 0 or 1 # Use a customer compiler
 CUSTOM_COMPILER_LOCATION="/home/roel/GCC-5.5.0/bin"
 USE_CLANG=0             # 0 or 1 # Use the clang compiler instead of gcc
-USE_SAN=1               # 0 or 1 # Use ASAN, MSAN, UBSAN
+USE_SAN=1               # 0 or 1 # Use [ASAN or MSAN], UBSAN, TSAN
+ASAN_OR_MSAN=0          # 0 or 1 # 0: ASAN, 1: MSAN
 #CLANG_LOCATION="/home/roel/third_party/llvm-build/Release+Asserts/bin/clang"  # Should end in /clang (and assumes presence of /clang++)
 CLANG_LOCATION="/usr/bin/clang"  # Should end in /clang (and assumes presence of /clang++)
 USE_AFL=0               # 0 or 1 # Use the American Fuzzy Lop gcc/g++ wrapper instead of gcc/g++
@@ -82,18 +83,18 @@ MS=0
 MD=0
 if [ ${MYSQL_VERSION_MAJOR} -eq 10 ]; then
   MD=1
-  PREFIX="ASAN_MD${DATE}"
+  PREFIX="SAN_MD${DATE}"
   ZLIB="-DWITH_ZLIB=bundled"  # 10.1 will fail with requirement for WITH_ZLIB=bundled. Building 10.1-10.5 with bundled ftm.
 elif [ ! -d rocksdb ]; then  # MS, PS
   VERSION_EXTRA="$(grep "MYSQL_VERSION_EXTRA=" VERSION | sed 's|MYSQL_VERSION_EXTRA=||;s|[ \t]||g')"
   if [ "${VERSION_EXTRA}" == "" -o "${VERSION_EXTRA}" == "-dmr" -o "${VERSION_EXTRA}" == "-rc" ]; then  # MS has no extra version number, or shows '-dmr' or '-rc' (both exactly and only) in this place
     MS=1
-    PREFIX="ASAN_MS${DATE}"
+    PREFIX="SAN_MS${DATE}"
   else
-    PREFIX="ASAN_PS${DATE}"
+    PREFIX="SAN_PS${DATE}"
   fi
 else
-  PREFIX="ASAN_FB${DATE}"
+  PREFIX="SAN_FB${DATE}"
   FB=1
 fi
 
@@ -157,15 +158,18 @@ if [ $USE_AFL -eq 1 ]; then
   #AFL="-DCMAKE_C_COMPILER=$AFL_LOCATION/afl-gcc -DCMAKE_CXX_COMPILER=$AFL_LOCATION/afl-g++"
 fi
 
-# ASAN, MSAN, UBSAN
+# [ASAN or MSAN], UBSAN, TSAN
 SAN=
 if [ $USE_SAN -eq 1 ]; then
   # MSAN and ASAN cannot be used at the same time, choose one of the two options below.
   # Also note that for MSAN to have an affect, all libs linked to MySQL must also have been compiled with this option enabled
   # Ref https://dev.mysql.com/doc/refman/5.7/en/source-configuration-options.html#option_cmake_with_msan
-  #SAN="--DWITH_MSAN=ON -DWITH_UBSAN=ON"
-  SAN="-DWITH_ASAN=ON -DWITH_ASAN_SCOPE=ON -DWITH_UBSAN=ON -DWITH_RAPID=OFF"  # Default
+  if [ ${ASAN_OR_MSAN} -eq 0 ]; then
     # The -DWITH_RAPID=OFF is a workaround for https://bugs.mysql.com/bug.php?id=90211 - it disables GR and mysqlx (rapid plugins)
+    SAN="-DWITH_ASAN=ON -DWITH_ASAN_SCOPE=ON -DWITH_UBSAN=ON -DWITH_RAPID=OFF -DWSREP_LIB_WITH_ASAN=ON -DWITH_TSAN=ON -DWSREP_LIB_WITH_TSAN=ON"
+  else
+    SAN="--DWITH_MSAN=ON -DWITH_UBSAN=ON -DWITH_RAPID=OFF -DWSREP_LIB_WITH_ASAN=ON -DWITH_TSAN=ON -DWSREP_LIB_WITH_TSAN=ON"
+  fi
 fi
 
 # Use a custom compiler
@@ -205,7 +209,7 @@ else
         # And the following was needed to avoid this error on mysqld startup:
         # ==PID== ASan runtime does not come first in initial library list; you should either link runtime to your application or manually preload it with LD_PRELOAD.
         FLAGS='-DCMAKE_CXX_FLAGS=-static-libasan'
-        echo "Using GCC for ASAN build."
+        echo "Using GCC for SAN build."
       fi
     fi
   fi
