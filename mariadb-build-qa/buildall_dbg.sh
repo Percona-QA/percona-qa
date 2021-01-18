6a529006e06eaf8060f9d4017e66a21ee544ecf7
Roel Van de Paar <roel.vandepaar@mariadb.com>
2021-01-18 16:07:24 +1100

    Script updates

--- mariadb-build-qa/buildall_dbg.sh
+++ mariadb-build-qa/buildall_dbg.sh
@@ -6,7 +6,7 @@
 
 DIR=${PWD}
 rm -Rf 10.1_dbg 10.2_dbg 10.3_dbg 10.4_dbg 10.5_dbg 10.6_dbg
-cd ${DIR}/10.1 && ~/mariadb-qa/build_mdpsms_dbg.sh &
+#cd ${DIR}/10.1 && ~/mariadb-qa/build_mdpsms_dbg.sh &
 cd ${DIR}/10.2 && ~/mariadb-qa/build_mdpsms_dbg.sh &
 cd ${DIR}/10.3 && ~/mariadb-qa/build_mdpsms_dbg.sh &
 cd ${DIR}/10.4 && ~/mariadb-qa/build_mdpsms_dbg.sh &
