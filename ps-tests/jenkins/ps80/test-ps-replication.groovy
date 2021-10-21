007b64cfc18fc7653ae3fd945cdffc97d4ce593a
Eleonora Zinchenko <eleonora.zinchenko@percona.com>
2021-10-21 12:07:01 +0300

    PS-7912: rename PS binary tarball in case its name is binary.tar.gz

--- ps-tests/jenkins/ps80/test-ps-replication.groovy
+++ ps-tests/jenkins/ps80/test-ps-replication.groovy
@@ -95,8 +95,11 @@ pipeline {
                 fi
                 wget -q ${PT_BIN}
                 wget -q ${PXB_BIN}
-#               PS_TARBALL="$(tar -ztf binary.tar.gz|head -n1|sed 's:/$::').tar.gz"
-#               mv binary.tar.gz ${PS_TARBALL}
+                # Rename PS binary tarball in case its name is binary.tar.gz.
+                if echo "${PS_BIN}" | grep -Eq "binary.tar.gz"; then
+                  PS_TARBALL="$(tar -ztf binary.tar.gz|head -n1|sed 's:/$::').tar.gz"
+                  mv binary.tar.gz ${PS_TARBALL}
+                fi
                 cd -
                 git clone https://github.com/Percona-QA/percona-qa.git --depth 1
                 ${WORKSPACE}/percona-qa/ps-async-repl-test.sh --workdir=${WORKSPACE}/${TEST_DIR} --build-number=${BUILD_NUMBER} --testcase=${TEST_CASE} --storage-engine=innodb
@@ -147,8 +150,11 @@ pipeline {
                 fi
                 wget -q ${PT_BIN}
                 wget -q ${PXB_BIN}
-#               PS_TARBALL="$(tar -ztf binary.tar.gz|head -n1|sed 's:/$::').tar.gz"
-#               mv binary.tar.gz ${PS_TARBALL}
+                # Rename PS binary tarball in case its name is binary.tar.gz.
+                if echo "${PS_BIN}" | grep -Eq "binary.tar.gz"; then
+                  PS_TARBALL="$(tar -ztf binary.tar.gz|head -n1|sed 's:/$::').tar.gz"
+                  mv binary.tar.gz ${PS_TARBALL}
+                fi
                 cd -
                 git clone https://github.com/Percona-QA/percona-qa.git --depth 1
                 ${WORKSPACE}/percona-qa/ps-async-repl-test.sh --workdir=${WORKSPACE}/${TEST_DIR} --build-number=${BUILD_NUMBER} --testcase=${TEST_CASE} --storage-engine=rocksdb
@@ -199,8 +205,11 @@ pipeline {
                 fi
                 wget -q ${PT_BIN}
                 wget -q ${PXB_BIN}
-#               PS_TARBALL="$(tar -ztf binary.tar.gz|head -n1|sed 's:/$::').tar.gz"
-#               mv binary.tar.gz ${PS_TARBALL}
+                # Rename PS binary tarball in case its name is binary.tar.gz.
+                if echo "${PS_BIN}" | grep -Eq "binary.tar.gz"; then
+                  PS_TARBALL="$(tar -ztf binary.tar.gz|head -n1|sed 's:/$::').tar.gz"
+                  mv binary.tar.gz ${PS_TARBALL}
+                fi
                 cd -
                 git clone https://github.com/Percona-QA/percona-qa.git --depth 1
                 ${WORKSPACE}/percona-qa/ps-async-repl-test.sh --workdir=${WORKSPACE}/${TEST_DIR} --build-number=${BUILD_NUMBER} --testcase=${TEST_CASE} --storage-engine=tokudb
@@ -251,8 +260,11 @@ pipeline {
                 fi
                 wget -q ${PT_BIN}
                 wget -q ${PXB_BIN}
-#               PS_TARBALL="$(tar -ztf binary.tar.gz|head -n1|sed 's:/$::').tar.gz"
-#               mv binary.tar.gz ${PS_TARBALL}
+                # Rename PS binary tarball in case its name is binary.tar.gz.
+                if echo "${PS_BIN}" | grep -Eq "binary.tar.gz"; then
+                  PS_TARBALL="$(tar -ztf binary.tar.gz|head -n1|sed 's:/$::').tar.gz"
+                  mv binary.tar.gz ${PS_TARBALL}
+                fi
                 cd -
                 git clone https://github.com/Percona-QA/percona-qa.git --depth 1
                 ${WORKSPACE}/percona-qa/ps-async-repl-test.sh --workdir=${WORKSPACE}/${TEST_DIR} --build-number=${BUILD_NUMBER} --testcase=${TEST_CASE} --with-encryption --keyring-plugin=file
@@ -303,8 +315,11 @@ pipeline {
                 fi
                 wget -q ${PT_BIN}
                 wget -q ${PXB_BIN}
-#               PS_TARBALL="$(tar -ztf binary.tar.gz|head -n1|sed 's:/$::').tar.gz"
-#               mv binary.tar.gz ${PS_TARBALL}
+                # Rename PS binary tarball in case its name is binary.tar.gz.
+                if echo "${PS_BIN}" | grep -Eq "binary.tar.gz"; then
+                  PS_TARBALL="$(tar -ztf binary.tar.gz|head -n1|sed 's:/$::').tar.gz"
+                  mv binary.tar.gz ${PS_TARBALL}
+                fi
                 cd -
                 git clone https://github.com/Percona-QA/percona-qa.git --depth 1
                 ${WORKSPACE}/percona-qa/ps-async-repl-test.sh --workdir=${WORKSPACE}/${TEST_DIR} --build-number=${BUILD_NUMBER} --testcase=${TEST_CASE} --with-encryption --keyring-plugin=vault
