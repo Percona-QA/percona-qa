d13c4ecaa97b80a864c9281262e3696dc0747474
Mohit Joshi <mohit.joshi@percona.com>
2023-10-20 05:58:24 +0000

    Added tests for percona-xtrabackup-81 packages in pkg_install_tests.sh

--- backup_tests/pkg_install_tests.sh
+++ backup_tests/pkg_install_tests.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-########################################################################
-# Created By Manish Chawla, Percona LLC                                #
-# This script installs pxb2.4/pxb8 from the main/testing repo          #
-# Usage:                                                               #
-# 1. Run the script as: ./pkg_install_tests.sh pxb24/pxb8 main/testing #
-# 3. Logs are available in: $HOME/install_log                          #
-########################################################################
+###############################################################################
+# Created By Manish Chawla, Percona LLC                                       #
+# Modified By Mohit Joshi, Percona LLC                                        #
+# This script installs pxb2.4/pxb80/pxb81 from the main/testing repo          #
+# Usage:                                                                      #
+# 1. Run the script as: ./pkg_install_tests.sh pxb24/pxb80/pxb81 main/testing #
+# 3. Logs are available in: $HOME/install_log                                 #
+###############################################################################
 
 log="$HOME/install_log"
 if [ "$#" -ne 2 ]; then
-    echo "Please run the script with parameters: <pxb-version=pxb24/pxb8> <repo=main/testing>"
+    echo "Please run the script with parameters: <pxb-version=pxb24/pxb80/pxb81> <repo=main/testing>"
     exit 1
 fi
 
@@ -23,12 +24,14 @@ if [ -f /usr/bin/yum ]; then
     list_cmd="sudo yum list installed"
     pxb24_packages="percona-xtrabackup-24 percona-xtrabackup-test-24 percona-xtrabackup-24-debuginfo"
     pxb80_packages="percona-xtrabackup-80 percona-xtrabackup-test-80 percona-xtrabackup-80-debuginfo"
+    pxb81_packages="percona-xtrabackup-81 percona-xtrabackup-test-81 percona-xtrabackup-81-debuginfo"
 else
     install_cmd="sudo apt-get install -y"
     remove_cmd="sudo apt-get remove -y"
     list_cmd="sudo apt list --installed"
     pxb24_packages="percona-xtrabackup-24 percona-xtrabackup-test-24 percona-xtrabackup-dbg-24"
     pxb80_packages="percona-xtrabackup-80 percona-xtrabackup-test-80 percona-xtrabackup-dbg-80"
+    pxb81_packages="percona-xtrabackup-81 percona-xtrabackup-test-81 percona-xtrabackup-dbg-81"
 fi
 
 install_pxb_package() {
@@ -39,6 +42,9 @@ install_pxb_package() {
     elif ${list_cmd} |grep "percona-xtrabackup-80" >"${log}" ; then
         echo "Uninstalling PXB 8.0 packages"
         ${remove_cmd} ${pxb80_packages} >>"${log}"
+    elif ${list_cmd} |grep "percona-xtrabackup-81" >"${log}" ; then
+        echo "Uninstalling PXB 8.0 packages"
+        ${remove_cmd} ${pxb81_packages} >>"${log}"
     else
         echo "PXB packages are not installed"
     fi
@@ -80,7 +86,7 @@ install_pxb_package() {
             echo "PXB 2.4 packages successfully installed with version: "
             xtrabackup --version
         fi
-    elif [[ "$1" = "pxb8" ]] && [[ "$2" = "main" ]]; then
+    elif [[ "$1" = "pxb80" ]] && [[ "$2" = "main" ]]; then
         echo "Installing PXB 8.0 packages from the main repo"
         sudo percona-release enable-only tools release
         if [ -f /usr/bin/apt ]; then
@@ -94,7 +100,7 @@ install_pxb_package() {
             echo "PXB 8.0 packages successfully installed with version: "
             xtrabackup --version
         fi
-    elif [[ "$1" = "pxb8" ]] && [[ "$2" = "testing" ]]; then
+    elif [[ "$1" = "pxb80" ]] && [[ "$2" = "testing" ]]; then
         echo "Installing PXB 8.0 packages from the testing repo"
         sudo percona-release enable-only tools testing
         if [ -f /usr/bin/apt ]; then
@@ -108,7 +114,36 @@ install_pxb_package() {
             echo "PXB 8.0 packages successfully installed with version: "
             xtrabackup --version
         fi
+    elif [[ "$1" = "pxb81" ]] && [[ "$2" = "main" ]]; then
+        echo "Installing PXB 8.1 packages from the main repo"
+        sudo percona-release enable-only tools release
+        if [ -f /usr/bin/apt ]; then
+            sudo apt-get update >>"${log}"
+        fi
+
+        if ! ${install_cmd} ${pxb81_packages} >>"${log}" ; then
+            echo "ERR: PXB 8.1 packages could not be installed from the main repo"
+            exit 1
+        else
+            echo "PXB 8.1 packages successfully installed with version: "
+            xtrabackup --version
+        fi
+    elif [[ "$1" = "pxb81" ]] && [[ "$2" = "testing" ]]; then
+        echo "Installing PXB 8.1 packages from the testing repo"
+        sudo percona-release enable-only tools testing
+        if [ -f /usr/bin/apt ]; then
+            sudo apt-get update >>"${log}"
+        fi
+
+        if ! ${install_cmd} ${pxb81_packages} >>"${log}" ; then
+            echo "ERR: PXB 8.1 packages could not be installed from the testing repo"
+            exit 1
+        else
+            echo "PXB 8.1 packages successfully installed with version: "
+            xtrabackup --version
+        fi
     fi
+
 }
 
 install_pxb_package "${pxb_version}" "${repo}"
