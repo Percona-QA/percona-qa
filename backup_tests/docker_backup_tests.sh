8257788425f96195d0fe4548cfdc6e410bb920d2
Mohit Joshi <mohit.joshi@percona.com>
2023-11-28 13:07:32 +0000

    Added support for Percona Server with PXB Docker tests Run PXB-8.1 with PS-8.1 Run PXB-8.0 with PS-8.0 Run PXB-2.4 with PS-5.7
    Run PXB-8.1 with MS-8.1
    Run PXB-8.0 with MS-8.0
    Run PXB-2.4 with MS-5.7

--- backup_tests/docker_backup_tests.sh
+++ backup_tests/docker_backup_tests.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
 
-#################################################################################
-# Created By Manish Chawla, Percona LLC                                         #
-# Modified By Mohit Joshi, Percona LLC                                          #
-# This script tests backup for docker containers                                #
-# Usage:                                                                        #
-# 1. Run the script as: ./docker_backup_tests.sh pxb24/pxb80/pxb81 main/testing #
-# 3. Logs are available in: $PWD/backup_log                                     #
-#################################################################################
+#######################################################################################
+# Created By Manish Chawla, Percona LLC                                               #
+# Modified By Mohit Joshi, Percona LLC                                                #
+# This script tests backup for docker containers                                      #
+# Usage:                                                                              #
+# 1. Run the script as: ./docker_backup_tests.sh pxb24/pxb80/pxb81 main/testing ps/ms #
+# 3. Logs are available in: $PWD/backup_log                                           #
+#######################################################################################
 
-if [ "$#" -ne 2 ]; then
-    echo "Please run the script with parameters: <version as pxb24/pxb80/pxb81> <repo as main/testing>"
+if [ "$#" -ne 3 ]; then
+    echo "Please run the script with parameters: <version as pxb24/pxb80/pxb81> <repo as main/testing> <product as ps/ms>"
     echo "Main repo is the percona docker image and testing repo is the perconalab docker image"
     exit 1
 fi
 
-if [ "$1" = "pxb81" ]; then
-    version=pxb81
-    server="mysql-8.1"
-    mysql_docker_image="mysql:8.1.0"
-    repo="$2"
+version=$1
+repo=$2
+product=$3
+if [ "$version" = "pxb81" ]; then
+    if [ "$product" = "ms" ]; then
+      server="mysql-8.1"
+      mysql_docker_image="mysql:8.1.0"
+    elif [ "$product" = "ps" ]; then
+      server="percona-server-8.1"
+      mysql_docker_image="percona/percona-server:8.1.0"
+    else
+      echo "Invalid product!"
+      exit 1
+    fi
     if [ "$repo" = "main" ]; then
         pxb_docker_image="percona/percona-xtrabackup:8.1"
     elif [ "$repo" = "testing" ]; then
@@ -28,11 +37,17 @@ if [ "$1" = "pxb81" ]; then
     pxb_backup_dir="pxb_backup_data:/backup_81"
     target_backup_dir="/backup_81"
     mount_dir="-v /tmp/mysql_data:/var/lib/mysql -v /var/run/mysqld:/var/run/mysqld"
-elif [ "$1" = "pxb80" ]; then
-    version=pxb80
-    server="mysql-8.0"
-    mysql_docker_image="mysql/mysql-server:latest"
-    repo="$2"
+elif [ "$version" = "pxb80" ]; then
+    if [ "$product" = "ms" ]; then
+      server="mysql-8.0"
+      mysql_docker_image="mysql/mysql-server:latest"
+    elif [ "$product" = "ps" ]; then
+      server="percona-server-8.0"
+      mysql_docker_image="percona/percona-server:8.0"
+    else
+      echo "Invalid product!"
+      exit 1
+    fi
     if [ "$repo" = "main" ]; then
         pxb_docker_image="percona/percona-xtrabackup:8.0"
     elif [ "$repo" = "testing" ]; then
@@ -41,11 +56,17 @@ elif [ "$1" = "pxb80" ]; then
     pxb_backup_dir="pxb_backup_data:/backup_80"
     target_backup_dir="/backup_80"
     mount_dir="-v /tmp/mysql_data:/var/lib/mysql"
-elif [ "$1" = "pxb24" ]; then
-    version=pxb24
-    server="mysql-5.7"
-    mysql_docker_image="mysql/mysql-server:5.7"
-    repo="$2"
+elif [ "$version" = "pxb24" ]; then
+    if [ "$product" = "ms" ]; then
+      server="mysql-5.7"
+      mysql_docker_image="mysql/mysql-server:5.7"
+    elif [ "$product" = "ps" ]; then
+      server="percona-server-5.7"
+      mysql_docker_image="percona/percona-server:5.7"
+    else
+      echo "Invalid product!"
+      exit 1
+    fi
     if [ "$repo" = "main" ]; then
         pxb_docker_image="percona/percona-xtrabackup:2.4"
     elif [ "$repo" = "testing" ]; then
@@ -79,9 +100,11 @@ clean_setup() {
 
 test_pxb_docker() {
     # This function runs tests for pxb 8.0 and ms 8.0 docker image
-    start_mysql_container="sudo docker run --name $server $mount_dir -p 3306:3306 -p 3060:3060 -e MYSQL_ROOT_HOST=% -e MYSQL_ROOT_PASSWORD=mysql -d $mysql_docker_image"
+    start_mysql_container="sudo docker run --name $server $mount_dir -p 3306:3306 -e PERCONA_TELEMETRY_DISABLE=1 -e MYSQL_ROOT_HOST=% -e MYSQL_ROOT_PASSWORD=mysql -d $mysql_docker_image"
 
     mkdir /tmp/mysql_data
+    sudo chmod -R 777 /tmp/mysql_data
+    sudo chmod -R 777 /var/run/mysqld
 
     echo "Run $server docker container"
     if ! $start_mysql_container >>backup_log 2>&1; then
@@ -180,6 +203,7 @@ test_pxb_docker() {
 }
 
 # Check and clean existing installation
+rm backup_log
 clean_setup
 test_pxb_docker | tee -a backup_log
 
