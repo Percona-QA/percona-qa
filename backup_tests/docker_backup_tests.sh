a0a6af8c35a47ad4254bcf4f24a114a3a959a326
Mohit Joshi <mohit.joshi@percona.com>
2023-12-08 17:43:22 +0530

    Added new repo pxb-8x-innovation to PXB testing script (#388)
    * Added new repo pxb-8x-innovation to PXB testing script
    
    * Fixed package name
    
    * Handled pxb-8x-innovation versions in a better way

--- backup_tests/docker_backup_tests.sh
+++ backup_tests/docker_backup_tests.sh
@@ -1,52 +1,70 @@
 #!/bin/bash
 
-#######################################################################################
-# Created By Manish Chawla, Percona LLC                                               #
-# Modified By Mohit Joshi, Percona LLC                                                #
-# This script tests backup for docker containers                                      #
-# Usage:                                                                              #
-# 1. Run the script as: ./docker_backup_tests.sh pxb24/pxb80/pxb81 main/testing ps/ms #
-# 3. Logs are available in: $PWD/backup_log                                           #
-#######################################################################################
+###################################################################################################
+# Created By Manish Chawla, Percona LLC                                                           #
+# Modified By Mohit Joshi, Percona LLC                                                            #
+# This script runs PXB against Percona Server and MySQL server in a docker container              #
+###################################################################################################
 
-if [ "$#" -ne 3 ]; then
-    echo "Please run the script with parameters: <version as pxb24/pxb80/pxb81> <repo as main/testing> <product as ps/ms>"
+help() {
+    echo "Usage: $0 pxb_version repo server [innovation]"
+    echo "Accepted values of version: pxb24, pxb80, pxb-8x-innovation"
+    echo "Accepted values of repo: main, testing"
+    echo "Accepted value of server: ps, ms"
+    echo "Accepted value of innovation: 8.1, 8.2"
     echo "Main repo is the percona docker image and testing repo is the perconalab docker image"
     exit 1
+}
+
+if [ "$#" -lt 3 ] || [ "$#" -gt 4 ]; then
+    help
 fi
 
-version=$1
+pxb_version=$1
 repo=$2
-product=$3
-if [ "$version" = "pxb81" ]; then
-    if [ "$product" = "ms" ]; then
-      server="mysql-8.1"
-      mysql_docker_image="mysql:8.1.0"
-    elif [ "$product" = "ps" ]; then
-      server="percona-server-8.1"
-      mysql_docker_image="percona/percona-server:8.1.0"
+server=$3
+
+if [ "$pxb_version" == "pxb-8x-innovation" ]; then
+    # Check if the number of arguments is 3
+    if [ "$#" -ne 4 ]; then
+        echo "ERR: 'innovation' argument is required for pxb-8x-innovation. Accepted values: 8.1, 8.2"
+        help
+    fi
+    innovation=$4
+else
+    innovation="" # Set innovation to an empty string for other cases
+fi
+
+
+if [ "$pxb_version" = "pxb-8x-innovation" ]; then
+    if [ "$server" = "ms" ]; then
+        container_name="mysql-$innovation"
+        mysql_docker_image="mysql:$innovation"
+    elif [ "$server" = "ps" ]; then
+        container_name="percona-server-$innovation"
+        mysql_docker_image="percona/percona-server:$innovation"
     else
-      echo "Invalid product!"
-      exit 1
+        echo "Invalid product!"
+        help
     fi
     if [ "$repo" = "main" ]; then
-        pxb_docker_image="percona/percona-xtrabackup:8.1"
+        pxb_docker_image="percona/percona-xtrabackup:$innovation"
     elif [ "$repo" = "testing" ]; then
-        pxb_docker_image="perconalab/percona-xtrabackup:8.1"
+        pxb_docker_image="perconalab/percona-xtrabackup:$innovation"
     fi
-    pxb_backup_dir="pxb_backup_data:/backup_81"
-    target_backup_dir="/backup_81"
+    pxb_backup_dir="pxb_backup_data:/backup_$innovation"
+    target_backup_dir="/backup_$innovation"
     mount_dir="-v /tmp/mysql_data:/var/lib/mysql -v /var/run/mysqld:/var/run/mysqld"
-elif [ "$version" = "pxb80" ]; then
-    if [ "$product" = "ms" ]; then
-      server="mysql-8.0"
-      mysql_docker_image="mysql/mysql-server:latest"
-    elif [ "$product" = "ps" ]; then
-      server="percona-server-8.0"
-      mysql_docker_image="percona/percona-server:8.0"
+elif [ "$pxb_version" = "pxb80" ]; then
+    if [ "$server" = "ms" ]; then
+        container_name="mysql-8.0"
+        mysql_docker_image="mysql/mysql-server:latest"
+    elif [ "$server" = "ps" ]; then
+        container_name="percona-server-8.0"
+        mysql_docker_image="percona/percona-server:8.0"
     else
-      echo "Invalid product!"
-      exit 1
+        echo "Invalid product!"
+        help
     fi
     if [ "$repo" = "main" ]; then
         pxb_docker_image="percona/percona-xtrabackup:8.0"
@@ -56,16 +74,16 @@ elif [ "$version" = "pxb80" ]; then
     pxb_backup_dir="pxb_backup_data:/backup_80"
     target_backup_dir="/backup_80"
     mount_dir="-v /tmp/mysql_data:/var/lib/mysql"
-elif [ "$version" = "pxb24" ]; then
-    if [ "$product" = "ms" ]; then
-      server="mysql-5.7"
-      mysql_docker_image="mysql/mysql-server:5.7"
-    elif [ "$product" = "ps" ]; then
-      server="percona-server-5.7"
-      mysql_docker_image="percona/percona-server:5.7"
+elif [ "$pxb_version" = "pxb24" ]; then
+    if [ "$server" = "ms" ]; then
+        container_name="mysql-5.7"
+        mysql_docker_image="mysql/mysql-server:5.7"
+    elif [ "$server" = "ps" ]; then
+        container_name="percona-server-5.7"
+        mysql_docker_image="percona/percona-server:5.7"
     else
-      echo "Invalid product!"
-      exit 1
+        echo "Invalid product!"
+        help
     fi
     if [ "$repo" = "main" ]; then
         pxb_docker_image="percona/percona-xtrabackup:2.4"
@@ -77,16 +95,16 @@ elif [ "$version" = "pxb24" ]; then
     mount_dir="-v /tmp/mysql_data:/var/lib/mysql"
 else
     echo "Invalid version parameter. Exiting"
-    exit 1
+    help
 fi
 
 
 clean_setup() {
     # This function checks and cleans the setup
 
-    if [ "$(sudo docker ps -a | grep $server)" ]; then
-        sudo docker stop $server >/dev/null 2>&1
-        sudo docker rm $server >/dev/null 2>&1
+    if [ "$(sudo docker ps -a | grep $container_name)" ]; then
+        sudo docker stop $container_name >/dev/null 2>&1
+        sudo docker rm $container_name >/dev/null 2>&1
     fi
 
     if [ -d /tmp/mysql_data ]; then
@@ -100,21 +118,21 @@ clean_setup() {
 
 test_pxb_docker() {
     # This function runs tests for pxb 8.0 and ms 8.0 docker image
-    start_mysql_container="sudo docker run --name $server $mount_dir -p 3306:3306 -e PERCONA_TELEMETRY_DISABLE=1 -e MYSQL_ROOT_HOST=% -e MYSQL_ROOT_PASSWORD=mysql -d $mysql_docker_image"
+    start_mysql_container="sudo docker run --name $container_name $mount_dir -p 3306:3306 -e PERCONA_TELEMETRY_DISABLE=1 -e MYSQL_ROOT_HOST=% -e MYSQL_ROOT_PASSWORD=mysql -d $mysql_docker_image"
 
     mkdir /tmp/mysql_data
     sudo chmod -R 777 /tmp/mysql_data
     sudo chmod -R 777 /var/run/mysqld
 
-    echo "Run $server docker container"
+    echo "Run $container_name docker container"
     if ! $start_mysql_container >>backup_log 2>&1; then
-        echo "ERR: The docker command to start $server failed"
+        echo "ERR: The docker command to start $container_name failed"
         exit 1
     fi
 
     echo "Waiting for mysql to start..."
     for ((i=1; i<=180; i++)); do
-        if ! sudo docker ps -a | grep $server | grep "Up" >/dev/null 2>&1; then
+        if ! sudo docker ps -a | grep $container_name | grep "Up" >/dev/null 2>&1; then
             sleep 1
         else
             break
@@ -130,33 +148,33 @@ test_pxb_docker() {
     sleep 20
 
     echo -n "Mysql started with version: "
-    sudo docker exec -it $server mysql -uroot -pmysql -Bse "SELECT @@version;" |grep -v "Using a password"
+    sudo docker exec -it $container_name mysql -uroot -pmysql -Bse "SELECT @@version;" |grep -v "Using a password"
 
     echo "Add data in the database"
-    sudo docker exec -it $server mysql -uroot -pmysql -e "CREATE DATABASE IF NOT EXISTS test;" >/dev/null 2>&1
-    sudo docker exec -it $server mysql -uroot -pmysql -e "CREATE TABLE test.t1(i INT);" >/dev/null 2>&1
-    sudo docker exec -it $server mysql -uroot -pmysql -e "INSERT INTO test.t1 VALUES (1), (2), (3), (4), (5);" >/dev/null 2>&1
+    sudo docker exec -it $container_name mysql -uroot -pmysql -e "CREATE DATABASE IF NOT EXISTS test;" >/dev/null 2>&1
+    sudo docker exec -it $container_name mysql -uroot -pmysql -e "CREATE TABLE test.t1(i INT);" >/dev/null 2>&1
+    sudo docker exec -it $container_name mysql -uroot -pmysql -e "INSERT INTO test.t1 VALUES (1), (2), (3), (4), (5);" >/dev/null 2>&1
 
     echo "Run pxb docker container, take backup and prepare it"
     echo "Using $repo repo docker image"
-    sudo docker run --volumes-from $server -v $pxb_backup_dir -it --rm --user root $pxb_docker_image /bin/bash -c "xtrabackup --backup --datadir=/var/lib/mysql/ --target-dir=$target_backup_dir --user=root --password=mysql ; xtrabackup --prepare --target-dir=$target_backup_dir" >>backup_log 2>&1
+    sudo docker run --volumes-from $container_name -v $pxb_backup_dir -it --rm --user root $pxb_docker_image /bin/bash -c "xtrabackup --backup --datadir=/var/lib/mysql/ --target-dir=$target_backup_dir --user=root --password=mysql ; xtrabackup --prepare --target-dir=$target_backup_dir" >>backup_log 2>&1
 
     if [ "$?" -ne 0 ]; then
-        echo "ERR: The docker command to run $version failed"
+        echo "ERR: The docker command to run $pxb_version-$innovation failed"
         exit 1
     else
         echo "The backup and prepare was successful. Log available at: ${PWD}/backup_log"
     fi
 
-    echo "Stop the $server docker container"
-    sudo docker stop $server >>backup_log 2>&1
+    echo "Stop the $container_name docker container"
+    sudo docker stop $container_name >>backup_log 2>&1
 
     sudo rm -r /tmp/mysql_data
     mkdir /tmp/mysql_data
 
     echo "Run pxb docker container to restore the backup"
     echo "Using $repo repo docker image"
-    sudo docker run --volumes-from $server -v $pxb_backup_dir -it --rm --user root $pxb_docker_image /bin/bash -c "xtrabackup --copy-back --datadir=/var/lib/mysql/ --target-dir=$target_backup_dir" >>backup_log 2>&1
+    sudo docker run --volumes-from $container_name -v $pxb_backup_dir -it --rm --user root $pxb_docker_image /bin/bash -c "xtrabackup --copy-back --datadir=/var/lib/mysql/ --target-dir=$target_backup_dir" >>backup_log 2>&1
 
     if [ "$?" -ne 0 ]; then
         echo "ERR: The docker command to restore the data failed"
@@ -167,15 +185,15 @@ test_pxb_docker() {
 
     sudo chmod -R 777 /tmp/mysql_data
 
-    echo "Start the $server container with the restored data"
-    if ! sudo docker start $server >>backup_log 2>&1; then
+    echo "Start the $container_name container with the restored data"
+    if ! sudo docker start $container_name >>backup_log 2>&1; then
         echo "ERR: The docker command to start mysql 8.0 with the restored data failed"
         exit 1
     fi
 
     echo "Waiting for mysql to start..."
     for ((i=1; i<=180; i++)); do
-        if ! sudo docker ps -a | grep $server >/dev/null 2>&1; then
+        if ! sudo docker ps -a | grep $container_name >/dev/null 2>&1; then
             sleep 1
         else
             break
@@ -190,16 +208,16 @@ test_pxb_docker() {
     # Sleep for sometime for the server to fully come-up
     sleep 20
 
-    if [ "$(sudo docker exec -it $server mysql -uroot -pmysql -Bse 'SELECT * FROM test.t1;' | grep -v password | wc -l)" != "5" ]; then
+    if [ "$(sudo docker exec -it $container_name mysql -uroot -pmysql -Bse 'SELECT * FROM test.t1;' | grep -v password | wc -l)" != "5" ]; then
         echo "ERR: Data could not be checked in the mysql container"
     else
         echo "Data was restored successfully"
     fi
 
     # Cleanup
-    echo "Stopping and removing $server docker container"
-    sudo docker stop $server >>backup_log 2>&1
-    sudo docker rm $server >>backup_log 2>&1
+    echo "Stopping and removing $container_name docker container"
+    sudo docker stop $container_name >>backup_log 2>&1
+    sudo docker rm $container_name >>backup_log 2>&1
 }
 
 # Check and clean existing installation
--- backup_tests/pkg_install_tests.sh
+++ backup_tests/pkg_install_tests.sh
@@ -1,149 +1,119 @@
 #!/bin/bash
 
-###############################################################################
-# Created By Manish Chawla, Percona LLC                                       #
-# Modified By Mohit Joshi, Percona LLC                                        #
-# This script installs pxb2.4/pxb80/pxb81 from the main/testing repo          #
-# Usage:                                                                      #
-# 1. Run the script as: ./pkg_install_tests.sh pxb24/pxb80/pxb81 main/testing #
-# 3. Logs are available in: $HOME/install_log                                 #
-###############################################################################
+############################################################################################
+# Created By Manish Chawla, Percona LLC                                                    #
+# Modified By Mohit Joshi, Percona LLC                                                     #
+# This script installs pxb24/pxb80/pxb8x-innovation from the main/testing repo             #
+############################################################################################
 
 log="$HOME/install_log"
-if [ "$#" -ne 2 ]; then
-    echo "Please run the script with parameters: <pxb-version=pxb24/pxb80/pxb81> <repo=main/testing>"
+
+help() {
+    echo "Usage: $0 pxb_version repo [version]"
+    echo "Accepted values for pxb_version: pxb24 , pxb80 , pxb-8x-innovation"
+    echo "Accepted values for repo: main, testing"
+    echo "Accepted value for version: 81, 82 [Required only when pxb_version=pxb-8x-innovation]"
+    echo "eg. pkg_install_tests.sh pxb-8x-innovation main 81"
+    echo "eg. pkg_install_tests.sh pxb80 testing"
+    echo "eg. pkg_install_tests.sh pxb24 main"
     exit 1
+}
+
+if [ "$#" -lt 2 ] || [ "$#" -gt 3 ]; then
+    help
 fi
 
 pxb_version="$1"
 repo="$2"
 
+# Check if pxb_version is "pxb-8x-innovation"
+if [ "$pxb_version" == "pxb-8x-innovation" ]; then
+    # Check if the number of arguments is 3
+    if [ "$#" -ne 3 ]; then
+        echo "Error: 'version' argument is required for pxb-8x-innovation. Accepted values: 81, 82"
+        help
+    fi
+    version=$3
+else
+    version=""  # Set version to an empty string for other cases
+fi
+
 if [ -f /usr/bin/yum ]; then
     install_cmd="sudo yum install -y"
     remove_cmd="sudo yum remove -y"
     list_cmd="sudo yum list installed"
-    pxb24_packages="percona-xtrabackup-24 percona-xtrabackup-test-24 percona-xtrabackup-24-debuginfo"
-    pxb80_packages="percona-xtrabackup-80 percona-xtrabackup-test-80 percona-xtrabackup-80-debuginfo"
-    pxb81_packages="percona-xtrabackup-81 percona-xtrabackup-test-81 percona-xtrabackup-81-debuginfo"
+    if [ "$pxb_version" = "pxb24" ]; then
+        pxb_package="percona-xtrabackup-24"
+        pxb_addon_packages="percona-xtrabackup-test-24 percona-xtrabackup-24-debuginfo"
+    elif [ "$pxb_version" = "pxb80" ]; then
+        pxb_package="percona-xtrabackup-80"
+        pxb_addon_packages="percona-xtrabackup-test-80 percona-xtrabackup-80-debuginfo"
+    elif [ "$pxb_version" = "pxb-8x-innovation" ]; then
+        pxb_package="percona-xtrabackup-$version"
+        pxb_addon_packages="percona-xtrabackup-test-$version percona-xtrabackup-$version-debuginfo"
+    else
+        echo "Invalid pxb version $pxb_version"
+        help
+    fi
 else
     install_cmd="sudo apt-get install -y"
     remove_cmd="sudo apt-get remove -y"
     list_cmd="sudo apt list --installed"
-    pxb24_packages="percona-xtrabackup-24 percona-xtrabackup-test-24 percona-xtrabackup-dbg-24"
-    pxb80_packages="percona-xtrabackup-80 percona-xtrabackup-test-80 percona-xtrabackup-dbg-80"
-    pxb81_packages="percona-xtrabackup-81 percona-xtrabackup-test-81 percona-xtrabackup-dbg-81"
+    if [ "$pxb_version" = "pxb24" ]; then
+        pxb_package="percona-xtrabackup-24"
+        pxb_addon_packages="percona-xtrabackup-test-24 percona-xtrabackup-dbg-24"
+    elif [ "$pxb_version" = "pxb80" ]; then
+        pxb_package="percona-xtrabackup-80"
+        pxb_addon_packages="percona-xtrabackup-test-80 percona-xtrabackup-dbg-80"
+    elif [ "$pxb_version" = "pxb-8x-innovation" ]; then
+        pxb_package="percona-xtrabackup-$version"
+        pxb_addon_packages="percona-xtrabackup-test-$version percona-xtrabackup-dbg-$version"
+    else
+        echo "Invalid pxb version $pxb_version"
+        help
+    fi
+fi
+
+if [ "$repo" = "main" ]; then
+    install_repo=release
+elif [ "$repo" = "testing" ]; then
+    install_repo=testing
 fi
 
 install_pxb_package() {
     echo "Checking if percona-xtrabackup is already installed" 
-    if ${list_cmd} | grep "percona-xtrabackup-24" >"${log}" ; then
-        echo "Uninstalling PXB 2.4 packages"
-        ${remove_cmd} ${pxb24_packages} >>"${log}"
-    elif ${list_cmd} |grep "percona-xtrabackup-80" >"${log}" ; then
-        echo "Uninstalling PXB 8.0 packages"
-        ${remove_cmd} ${pxb80_packages} >>"${log}"
-    elif ${list_cmd} |grep "percona-xtrabackup-81" >"${log}" ; then
-        echo "Uninstalling PXB 8.0 packages"
-        ${remove_cmd} ${pxb81_packages} >>"${log}"
+    if ${list_cmd} | grep "$pxb_package" >"${log}" ; then
+        echo "Uninstalling $pxb_version packages"
+        ${remove_cmd} ${pxb_package} >>"${log}" 2>&1
+        ${remove_cmd} ${pxb_addon_packages} >>"${log}" 2>&1
     else
         echo "PXB packages are not installed"
     fi
-    ${remove_cmd} percona-release >>"${log}"
+
+    ${remove_cmd} percona-release >>"${log}" 2>&1
 
     if [ -f /usr/bin/apt ]; then
-        sudo apt-get update >>"${log}"
-        wget https://repo.percona.com/apt/percona-release_latest.generic_all.deb
-        sudo dpkg -i percona-release_latest.generic_all.deb
+        sudo apt-get update > /dev/null 2>&1
+        wget https://repo.percona.com/apt/percona-release_latest.generic_all.deb > /dev/null 2>&1
+        sudo dpkg -i percona-release_latest.generic_all.deb > /dev/null 2>&1 
     else
-        ${install_cmd} https://repo.percona.com/yum/percona-release-latest.noarch.rpm >>"${log}"
+        ${install_cmd} https://repo.percona.com/yum/percona-release-latest.noarch.rpm  > /dev/null  2>&1
     fi
 
-    if [[ "$1" = "pxb24" ]] && [[ "$2" = "main" ]]; then
-        echo "Installing PXB 2.4 packages from the main repo"
-        sudo percona-release enable-only tools release
-        if [ -f /usr/bin/apt ]; then
-            sudo apt-get update >>"${log}"
-        fi
-
-        if ! ${install_cmd} ${pxb24_packages} >>"${log}" ; then
-            echo "ERR: PXB 2.4 packages could not be installed from the main repo"
-            exit 1
-        else
-            echo "PXB 2.4 packages successfully installed with version: "
-            xtrabackup --version
-        fi
-    elif [[ "$1" = "pxb24" ]] && [[ "$2" = "testing" ]]; then
-        echo "Installing PXB 2.4 packages from the testing repo"
-        sudo percona-release enable-only tools testing
-        if [ -f /usr/bin/apt ]; then
-            sudo apt-get update >>"${log}"
-        fi
-
-        if ! ${install_cmd} ${pxb24_packages} >>"${log}" ; then
-            echo "ERR: PXB 2.4 packages could not be installed from the testing repo"
-            exit 1
-        else
-            echo "PXB 2.4 packages successfully installed with version: "
-            xtrabackup --version
-        fi
-    elif [[ "$1" = "pxb80" ]] && [[ "$2" = "main" ]]; then
-        echo "Installing PXB 8.0 packages from the main repo"
-        sudo percona-release enable-only tools release
-        if [ -f /usr/bin/apt ]; then
-            sudo apt-get update >>"${log}"
-        fi
-
-        if ! ${install_cmd} ${pxb80_packages} >>"${log}" ; then
-            echo "ERR: PXB 8.0 packages could not be installed from the main repo"
-            exit 1
-        else
-            echo "PXB 8.0 packages successfully installed with version: "
-            xtrabackup --version
-        fi
-    elif [[ "$1" = "pxb80" ]] && [[ "$2" = "testing" ]]; then
-        echo "Installing PXB 8.0 packages from the testing repo"
-        sudo percona-release enable-only tools testing
-        if [ -f /usr/bin/apt ]; then
-            sudo apt-get update >>"${log}"
-        fi
-
-        if ! ${install_cmd} ${pxb80_packages} >>"${log}" ; then
-            echo "ERR: PXB 8.0 packages could not be installed from the testing repo"
-            exit 1
-        else
-            echo "PXB 8.0 packages successfully installed with version: "
-            xtrabackup --version
-        fi
-    elif [[ "$1" = "pxb81" ]] && [[ "$2" = "main" ]]; then
-        echo "Installing PXB 8.1 packages from the main repo"
-        sudo percona-release enable-only tools release
-        if [ -f /usr/bin/apt ]; then
-            sudo apt-get update >>"${log}"
-        fi
-
-        if ! ${install_cmd} ${pxb81_packages} >>"${log}" ; then
-            echo "ERR: PXB 8.1 packages could not be installed from the main repo"
-            exit 1
-        else
-            echo "PXB 8.1 packages successfully installed with version: "
-            xtrabackup --version
-        fi
-    elif [[ "$1" = "pxb81" ]] && [[ "$2" = "testing" ]]; then
-        echo "Installing PXB 8.1 packages from the testing repo"
-        sudo percona-release enable-only tools testing
-        if [ -f /usr/bin/apt ]; then
-            sudo apt-get update >>"${log}"
-        fi
-
-        if ! ${install_cmd} ${pxb81_packages} >>"${log}" ; then
-            echo "ERR: PXB 8.1 packages could not be installed from the testing repo"
-            exit 1
-        else
-            echo "PXB 8.1 packages successfully installed with version: "
-            xtrabackup --version
-        fi
+    echo "Installing $pxb_version packages from the $repo repo"
+    sudo percona-release enable-only tools $install_repo
+    if [ -f /usr/bin/apt ]; then
+        sudo apt-get update > /dev/null 2>&1
     fi
 
+    if ! ${install_cmd} ${pxb_package} >>"${log}" ; then
+        echo "ERR: $pxb_version packages could not be installed from the $repo repo"
+        exit 1
+    else
+        ${install_cmd} ${pxb_addon_packages}
+        echo "$pxb_version packages successfully installed with version: "
+        xtrabackup --version
+    fi
 }
 
-install_pxb_package "${pxb_version}" "${repo}"
+install_pxb_package
