c28d7ab85c9dc17a79080f1825b5449f91ea3b09
Mohit Joshi <mohit.joshi@percona.com>
2024-08-08 17:58:03 +0000

    Modified pkg_install_tests.sh to accomodate new pxb-84-lts repo

--- backup_tests/pkg_install_tests.sh
+++ backup_tests/pkg_install_tests.sh
@@ -9,13 +9,14 @@
 log="$HOME/install_log"
 
 help() {
-    echo "Usage: $0 pxb_version repo [version]"
-    echo "Accepted values for pxb_version: pxb24 , pxb80 , pxb-8x-innovation"
-    echo "Accepted values for repo: main, testing"
-    echo "Accepted value for version: 81, 82 [Required only when pxb_version=pxb-8x-innovation]"
+    echo "Usage: $0 repo_name repo_type [version]"
+    echo "Accepted values for repo_name: pxb-24 , pxb-80 , pxb-8x-innovation, pxb-84-lts"
+    echo "Accepted values for repo_type: main, testing, experimental"
+    echo "Accepted value for version: 81, 82, 83 [Required only when repo_name=pxb-8x-innovation]"
     echo "eg. pkg_install_tests.sh pxb-8x-innovation main 81"
     echo "eg. pkg_install_tests.sh pxb80 testing"
     echo "eg. pkg_install_tests.sh pxb24 main"
+    echo "eg. pkg_install_test.sh pxb-84-lts testing"
     exit 1
 }
 
@@ -23,14 +24,14 @@ if [ "$#" -lt 2 ] || [ "$#" -gt 3 ]; then
     help
 fi
 
-pxb_version="$1"
-repo="$2"
+repo_name="$1"
+repo_type="$2"
 
 # Check if pxb_version is "pxb-8x-innovation"
-if [ "$pxb_version" == "pxb-8x-innovation" ]; then
+if [ "$repo_name" == "pxb-8x-innovation" ]; then
     # Check if the number of arguments is 3
     if [ "$#" -ne 3 ]; then
-        echo "Error: 'version' argument is required for pxb-8x-innovation. Accepted values: 81, 82"
+        echo "Error: 'version' argument is required for pxb-8x-innovation. Accepted values: 81, 82, 83"
         help
     fi
     version=$3
@@ -42,48 +43,48 @@ if [ -f /usr/bin/yum ]; then
     install_cmd="sudo yum install -y"
     remove_cmd="sudo yum remove -y"
     list_cmd="sudo yum list installed"
-    if [ "$pxb_version" = "pxb24" ]; then
+    if [ "$repo_name" = "pxb-24" ]; then
         pxb_package="percona-xtrabackup-24"
         pxb_addon_packages="percona-xtrabackup-test-24 percona-xtrabackup-24-debuginfo"
-    elif [ "$pxb_version" = "pxb80" ]; then
+    elif [ "$repo_name" = "pxb-80" ]; then
         pxb_package="percona-xtrabackup-80"
         pxb_addon_packages="percona-xtrabackup-test-80 percona-xtrabackup-80-debuginfo"
-    elif [ "$pxb_version" = "pxb-8x-innovation" ]; then
+    elif [ "$repo_name" = "pxb-8x-innovation" ]; then
         pxb_package="percona-xtrabackup-$version"
         pxb_addon_packages="percona-xtrabackup-test-$version percona-xtrabackup-$version-debuginfo"
+    elif [ "$repo_name" = "pxb-84-lts" ]; then
+        pxb_package="percona-xtrabackup-84"
+        pxb_addon_packages="percona-xtrabackup-test-84 percona-xtrabackup-84-debuginfo"
     else
-        echo "Invalid pxb version $pxb_version"
+        echo "Invalid pxb repo $repo_name"
         help
     fi
 else
     install_cmd="sudo apt-get install -y"
     remove_cmd="sudo apt-get remove -y"
     list_cmd="sudo apt list --installed"
-    if [ "$pxb_version" = "pxb24" ]; then
+    if [ "$repo_name" = "pxb-24" ]; then
         pxb_package="percona-xtrabackup-24"
         pxb_addon_packages="percona-xtrabackup-test-24 percona-xtrabackup-dbg-24"
-    elif [ "$pxb_version" = "pxb80" ]; then
+    elif [ "$repo_name" = "pxb-80" ]; then
         pxb_package="percona-xtrabackup-80"
         pxb_addon_packages="percona-xtrabackup-test-80 percona-xtrabackup-dbg-80"
-    elif [ "$pxb_version" = "pxb-8x-innovation" ]; then
+    elif [ "$repo_name" = "pxb-8x-innovation" ]; then
         pxb_package="percona-xtrabackup-$version"
         pxb_addon_packages="percona-xtrabackup-test-$version percona-xtrabackup-dbg-$version"
+    elif [ "$repo_name" = "pxb-84-lts" ]; then
+        pxb_package="percona-xtrabackup-84"
+        pxb_addon_packages="percona-xtrabackup-test-84 percona-xtrabackup-dbg-84"
     else
-        echo "Invalid pxb version $pxb_version"
+        echo "Invalid pxb repo $repo_name"
         help
     fi
-fi
-
-if [ "$repo" = "main" ]; then
-    install_repo=release
-elif [ "$repo" = "testing" ]; then
-    install_repo=testing
 fi
 
 install_pxb_package() {
     echo "Checking if percona-xtrabackup is already installed" 
     if ${list_cmd} | grep "$pxb_package" >"${log}" ; then
-        echo "Uninstalling $pxb_version packages"
+        echo "Uninstalling $repo_name $version packages"
         ${remove_cmd} ${pxb_package} >>"${log}" 2>&1
         ${remove_cmd} ${pxb_addon_packages} >>"${log}" 2>&1
     else
@@ -100,18 +101,18 @@ install_pxb_package() {
         ${install_cmd} https://repo.percona.com/yum/percona-release-latest.noarch.rpm  > /dev/null  2>&1
     fi
 
-    echo "Installing $pxb_version packages from the $repo repo"
-    sudo percona-release enable-only tools $install_repo
+    echo "Installing $repo_name $version packages from the $repo_type repo"
+    sudo percona-release enable-only $repo_name $repo_type
     if [ -f /usr/bin/apt ]; then
         sudo apt-get update > /dev/null 2>&1
     fi
 
     if ! ${install_cmd} ${pxb_package} >>"${log}" ; then
-        echo "ERR: $pxb_version packages could not be installed from the $repo repo"
+        echo "ERR: $repo_name $version packages could not be installed from the $repo_type repo"
         exit 1
     else
         ${install_cmd} ${pxb_addon_packages}
-        echo "$pxb_version packages successfully installed with version: "
+        echo "$repo_name $version packages successfully installed with version: "
         xtrabackup --version
     fi
 }
