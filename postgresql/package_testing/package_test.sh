02131c02349fa513f5d3999f40d8c1a44d0cc601
Mohit Joshi <mohit.joshi@percona.com>
2025-05-22 06:22:20 +0000

    Added pg_tde package testing framework using vagrant => Added Debian11, Debian12, OL8, OL9, Ubuntu20.04, Ubuntu22.04, Ubuntu24.04

---
+++ postgresql/package_testing/package_test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for os in debian11 debian12 ol8 ol9 ubuntu20 ubuntu22 ubuntu24; do
+    echo -e "\n=> Running tests on ${os^^}"
+    pushd $os > /dev/null
+    vagrant destroy -f
+    vagrant up
+    vagrant halt
+    popd > /dev/null
+    echo "=> Tests completed for $os"
+done
