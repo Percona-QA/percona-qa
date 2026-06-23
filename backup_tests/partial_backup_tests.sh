c20ba2dc289f1527f4dee952364cb7a61e353509
Saikumar VS <saikumar.vs@percona.com>
2026-06-23 18:56:54 +0530

    kmip defaults and some fixs for typos

--- backup_tests/partial_backup_tests.sh
+++ backup_tests/partial_backup_tests.sh
@@ -20,14 +20,17 @@ export qascripts="$HOME/percona-qa"
 export logdir="$HOME/backuplogs"
 export mysql_random_data_load_tool="$HOME/mysql_random_data_load"
 declare -A KMIP_CONFIGS=(
-    # PyKMIP Docker Configuration
-    ["pykmip"]="addr=127.0.0.1,image=mohitpercona/kmip:latest,port=5696,name=kmip_pykmip"
-
     # Hashicorp Docker Setup Configuration
-    ["hashicorp"]="addr=127.0.0.1,port=5696,name=kmip_hashicorp,setup_script=hashicorp-kmip-setup.sh"
+    ["hashicorp"]="addr=127.0.0.1,port=5696,name=kmip_hashicorp,setup_script=hashicorp-kmip-setup.py"
+
+    # Fortanix Setup Configuration
+    ["fortanix"]="addr=216.180.120.88,port=5696,name=kmip_fortanix,setup_script=fortanix_kmip_setup.py"
 
     # API Configuration
     # ["ciphertrust"]="addr=127.0.0.1,port=5696,name=kmip_ciphertrust,setup_script=setup_kmip_api.py"
+
+    # PyKMIP Docker Configuration
+    #["pykmip"]="addr=127.0.0.1,image=satyapercona/kmip:latest,port=5696,name=kmip_pykmip"
 )
 
 # Set sysbench variables
@@ -250,7 +253,7 @@ create_keyring_component_files() {
     }' > "$mysqldir/bin/mysqld.my"
 
     start_kmip_server "$kmip_type"
-    [ -f "${HOME}/${kimp_config[cert_dir]}/component_keyring_kmip.cnf" ] && cp "${HOME}/${kimp_config[cert_dir]}/component_keyring_kmip.cnf" "$mysqldir/lib/plugin/"
+    [ -f "${HOME}/${kmip_config[cert_dir]}/component_keyring_kmip.cnf" ] && cp "${HOME}/${kmip_config[cert_dir]}/component_keyring_kmip.cnf" "$mysqldir/lib/plugin/"
 
 elif [ "$keyring_type" = "keyring_file" ]; then
     echo "Keyring type is file. Taking file-based action..."
